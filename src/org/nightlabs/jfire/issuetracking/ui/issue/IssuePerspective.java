5d12c98c26a864ee49b615f7ac27fade76c700a2
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-10-19 09:24:37 +0000

    - modified editor.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@8986 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/IssuePerspective.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/IssuePerspective.java
@@ -5,6 +5,9 @@ import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.nightlabs.base.ui.util.RCPUtil;
 import org.nightlabs.jfire.issuetracking.ui.overview.IssueOverviewView;
+import org.nightlabs.jfire.trade.ui.articlecontainer.header.HeaderTreeView;
+import org.nightlabs.jfire.trade.ui.legalentity.view.LegalEntityEditorView;
+import org.nightlabs.jfire.trade.ui.producttype.quicklist.ProductTypeQuickListView;
 
 public class IssuePerspective implements IPerspectiveFactory{
 	
@@ -19,25 +22,23 @@ public class IssuePerspective implements IPerspectiveFactory{
 	}
 	
 	private void createLayout(IPageLayout layout) {
-//		layout.setEditorAreaVisible(true);
-//		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.25f,	IPageLayout.ID_EDITOR_AREA); //$NON-NLS-1$
-//		left.addView(TradeOverviewView.VIEW_ID);
-//		RCPUtil.addAllPerspectiveShortcuts(layout);
-//		layout.addShowViewShortcut(TradeOverviewView.VIEW_ID);		
 		
 		layout.setEditorAreaVisible(true);
+		
 		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.25f,	IPageLayout.ID_EDITOR_AREA); //$NON-NLS-1$
 		left.addView(IssueOverviewView.VIEW_ID);
+		
+//		layout.addView(
+//		IssueOverviewView.VIEW_ID,
+//		IPageLayout.LEFT,
+//		0.3f,
+//		IPageLayout.ID_EDITOR_AREA
+//	);
+		
 		layout.addPerspectiveShortcut(ID_PERSPECTIVE);
 		layout.addShowViewShortcut(IssueOverviewView.VIEW_ID);
 		RCPUtil.addAllPerspectiveShortcuts(layout);
 		
-//		// Editors are placed for free.
-//		String editorArea = layout.getEditorArea();
-//
-//		// left
-//		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.LEFT, (float) 0.26, editorArea);		 //$NON-NLS-1$
-//		bottomLeft.addView(IssueView.ID_VIEW);
 	}
 	
 	private void addShortCuts(IPageLayout layout) {
--- src/org/nightlabs/jfire/issuetracking/ui/overview/IssueEntryFactory.java
+++ src/org/nightlabs/jfire/issuetracking/ui/overview/IssueEntryFactory.java
@@ -1,9 +1,13 @@
 package org.nightlabs.jfire.issuetracking.ui.overview;
 
+import org.eclipse.ui.PartInitException;
+import org.nightlabs.base.ui.util.RCPUtil;
 import org.nightlabs.jfire.base.ui.overview.AbstractEntryFactory;
 import org.nightlabs.jfire.base.ui.overview.DefaultEntry;
 import org.nightlabs.jfire.base.ui.overview.Entry;
 import org.nightlabs.jfire.base.ui.overview.EntryViewer;
+import org.nightlabs.jfire.base.ui.overview.OverviewEntryEditorInput;
+import org.nightlabs.jfire.issuetracking.ui.issue.editor.IssueEditor;
 
 /**
  * @author Daniel.Mazurek [at] NightLabs [dot] de
@@ -21,17 +25,17 @@ extends AbstractEntryFactory
 
 			public EntryViewer createEntryViewer() {
 				return null;
-//				return new DeliveryNoteEntryViewer(this);
+//				return new IssueEntryViewer(this);
 			}
 
 			public void handleActivation() {
-//				try {
-//					RCPUtil.openEditor(
-//							new OverviewEntryEditorInput(this), 
-//							DeliveryNoteEntryEditor.EDITOR_ID);
-//				} catch (PartInitException e) {
-//					throw new RuntimeException(e);
-//				}
+				try {
+					RCPUtil.openEditor(
+							new OverviewEntryEditorInput(this), 
+							IssueEditor.EDITOR_ID);
+				} catch (PartInitException e) {
+					throw new RuntimeException(e);
+				}
 			}
 			
 		};
