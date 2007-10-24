9a61c60cda30a08daf24113b5f46458ad71d4a6d
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-10-24 04:58:31 +0000

    - issue priorities
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@9035 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/CreateIssueComposite.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/CreateIssueComposite.java
@@ -9,6 +9,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -17,16 +18,25 @@ import org.nightlabs.base.ui.composite.XComboComposite;
 import org.nightlabs.base.ui.composite.XComposite;
 import org.nightlabs.base.ui.exceptionhandler.ExceptionHandlerRegistry;
 import org.nightlabs.base.ui.job.Job;
+import org.nightlabs.base.ui.language.I18nTextEditorMultiLine;
+import org.nightlabs.base.ui.language.I18nTextEditor.EditMode;
+import org.nightlabs.jfire.issue.IssuePriority;
 import org.nightlabs.jfire.issue.IssueSeverityType;
 import org.nightlabs.jfire.issue.IssueStatus;
+import org.nightlabs.jfire.issue.dao.IssuePriorityDAO;
 import org.nightlabs.jfire.issue.dao.IssueSeverityTypeDAO;
 import org.nightlabs.jfire.issue.dao.IssueStatusDAO;
+import org.nightlabs.jfire.trade.ui.resource.Messages;
 import org.nightlabs.progress.ProgressMonitor;
 
 public class CreateIssueComposite extends XComposite{
 	
 	private List<IssueSeverityType> issueSeverityTypes = new ArrayList<IssueSeverityType>();
 	private List<IssueStatus> issueStatus = new ArrayList<IssueStatus>();
+	private List<IssuePriority> issuePriorities = new ArrayList<IssuePriority>();
+	
+	private Label descriptionLabel;
+	private I18nTextEditorMultiLine descriptionText;
 	
 	public CreateIssueComposite(Composite parent, int style) {
 		super(parent, style);
@@ -45,30 +55,24 @@ public class CreateIssueComposite extends XComposite{
 		
 		Label severityLbl = new Label(this, SWT.NONE);
 		severityLbl.setText("Severity: ");
-		
-		ILabelProvider labelProvider = new LabelProvider() {
-			@Override
-			public String getText(Object element) 
-			{
-				if (element instanceof IssueSeverityType) {
-					IssueSeverityType issueSeverityType = (IssueSeverityType) element;
-					return issueSeverityType.getIssueSeverityTypeText().getText();
-				}
-				
-				if (element instanceof IssueStatus) {
-					IssueStatus issueStatus = (IssueStatus) element;
-					return issueStatus.getIssueStatusText().getText();
-				}
-				return super.getText(element);
-			}		
-		};
-		
 		final XComboComposite<IssueSeverityType> severityCombo = new XComboComposite<IssueSeverityType>(this, SWT.NONE, labelProvider);
 		
 		Label statusLbl = new Label(this, SWT.NONE);
 		statusLbl.setText("Status: ");
-		
 		final XComboComposite<IssueStatus> statusCombo = new XComboComposite<IssueStatus>(this, SWT.NONE, labelProvider);
+		
+		Label priorityLbl = new Label(this, SWT.NONE);
+		priorityLbl.setText("Priority: ");
+		final XComboComposite<IssuePriority> priorityCombo = new XComboComposite<IssuePriority>(this, SWT.NONE, labelProvider);
+		
+		descriptionLabel = new Label(this, SWT.NONE);
+		descriptionLabel.setText("Description: ");
+
+		descriptionText = new I18nTextEditorMultiLine(this);
+		descriptionText.setI18nText(null, EditMode.BUFFERED);
+
+		GridData gridData = new GridData(GridData.FILL_BOTH);
+		descriptionText.setLayoutData(gridData);
 		
 		Job loadJob = new Job("Loading Issue Severity Types....") {
 			@Override
@@ -76,6 +80,7 @@ public class CreateIssueComposite extends XComposite{
 				try {
 					issueSeverityTypes = IssueSeverityTypeDAO.sharedInstance().getIssueSeverityTypes(monitor);
 					issueStatus = IssueStatusDAO.sharedInstance().getIssueStatus(monitor);
+					issuePriorities = IssuePriorityDAO.sharedInstance().getIssuePriorities(monitor);
 				} catch (Exception e) {
 					ExceptionHandlerRegistry.asyncHandleException(e);
 					throw new RuntimeException(e);
@@ -101,6 +106,17 @@ public class CreateIssueComposite extends XComposite{
 						statusCombo.selectElementByIndex(1);
 					}
 				});
+				
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						priorityCombo.removeAll();
+						for (Iterator it = issuePriorities.iterator(); it.hasNext(); ) {
+							IssuePriority ip = (IssuePriority) it.next();
+							priorityCombo.addElement(ip);
+						}
+						priorityCombo.selectElementByIndex(1);
+					}
+				});
 
 				return Status.OK_STATUS;
 			} 
@@ -110,7 +126,27 @@ public class CreateIssueComposite extends XComposite{
 		
 //		 Category  	
 //		 Reproducibility 	
-//		 Severity 	
-//		 Priority 	
 	}
+	
+	private ILabelProvider labelProvider = new LabelProvider() {
+		@Override
+		public String getText(Object element) 
+		{
+			if (element instanceof IssueSeverityType) {
+				IssueSeverityType issueSeverityType = (IssueSeverityType) element;
+				return issueSeverityType.getIssueSeverityTypeText().getText();
+			}
+			
+			if (element instanceof IssueStatus) {
+				IssueStatus issueStatus = (IssueStatus) element;
+				return issueStatus.getIssueStatusText().getText();
+			}
+			
+			if (element instanceof IssuePriority) {
+				IssuePriority issuePriority = (IssuePriority) element;
+				return issuePriority.getIssuePriorityText().getText();
+			}
+			return super.getText(element);
+		}		
+	};
 }
