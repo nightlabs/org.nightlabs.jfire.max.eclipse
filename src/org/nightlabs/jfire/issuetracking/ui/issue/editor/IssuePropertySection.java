5bafc129e68b5de4cbce77b3a3bd2a93631efe46
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-12-20 06:12:22 +0000

    Issue Editor
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@9588 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssuePropertySection.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssuePropertySection.java
@@ -5,12 +5,20 @@ package org.nightlabs.jfire.issuetracking.ui.issue.editor;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.forms.editor.FormPage;
+import org.nightlabs.base.ui.action.SelectionListenerAction;
 import org.nightlabs.base.ui.composite.XComboComposite;
 import org.nightlabs.base.ui.exceptionhandler.ExceptionHandlerRegistry;
 import org.nightlabs.base.ui.job.Job;
@@ -32,61 +40,92 @@ public class IssuePropertySection extends AbstractIssueEditorGeneralSection {
 	private Label priorityLabel;
 	private Label severityLabel;
 	private Label resolutionLabel;
-	
+
 	private XComboComposite<IssuePriority> issuePriorityCombo;
 	private XComboComposite<IssueSeverityType> issueSeverityTypeCombo;
 	private XComboComposite<IssueResolution> issueResolutionCombo;
-	
+
 	private CurrentStateComposite currentStateComposite;
 	private NextTransitionComposite nextTransitionComposite;
-	
+
 	/**
 	 * @param section
 	 * @param managedForm
 	 */
-	public IssuePropertySection(FormPage page, Composite parent, IssueEditorPageController controller) {
+	public IssuePropertySection(FormPage page, Composite parent, final IssueEditorPageController controller) {
 		super(page, parent, controller);
 		getClient().getGridLayout().numColumns = 3;
 		getClient().getGridLayout().makeColumnsEqualWidth = false;
 		getSection().setText("Properties");
-		
+
 		priorityLabel = new Label(getClient(), SWT.WRAP);
 		priorityLabel.setText("Priority: ");
-		priorityLabel.setLayoutData(new GridData());
-		
+		priorityLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
 		severityLabel = new Label(getClient(), SWT.WRAP);
 		severityLabel.setText("Severity: ");
-		severityLabel.setLayoutData(new GridData());
-		
+		severityLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
 		resolutionLabel = new Label(getClient(), SWT.WRAP);
 		resolutionLabel.setText("Resolution: ");
-		resolutionLabel.setLayoutData(new GridData());
-		
-		issuePriorityCombo = new XComboComposite<IssuePriority>(getClient(), SWT.WRAP);
+		resolutionLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		issuePriorityCombo = new XComboComposite<IssuePriority>(getClient(), SWT.NONE);
 		issuePriorityCombo.setLabelProvider(new IssueLabelProvider());
 		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
 		issuePriorityCombo.setLayoutData(gd);
-		
-		issueSeverityTypeCombo = new XComboComposite<IssueSeverityType>(getClient(), SWT.WRAP);
+		issuePriorityCombo.addSelectionChangedListener(new ISelectionChangedListener(){
+			public void selectionChanged(SelectionChangedEvent e) {
+				controller.getIssue().setIssuePriority(issuePriorityCombo.getSelectedElement());
+				markDirty();
+			}
+		});
+
+		issueSeverityTypeCombo = new XComboComposite<IssueSeverityType>(getClient(), SWT.NONE);
 		issueSeverityTypeCombo.setLabelProvider(new IssueLabelProvider());
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		issueSeverityTypeCombo.setLayoutData(gd);
-		
-		issueResolutionCombo = new XComboComposite<IssueResolution>(getClient(), SWT.WRAP);
+		issueSeverityTypeCombo.addSelectionChangedListener(new ISelectionChangedListener(){
+			public void selectionChanged(SelectionChangedEvent e) {
+				controller.getIssue().setIssueSeverityType(issueSeverityTypeCombo.getSelectedElement());
+				markDirty();
+			}
+		});
+
+		issueResolutionCombo = new XComboComposite<IssueResolution>(getClient(), SWT.NONE);
 		issueResolutionCombo.setLabelProvider(new IssueLabelProvider());
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		issueResolutionCombo.setLayoutData(gd);
+		issueResolutionCombo.addSelectionChangedListener(new ISelectionChangedListener(){
+			public void selectionChanged(SelectionChangedEvent e) {
+				controller.getIssue().setIssueResolution(issueResolutionCombo.getSelectedElement());
+				markDirty();
+			}
+		});
 	}
-	
+
 	protected void doSetIssue(Issue issue) {
 		loadProperties(issue);
+
+		priorityLabel.setText(
+				String.format(
+						"Priority: %s", 
+						issue.getIssuePriority().getIssuePriorityText().getText())
+		);
+
 		severityLabel.setText(
-			String.format(
-				"Severity Type: %s", 
-				issue.getIssueType().getName().getText())
+				String.format(
+						"Severity Type: %s", 
+						issue.getIssueSeverityType().getIssueSeverityTypeText().getText())
+		);
+		
+		resolutionLabel.setText(
+				String.format(
+						"Resolution: %s", 
+						issue.getIssueResolution() == null? "-": issue.getIssueResolution().getName().getText())
 		);
 	}
-	
+
 	private void loadProperties(final Issue issue){
 		Job loadJob = new Job("Loading Issue Properties....") {
 			@Override
@@ -98,19 +137,16 @@ public class IssuePropertySection extends AbstractIssueEditorGeneralSection {
 							for (IssueResolution ir : issue.getIssueType().getIssueResolutions()) {
 								issueResolutionCombo.addElement(ir);
 							}
-							issueResolutionCombo.selectElement(issue.getIssueResolution());
-							
+
 							issuePriorityCombo.removeAll();
 							for (IssuePriority ip : issue.getIssueType().getIssuePriorities()) {
 								issuePriorityCombo.addElement(ip);
 							}
-							issuePriorityCombo.selectElement(issue.getIssuePriority());
-							
+
 							issueSeverityTypeCombo.removeAll();
 							for (IssueSeverityType it : issue.getIssueType().getIssueSeverityTypes()) {
 								issueSeverityTypeCombo.addElement(it);
 							}
-							issueSeverityTypeCombo.selectElement(issue.getIssueSeverityType());
 						}
 					});
 				}catch (Exception e1) {
@@ -125,4 +161,15 @@ public class IssuePropertySection extends AbstractIssueEditorGeneralSection {
 		loadJob.schedule();
 	}
 
+	public IssuePriority getSelectedPriority(){
+		return issuePriorityCombo.getSelectedElement();
+	}
+	
+	public IssueSeverityType getSelectedSeverityType(){
+		return issueSeverityTypeCombo.getSelectedElement();
+	}
+	
+	public IssueResolution getSelectedResolution(){
+		return issueResolutionCombo.getSelectedElement();
+	}
 }
