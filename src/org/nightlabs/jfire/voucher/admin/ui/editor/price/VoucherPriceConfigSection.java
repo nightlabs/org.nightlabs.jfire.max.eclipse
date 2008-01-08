bd729e7a378cb8f77a3f602da1dcea549da958d1
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-08 08:56:37 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.voucher.admin.ui@9622 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/voucher/admin/ui/editor/price/VoucherPriceConfigSection.java
@@ -0,0 +1,62 @@
+package org.nightlabs.jfire.voucher.admin.ui.editor.price;
+
+import java.util.Map;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.editor.IFormPage;
+import org.nightlabs.base.ui.editor.ToolBarSectionPart;
+import org.nightlabs.jfire.accounting.Currency;
+import org.nightlabs.jfire.voucher.accounting.VoucherPriceConfig;
+import org.nightlabs.jfire.voucher.admin.ui.priceconfig.CurrencyAmountTable;
+import org.nightlabs.jfire.voucher.store.VoucherType;
+
+/**
+ * @author fitas [at] NightLabs [dot] de
+ *
+ */
+
+public class VoucherPriceConfigSection 
+extends ToolBarSectionPart 
+{
+	private CurrencyAmountTable currencyAmountTable;
+	private VoucherPriceConfig voucherconfig;
+	
+	
+	/**
+	 * @param page
+	 * @param parent
+	 * @param style
+	 */
+	public VoucherPriceConfigSection(IFormPage page, Composite parent, int style) {
+		super(page, parent, style, "PriceConfig");
+		currencyAmountTable = new CurrencyAmountTable(getContainer(),false);
+	
+	
+	}
+	
+	@Override
+	public void commit(boolean onSave) {
+		super.commit(onSave);
+		
+		Map<Currency, Long> map = currencyAmountTable.getMap();
+		
+		
+		for(Map.Entry<Currency, Long> me : map.entrySet()) {
+			voucherconfig.setPrice(me.getKey(), me.getValue());
+		}
+	
+	
+	}
+	
+	
+	
+	public void setVoucherType(VoucherType voucher)
+	{
+	
+     voucherconfig = (VoucherPriceConfig) voucher.getInnerPriceConfig();
+
+     Map<Currency, Long> map = voucherconfig.getPrices();
+	 currencyAmountTable.setMap(map);
+	}
+
+}
---
+++ src/org/nightlabs/jfire/voucher/admin/ui/editor/price/VoucherTypePricePage.java
@@ -0,0 +1,120 @@
+package org.nightlabs.jfire.voucher.admin.ui.editor.price;
+
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.editor.IFormPage;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
+import org.nightlabs.base.ui.entity.editor.EntityEditor;
+import org.nightlabs.base.ui.entity.editor.EntityEditorPageWithProgress;
+import org.nightlabs.base.ui.entity.editor.IEntityEditorPageController;
+import org.nightlabs.base.ui.entity.editor.IEntityEditorPageFactory;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.jfire.trade.admin.ui.editor.IProductTypeSectionPart;
+import org.nightlabs.jfire.voucher.admin.ui.editor.VoucherLayoutSection;
+import org.nightlabs.jfire.voucher.admin.ui.editor.VoucherTypeDetailPage;
+import org.nightlabs.jfire.voucher.admin.ui.editor.VoucherTypeDetailPageController;
+import org.nightlabs.jfire.voucher.admin.ui.editor.VoucherTypeNameSection;
+import org.nightlabs.jfire.voucher.admin.ui.editor.VoucherTypeSaleAccessControlSection;
+import org.nightlabs.jfire.voucher.admin.ui.resource.Messages;
+import org.nightlabs.jfire.voucher.store.VoucherType;
+
+/**
+ * @author fitas [at] NightLabs [dot] de
+ *
+ */
+public class VoucherTypePricePage 
+extends EntityEditorPageWithProgress 
+
+{
+	public VoucherTypePricePage(FormEditor editor, String id, String name) {
+		//super(editor, id, name);
+		
+		super(editor, VoucherTypePricePage.class.getName(), 
+				Messages.getString("org.nightlabs.jfire.voucher.admin.ui.editor.price.VoucherTypePricePage.title")); //$NON-NLS-1$
+
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * The Factory is registered to the extension-point and creates
+	 * new instances of {@link EventDetailPage}. 
+	 */
+	public static class Factory implements IEntityEditorPageFactory {
+		public IFormPage createPage(FormEditor formEditor) {
+			return new VoucherTypeDetailPage(formEditor);
+		}
+
+		public IEntityEditorPageController createPageController(EntityEditor editor) {
+			
+			VoucherTypeDetailPageController controller = (VoucherTypeDetailPageController) editor.getController().getPageControllers(VoucherTypeDetailPageController.class);
+			
+			if(controller == null)
+		    controller = new VoucherTypeDetailPageController(editor);
+			
+			return controller;
+		}
+	}
+	
+
+	
+	private VoucherPriceConfigSection voucherPriceConfigSection = null;
+	
+	public VoucherPriceConfigSection getVoucherLayoutSection() {
+		return voucherPriceConfigSection;
+	}
+	
+//	private VoucherTypeNameSection voucherTypeNameSection = null;
+//	public VoucherTypeNameSection getVoucherTypeNameSection() {
+//		return voucherTypeNameSection;
+//	}
+//	
+//	private VoucherTypeSaleAccessControlSection voucherTypeSaleAccessControlSection = null;
+//	public VoucherTypeSaleAccessControlSection getVoucherSaleAccessControlSection() {
+//		return voucherTypeSaleAccessControlSection;
+//	}
+	
+	@Override
+	protected void addSections(Composite parent) 
+	{
+		//super.addSections(parent);
+		
+		
+		voucherPriceConfigSection = new VoucherPriceConfigSection(this, parent, ExpandableComposite.TITLE_BAR);
+		voucherPriceConfigSection.getSection().setLayoutData(new GridData(GridData.FILL_HORIZONTAL));		
+		getManagedForm().addPart(voucherPriceConfigSection);
+	}
+
+	@Override
+	protected void asyncCallback() 
+	{
+		VoucherTypeDetailPageController controller = (VoucherTypeDetailPageController) getPageController();
+		final VoucherType voucherType = (VoucherType) controller.getProductType();
+		Display.getDefault().asyncExec(new Runnable() {
+			
+			public void run() {
+				
+				voucherPriceConfigSection.setVoucherType(voucherType);
+				//getNameSection().setProductType(voucherType);
+				//getSaleAccessControlSection().setProductType(voucherType);
+				
+				if (voucherType.isClosed()) {
+					getManagedForm().getForm().getForm().setMessage(
+							Messages.getString("org.nightlabs.jfire.voucher.admin.ui.editor.VoucherTypeDetailPage.productTypeClosedMessage"), //$NON-NLS-1$
+							IMessageProvider.INFORMATION);
+					RCPUtil.setControlEnabledRecursive(getManagedForm().getForm(), false);
+			
+				}
+				switchToContent();				
+			}
+		});
+	}
+
+	@Override
+	protected String getPageFormTitle() {
+		return Messages.getString("org.nightlabs.jfire.voucher.admin.ui.editor.price.VoucherTypePricePage.pageFormTitle"); //$NON-NLS-1$
+	}
+	
+}
