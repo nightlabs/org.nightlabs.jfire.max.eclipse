0069892e84e3d3f4895962de1396f2cc6a99c681
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-08 11:25:36 +0000

    issueLinkHandler Stuffs.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.trade.ui@9631 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/issuetracking/trade/ui/issuelink/IssueLinkAdder.java
@@ -0,0 +1,27 @@
+package org.nightlabs.jfire.issuetracking.trade.ui.issuelink;
+
+import org.nightlabs.jfire.issuetracking.ui.issuelink.AbstractIssueLinkAdder;
+
+/**
+ * @author Chairat Kongarayawetchakun - chairat at nightlabs dot de
+ *
+ */
+public class IssueLinkAdder extends AbstractIssueLinkAdder{
+
+	@Override
+	protected Object doCreateComposite(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getIssueLinkObjectId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isComplete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/org/nightlabs/jfire/issuetracking/trade/ui/issuelink/IssueLinkAdderFactory.java
@@ -0,0 +1,27 @@
+package org.nightlabs.jfire.issuetracking.trade.ui.issuelink;
+
+import org.nightlabs.jfire.issuetracking.ui.issuelink.AbstractIssueLinkHandlerFactory;
+import org.nightlabs.jfire.issuetracking.ui.issuelink.IssueLinkAdder;
+
+/**
+ * @author Chairat Kongarayawetchakun - chairat at nightlabs dot de
+ *
+ */
+public class IssueLinkAdderFactory 
+extends AbstractIssueLinkHandlerFactory
+{
+	public IssueLinkAdder createIssueLinkAdder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Class<? extends Object> getLinkObjectClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setEntryType(String entryType) {
+		// TODO Auto-generated method stub
+		
+	}
+}
