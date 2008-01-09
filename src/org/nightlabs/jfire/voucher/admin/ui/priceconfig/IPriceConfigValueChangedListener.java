0fadcab1e07926c4571f935b2ff6fbfeb97bb94c
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-09 11:13:22 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.voucher.admin.ui@9646 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/voucher/admin/ui/priceconfig/IPriceConfigValueChangedListener.java
@@ -0,0 +1,15 @@
+package org.nightlabs.jfire.voucher.admin.ui.priceconfig;
+
+/**
+ * @author fitas [at] NightLabs [dot] de
+ *
+ */
+
+
+public interface IPriceConfigValueChangedListener extends java.util.EventListener {
+
+	  public void priceValueChanged();
+
+	}
+
+
