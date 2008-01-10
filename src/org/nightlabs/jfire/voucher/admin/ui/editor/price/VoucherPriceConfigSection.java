39656b4433f34b1b467a72a1892ce6019034dd32
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-10 06:32:23 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.voucher.admin.ui@9657 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/voucher/admin/ui/editor/price/VoucherPriceConfigSection.java
+++ src/org/nightlabs/jfire/voucher/admin/ui/editor/price/VoucherPriceConfigSection.java
@@ -6,12 +6,16 @@ import java.util.TreeMap;
 
 
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.forms.editor.IFormPage;
 import org.nightlabs.base.ui.editor.ToolBarSectionPart;
+import org.nightlabs.base.ui.resource.SharedImages;
 import org.nightlabs.jfire.accounting.Currency;
+import org.nightlabs.jfire.trade.admin.ui.TradeAdminPlugin;
+import org.nightlabs.jfire.trade.admin.ui.resource.Messages;
 import org.nightlabs.jfire.voucher.accounting.VoucherPriceConfig;
 import org.nightlabs.jfire.voucher.admin.ui.priceconfig.CurrencyAmountTable;
 import org.nightlabs.jfire.voucher.admin.ui.priceconfig.IPriceConfigValueChangedListener;
@@ -36,6 +40,11 @@ extends ToolBarSectionPart
 	 */
 	public VoucherPriceConfigSection(IFormPage page, Composite parent, int style) {
 		super(page, parent, style, "Price Config");
+		
+		AssignNewCurrencyConfigAction assignNewCurrencyConfigAction = new AssignNewCurrencyConfigAction();
+		getToolBarManager().add(assignNewCurrencyConfigAction);
+		
+		
 		currencyAmountTable = new CurrencyAmountTable(getContainer(),false);
 	
 		
@@ -46,6 +55,8 @@ extends ToolBarSectionPart
 			 markDirty();
 			}
 		});
+		
+		updateToolBarManager();
 		
 		
 	}
@@ -58,6 +69,7 @@ extends ToolBarSectionPart
 
 		for (Map.Entry<Currency, Long> me : map.entrySet()) {
 			voucherconfig.setPrice(me.getKey(), me.getValue());
+			
 		}
 	
 	
@@ -68,15 +80,53 @@ extends ToolBarSectionPart
 	public void setVoucherType(VoucherType voucher)
 	{
 		
-	
      voucherconfig = (VoucherPriceConfig) voucher.getPackagePriceConfig();
 
 		Map<Currency, Long> map = new TreeMap<Currency, Long>(voucherconfig.getPrices());
 
 		currencyAmountTable.setMap(map);
 
-	
-	
 	}
 
+	
+	
+	
+	
+	protected void assignCurrencyPressed() 
+	{
+	
+		currencyAmountTable.addCurrency();
+	}
+	
+	
+	class AssignNewCurrencyConfigAction
+	extends Action 
+	{
+		public AssignNewCurrencyConfigAction() {
+			super();
+			setId(AssignNewCurrencyConfigAction.class.getName());
+			//setImageDescriptor(SharedImages.getSharedImageDescriptor(
+				//	TradeAdminPlugin.getDefault(), AssignNewCurrencyConfigAction.class, "AssignPriceConfig")); //$NON-NLS-1$
+			setToolTipText(Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractGridPriceConfigSection.AssignNewPriceConfigAction.toolTipText")); //$NON-NLS-1$
+			setText("Currency");
+		}
+
+		@Override
+		public void run() {
+			//assignNewPressed();
+			assignCurrencyPressed(); 
+			//MessageDialog.openInformation(Display.getCurrent().getActiveShell(), "test", "test");
+
+		}		
+	}
+
+
+
+
+
+
+
+
+
 }
+
