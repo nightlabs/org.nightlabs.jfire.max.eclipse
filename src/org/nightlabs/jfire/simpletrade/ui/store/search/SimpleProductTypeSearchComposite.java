f8ef4d8c91bf1f06b446c3327a6bcbbb09284828
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-11 17:53:40 +0000

    fixed JDODetachFieldAccessException
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.simpletrade.ui@9692 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/simpletrade/ui/store/search/SimpleProductTypeSearchComposite.java
+++ src/org/nightlabs/jfire/simpletrade/ui/store/search/SimpleProductTypeSearchComposite.java
@@ -1,9 +1,13 @@
 package org.nightlabs.jfire.simpletrade.ui.store.search;
 
+import java.util.Set;
+
 import org.eclipse.swt.widgets.Composite;
 import org.nightlabs.jfire.simpletrade.store.search.SimpleProductTypeQuery;
+import org.nightlabs.jfire.store.ProductTypeLocal;
 import org.nightlabs.jfire.store.search.ProductTypeQuery;
 import org.nightlabs.jfire.trade.ui.store.search.AbstractProductTypeSearchComposite;
+import org.nightlabs.util.CollectionUtil;
 
 /**
  * @author Daniel.Mazurek [at] NightLabs [dot] de
@@ -25,4 +29,11 @@ extends AbstractProductTypeSearchComposite
 		return new SimpleProductTypeQuery();
 	}
 
+	@Override
+	protected String[] getFetchGroups()
+	{
+		Set<String> fetchGroups = CollectionUtil.array2HashSet(super.getFetchGroups());
+		fetchGroups.add(ProductTypeLocal.FETCH_GROUP_NESTED_PRODUCT_TYPE_LOCALS);
+		return CollectionUtil.collection2TypedArray(fetchGroups, String.class);
+	}
 }
