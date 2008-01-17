9cb3fb2763f67c342fa3bebc1a45a982fa6396cf
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-17 17:01:20 +0000

    Create new project to bundle parameter ui for trade
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.reporting.trade.ui@9719 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/reporting/trade/ui/accounting/modeofpayment/ValueProviderGUIModeOfPaymentFlavour.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.reporting.trade.ui.accounting.modeofpayment;
+
+import java.util.List;
+
+import javax.jdo.JDOHelper;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.nightlabs.base.ui.job.Job;
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.accounting.dao.ModeOfPaymentFlavourDAO;
+import org.nightlabs.jfire.accounting.pay.ModeOfPaymentFlavour;
+import org.nightlabs.jfire.accounting.pay.id.ModeOfPaymentFlavourID;
+import org.nightlabs.jfire.reporting.parameter.config.ValueProviderConfig;
+import org.nightlabs.jfire.reporting.parameter.id.ValueProviderID;
+import org.nightlabs.jfire.reporting.trade.ReportingTradeConstants;
+import org.nightlabs.jfire.reporting.ui.parameter.AbstractValueProviderGUI;
+import org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI;
+import org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUIFactory;
+import org.nightlabs.jfire.trade.ui.modeofpayment.ModeOfPaymentFlavourTable;
+import org.nightlabs.progress.ProgressMonitor;
+
+/**
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ *
+ */
+public class ValueProviderGUIModeOfPaymentFlavour
+extends AbstractValueProviderGUI<ModeOfPaymentFlavourID>
+{
+	public static class Factory implements IValueProviderGUIFactory {
+		
+		/* (non-Javadoc)
+		 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUIFactory#createValueProviderGUI()
+		 */
+		public IValueProviderGUI<ModeOfPaymentFlavourID> createValueProviderGUI(ValueProviderConfig valueProviderConfig) {
+			return new ValueProviderGUIModeOfPaymentFlavour(valueProviderConfig);
+		}
+
+		/* (non-Javadoc)
+		 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUIFactory#getValueProviderID()
+		 */
+		public ValueProviderID getValueProviderID() {
+			return ReportingTradeConstants.VALUE_PROVIDER_ID_ACCOUNTING_MODE_OF_PAYMENT_FLAVOUR;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime.IConfigurationElement, java.lang.String, java.lang.Object)
+		 */
+		public void setInitializationData(IConfigurationElement arg0, String arg1,
+				Object arg2) throws CoreException {
+		}
+	}
+	
+	private ModeOfPaymentFlavourTable modeOfPaymentFlavourTable;
+	
+	public ValueProviderGUIModeOfPaymentFlavour(ValueProviderConfig valueProviderConfig) {
+		super(valueProviderConfig);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#createGUI(org.eclipse.swt.widgets.Composite)
+	 */
+	public Control createGUI(Composite wrapper) {
+		Group group = new Group(wrapper, SWT.NONE);
+		group.setLayoutData(new GridData(GridData.FILL_BOTH));
+		group.setLayout(new GridLayout());
+		group.setText(getValueProviderConfig().getMessage().getText());
+		
+		modeOfPaymentFlavourTable = new ModeOfPaymentFlavourTable(group);
+		modeOfPaymentFlavourTable.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				notifyOutputChanged();
+			}
+		});
+		
+		modeOfPaymentFlavourTable.setLoadingMessage("Loading...");
+		Job job = new Job("Loading mode of payment flavours") {
+			@Override
+			protected IStatus run(ProgressMonitor monitor) throws Exception {
+				final List<ModeOfPaymentFlavour> flavours = ModeOfPaymentFlavourDAO.sharedInstance().getModeOfPaymentFlavours(
+						ModeOfPaymentFlavourTable.FETCH_GROUPS_MODE_OF_PAYMENT_FLAVOUR, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor);
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						modeOfPaymentFlavourTable.setInput(flavours);
+					}
+				});
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+		return group;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#getOutputValue()
+	 */
+	public ModeOfPaymentFlavourID getOutputValue() {
+		ModeOfPaymentFlavour mopf = modeOfPaymentFlavourTable.getFirstSelectedElement(); 
+		if (mopf != null)
+			return (ModeOfPaymentFlavourID) JDOHelper.getObjectId(mopf);
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#isAcquisitionComplete()
+	 */
+	public boolean isAcquisitionComplete() {
+		return getOutputValue() != null || getValueProviderConfig().isAllowNullOutputValue();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#setInputParameterValue(java.lang.String, java.lang.Object)
+	 */
+	public void setInputParameterValue(String parameterID, final Object value) {
+	}
+
+}
---
+++ src/org/nightlabs/jfire/reporting/trade/ui/accounting/modeofpayment/ValueProviderGUIModeOfPaymentFlavoursByModeOfPayment.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.reporting.trade.ui.accounting.modeofpayment;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.jdo.FetchPlan;
+import javax.jdo.JDOHelper;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.nightlabs.base.ui.job.Job;
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.accounting.dao.ModeOfPaymentDAO;
+import org.nightlabs.jfire.accounting.pay.ModeOfPayment;
+import org.nightlabs.jfire.accounting.pay.ModeOfPaymentFlavour;
+import org.nightlabs.jfire.accounting.pay.id.ModeOfPaymentFlavourID;
+import org.nightlabs.jfire.reporting.parameter.config.ValueProviderConfig;
+import org.nightlabs.jfire.reporting.parameter.id.ValueProviderID;
+import org.nightlabs.jfire.reporting.trade.ReportingTradeConstants;
+import org.nightlabs.jfire.reporting.ui.parameter.AbstractValueProviderGUI;
+import org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI;
+import org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUIFactory;
+import org.nightlabs.jfire.trade.ui.modeofpayment.ModeOfPaymentTable;
+import org.nightlabs.progress.ProgressMonitor;
+
+/**
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ *
+ */
+public class ValueProviderGUIModeOfPaymentFlavoursByModeOfPayment
+extends AbstractValueProviderGUI<Collection<ModeOfPaymentFlavourID>>
+{
+	
+	public static class Factory implements IValueProviderGUIFactory {
+		
+		/* (non-Javadoc)
+		 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUIFactory#createValueProviderGUI()
+		 */
+		public IValueProviderGUI<Collection<ModeOfPaymentFlavourID>> createValueProviderGUI(ValueProviderConfig valueProviderConfig) {
+			return new ValueProviderGUIModeOfPaymentFlavoursByModeOfPayment(valueProviderConfig);
+		}
+
+		/* (non-Javadoc)
+		 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUIFactory#getValueProviderID()
+		 */
+		public ValueProviderID getValueProviderID() {
+			return ReportingTradeConstants.VALUE_PROVIDER_ID_ACCOUNTING_MODE_OF_PAYMENT_FLAVOURS_BY_MODE_OF_PAYMENT;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime.IConfigurationElement, java.lang.String, java.lang.Object)
+		 */
+		public void setInitializationData(IConfigurationElement arg0, String arg1,
+				Object arg2) throws CoreException {
+		}
+	}
+	
+	private ModeOfPaymentTable modeOfPaymentTable;
+	
+	public ValueProviderGUIModeOfPaymentFlavoursByModeOfPayment(ValueProviderConfig valueProviderConfig) {
+		super(valueProviderConfig);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#createGUI(org.eclipse.swt.widgets.Composite)
+	 */
+	public Control createGUI(Composite wrapper) {
+		Group group = new Group(wrapper, SWT.NONE);
+		group.setLayoutData(new GridData(GridData.FILL_BOTH));
+		group.setLayout(new GridLayout());
+		group.setText(getValueProviderConfig().getMessage().getText());
+		
+		modeOfPaymentTable = new ModeOfPaymentTable(group);
+		modeOfPaymentTable.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				notifyOutputChanged();
+			}
+		});
+		
+		modeOfPaymentTable.setLoadingMessage("Loading...");
+		Job job = new Job("Loading modes of payment") {
+			@Override
+			protected IStatus run(ProgressMonitor monitor) throws Exception {
+				final List<ModeOfPayment> mops = ModeOfPaymentDAO.sharedInstance().getModeOfPayments(
+						new String[] {
+								FetchPlan.DEFAULT, ModeOfPayment.FETCH_GROUP_NAME, ModeOfPayment.FETCH_GROUP_FLAVOURS
+						}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor);
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						modeOfPaymentTable.setInput(mops);
+					}
+				});
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+		return group;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#getOutputValue()
+	 */
+	public Collection<ModeOfPaymentFlavourID> getOutputValue() {
+		Collection<ModeOfPayment> mops = modeOfPaymentTable.getSelectedElements();
+		if (mops.size() == 0)
+			return null;
+		List<ModeOfPaymentFlavourID> mopfIDs = new ArrayList<ModeOfPaymentFlavourID>();
+		for (ModeOfPayment modeOfPayment : mops) {
+			for (ModeOfPaymentFlavour mopf : modeOfPayment.getFlavours()) {
+				mopfIDs.add((ModeOfPaymentFlavourID) JDOHelper.getObjectId(mopf));
+			}
+		}
+		return mopfIDs;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#isAcquisitionComplete()
+	 */
+	public boolean isAcquisitionComplete() {
+		return getOutputValue() != null || getValueProviderConfig().isAllowNullOutputValue();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.reporting.ui.parameter.IValueProviderGUI#setInputParameterValue(java.lang.String, java.lang.Object)
+	 */
+	public void setInputParameterValue(String parameterID, final Object value) {
+	}
+
+}
