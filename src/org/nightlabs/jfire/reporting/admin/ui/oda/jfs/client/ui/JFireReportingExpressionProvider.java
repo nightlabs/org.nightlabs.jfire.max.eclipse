9da91fb117e2443d8e1cbe11abcdb978fa6512d3
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-22 17:35:33 +0000

    Contributions to JFireExpressionProvider are now made declarative via extensions, not in code any more
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.reporting.admin.ui@9767 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/admin/ui/oda/jfs/client/ui/JFireReportingExpressionProvider.java
+++ src/org/nightlabs/jfire/reporting/admin/ui/oda/jfs/client/ui/JFireReportingExpressionProvider.java
@@ -3,17 +3,13 @@
  */
 package org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.birt.report.designer.ui.dialogs.IExpressionProvider;
 import org.eclipse.birt.report.designer.ui.dialogs.Operator;
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.swt.graphics.Image;
-import org.nightlabs.jfire.reporting.JFireReportingHelper;
-import org.nightlabs.jfire.reporting.admin.ui.resource.Messages;
+import org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProviderRegistry.ItemCarrier;
 
 /**
  * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
@@ -40,178 +36,39 @@ public class JFireReportingExpressionProvider implements IExpressionProvider {
 		
 	}
 	
-	public static class ItemCarrier {
-		private String itemKey;
-
-		public ItemCarrier(String itemKey) {
-			this.itemKey = itemKey;
-		}
-		
-		public String getItemKey() {
-			return itemKey;
-		}
-		
-		public String getDisplayText() {
-			return identifier2Name.get(itemKey);
-		}
-		
-		public String getInsertText() {
-			return identifier2InsertText.get(itemKey);
-		}
-		
-	}
-	
-	private static Map<String, String> identifier2Name = new HashMap<String, String>();
-	private static Map<String, List<ItemCarrier>> category2SubCategories = new HashMap<String, List<ItemCarrier>>();
-	private static Map<String, List<ItemCarrier>> subCategory2Elements = new HashMap<String, List<ItemCarrier>>();
-	private static Map<String, String> identifier2InsertText = new HashMap<String, String>();
-	
-	private static final String IDENTIFIER_JFIRE_REPORTING = "JFireReporting"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_IMPORTS = "JFireReporting.Imports"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_IMPORTS_HELPER = "JFireReporting.Imports.JFireReportingHelper"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_HELPER_METHODS = "JFireReporting.HelperMethods"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_HELPER_METHODS_DATASET_PARAM = "JFireReporting.HelperMethods.createDataSetParam"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_HELPER_METHODS_PERSISTENCE_MANAGER = "JFireReporting.HelperMethods.getPersistenceManager"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_HELPER_METHODS_VAR = "JFireReporting.HelperMethods.getVar"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_HELPER_METHODS_GET_JDO_OBJECT = "JFireReporting.HelperMethods.getJDOObject"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_HELPER_METHODS_LOGGER = "JFireReporting.HelperMethods.getLogger"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_LOCALISATION = "JFireReporting.Localisatoin"; //$NON-NLS-1$
-	private static final String IDENTIFIER_CATEGORY_LOCALISATION_MESSAGE_IN_DYNAMIC_TEXT = "JFireReporting.Localisatoin.MessageInDynamicText"; //$NON-NLS-1$
-	
-	public static final ItemCarrier ITEM_JFIRE_REPORTING = new ItemCarrier(IDENTIFIER_JFIRE_REPORTING);
-	public static final ItemCarrier CATEGORY_IMPORTS = new ItemCarrier(IDENTIFIER_CATEGORY_IMPORTS);
-	public static final ItemCarrier CATEGORY_IMPORTS_HELPER = new ItemCarrier(IDENTIFIER_CATEGORY_IMPORTS_HELPER);
-	public static final ItemCarrier CATEGORY_HELPER_METHODS = new ItemCarrier(IDENTIFIER_CATEGORY_HELPER_METHODS);
-	public static final ItemCarrier CATEGORY_HELPER_METHODS_DATASET_PARAM = new ItemCarrier(IDENTIFIER_CATEGORY_HELPER_METHODS_DATASET_PARAM);
-	public static final ItemCarrier CATEGORY_HELPER_METHODS_PERSISTENCE_MANAGER = new ItemCarrier(IDENTIFIER_CATEGORY_HELPER_METHODS_PERSISTENCE_MANAGER);
-	public static final ItemCarrier CATEGORY_HELPER_METHODS_VAR = new ItemCarrier(IDENTIFIER_CATEGORY_HELPER_METHODS_VAR);
-	public static final ItemCarrier CATEGORY_HELPER_METHODS_LOGGER = new ItemCarrier(IDENTIFIER_CATEGORY_HELPER_METHODS_LOGGER);
-	public static final ItemCarrier CATEGORY_HELPER_METHODS_GET_JDO_OBJECT = new ItemCarrier(IDENTIFIER_CATEGORY_HELPER_METHODS_GET_JDO_OBJECT);
-	public static final ItemCarrier CATEGORY_LOCALISATION = new ItemCarrier(IDENTIFIER_CATEGORY_LOCALISATION);
-	public static final ItemCarrier CATEGORY_LOCALISATION_MESSAGE_IN_DYNAMIC_TEXT = new ItemCarrier(IDENTIFIER_CATEGORY_LOCALISATION_MESSAGE_IN_DYNAMIC_TEXT);
-	
-	private static boolean indexCreated = false;
-	
-	private synchronized static void createIndex() {
-		if (indexCreated)
-			return;
-		identifier2Name.put(IDENTIFIER_JFIRE_REPORTING, "JFire Reporting"); //$NON-NLS-1$
-		
-		addSubCategories(IDENTIFIER_JFIRE_REPORTING, new ItemCarrier[] {
-				CATEGORY_IMPORTS, CATEGORY_HELPER_METHODS, CATEGORY_LOCALISATION
-			});
-
-		addSubCategoryElements(CATEGORY_IMPORTS, new ItemCarrier[] {
-				CATEGORY_IMPORTS_HELPER
-			});
-		
-		addSubCategoryElements(CATEGORY_HELPER_METHODS, new ItemCarrier[] {
-				CATEGORY_HELPER_METHODS_DATASET_PARAM,
-				CATEGORY_HELPER_METHODS_GET_JDO_OBJECT,
-				CATEGORY_HELPER_METHODS_PERSISTENCE_MANAGER, 
-				CATEGORY_HELPER_METHODS_VAR,
-				CATEGORY_HELPER_METHODS_LOGGER
-			});
-		
-		addSubCategoryElements(CATEGORY_LOCALISATION, new ItemCarrier[] {
-				CATEGORY_LOCALISATION_MESSAGE_IN_DYNAMIC_TEXT
-			});
-		
-		identifier2InsertText.put(CATEGORY_IMPORTS_HELPER.getItemKey(), "importClass(Packages."+JFireReportingHelper.class.getName()+");"); //$NON-NLS-1$ //$NON-NLS-2$
-		
-		identifier2InsertText.put(CATEGORY_HELPER_METHODS_DATASET_PARAM.getItemKey(), "JFireReportingHelper.createDataSetParam()"); //$NON-NLS-1$
-		identifier2InsertText.put(CATEGORY_HELPER_METHODS_PERSISTENCE_MANAGER.getItemKey(), "JFireReportingHelper.getPersistenceManager()"); //$NON-NLS-1$
-		identifier2InsertText.put(CATEGORY_HELPER_METHODS_VAR.getItemKey(), "JFireReportingHelper.getVar()"); //$NON-NLS-1$
-		identifier2InsertText.put(CATEGORY_HELPER_METHODS_GET_JDO_OBJECT.getItemKey(), "JFireReportingHelper.getJDOObject()"); //$NON-NLS-1$
-		identifier2InsertText.put(CATEGORY_HELPER_METHODS_LOGGER.getItemKey(), "JFireReportingHelper.getLogger()"); //$NON-NLS-1$
-		identifier2InsertText.put(CATEGORY_LOCALISATION_MESSAGE_IN_DYNAMIC_TEXT.getItemKey(), "reportContext.getMessage(\"\", reportContext.getLocale())"); //$NON-NLS-1$
-		
-		identifier2Name.put(CATEGORY_IMPORTS.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.import")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_IMPORTS_HELPER.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.reportingHelper")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_HELPER_METHODS.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.helperMethods")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_HELPER_METHODS_DATASET_PARAM.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.createDatasetParameters")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_HELPER_METHODS_GET_JDO_OBJECT.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.resolveJDOObjects")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_HELPER_METHODS_PERSISTENCE_MANAGER.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.getPersistenceManager")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_HELPER_METHODS_VAR.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.getSharedVariables")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_HELPER_METHODS_LOGGER.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.loggerToUse")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_LOCALISATION.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.localisation")); //$NON-NLS-1$
-		identifier2Name.put(CATEGORY_LOCALISATION_MESSAGE_IN_DYNAMIC_TEXT.getItemKey(), Messages.getString("org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui.JFireReportingExpressionProvider.identifier.accessRessource")); //$NON-NLS-1$
-		
-		indexCreated = true;
-	}
-	
-	private static void addSubCategories(String identifier, ItemCarrier[] subCats) {
-		List<ItemCarrier> cats = category2SubCategories.get(identifier);
-		if (cats == null) {
-			cats = new ArrayList<ItemCarrier>(subCats.length);
-			category2SubCategories.put(identifier, cats);
-		}
-		for (ItemCarrier subCat : subCats) {
-			cats.add(subCat);
-			identifier2Name.put(subCat.getItemKey(), null);
-		}
-	}
-	
-	private static void addSubCategoryElements(ItemCarrier category, ItemCarrier[] elements) {
-		List<ItemCarrier> elems = category2SubCategories.get(category);
-		if (elems == null) {
-			elems = new ArrayList<ItemCarrier>(elements.length);
-			subCategory2Elements.put(category.getItemKey(), elems);
-		}
-		for (ItemCarrier element : elements) {
-			elems.add(element);
-			identifier2Name.put(element.getItemKey(), null);
-		}
-	}
-	
-	private static String getIdentifierName(ItemCarrier identifier) {
-		String name = identifier2Name.get(identifier.getItemKey());
-		return name != null ? name : null;
-	}
-	
-	private static String getIdentifierInsertText(ItemCarrier identifier) {
-		String name = identifier2InsertText.get(identifier.getItemKey());
-		return name != null ? name : null;
-	}
-	
 	/**
 	 * 
 	 */
 	public JFireReportingExpressionProvider() {
-		createIndex();
 	}
 
 	public Object[] getCategory() {
-		return new Object[] { ITEM_JFIRE_REPORTING };
+		List<ItemCarrier> topLevelCarriers = JFireReportingExpressionProviderRegistry.sharedInstance().getSubItemsForParentId(null);
+		if (topLevelCarriers != null)
+			return topLevelCarriers.toArray();
+		else 
+			return new Object[0];
 	}
 
 	public Object[] getChildren(Object parent) {
-		if (parent.equals(ITEM_JFIRE_REPORTING)) {
-			return new ArrayList<ItemCarrier>(category2SubCategories.get(IDENTIFIER_JFIRE_REPORTING)).toArray();
-		}
 		if (parent instanceof ItemCarrier) {
-			String parentKey = ((ItemCarrier) parent).getItemKey();
-			for (Map.Entry<String, List<ItemCarrier>> subCatEntry : category2SubCategories.entrySet()) {
-				if (parentKey.equals(subCatEntry.getKey()))
-					return subCatEntry.getValue().toArray();
-			}
-			for (Map.Entry<String, List<ItemCarrier>> subCatElementEntry : subCategory2Elements.entrySet()) {
-				if (parentKey.equals(subCatElementEntry.getKey()))
-					return subCatElementEntry.getValue().toArray();
-			}
+			String parentId = ((ItemCarrier) parent).getItemId();
+			List<ItemCarrier> children = JFireReportingExpressionProviderRegistry.sharedInstance().getSubItemsForParentId(parentId);
+			if (children != null)
+				return children.toArray();
 		}
-		return new Object[] {};
+		return new Object[0];
 	}
 	
 	public String getDisplayText(Object element) {		
 		if (element instanceof ItemCarrier)
-			return getIdentifierName((ItemCarrier) element);
+			return ((ItemCarrier) element).getDisplayText();
 		return null;
 	}
 	
 	public String getInsertText(Object element) {
 		if (element instanceof ItemCarrier)
-			return getIdentifierInsertText((ItemCarrier) element);
+			return ((ItemCarrier) element).getInsertText();
 		return null;
 	}
 
@@ -224,7 +81,10 @@ public class JFireReportingExpressionProvider implements IExpressionProvider {
 	}
 
 	public String getTooltipText(Object element) {
-		return getDisplayText(element);
+		if (element instanceof ItemCarrier) {
+			return ((ItemCarrier) element).getTooltipText();
+		}
+		return null;
 	}
 
 	public boolean hasChildren(Object element) {
---
+++ src/org/nightlabs/jfire/reporting/admin/ui/oda/jfs/client/ui/JFireReportingExpressionProviderRegistry.java
@@ -0,0 +1,158 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.reporting.admin.ui.oda.jfs.client.ui;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.nightlabs.base.ui.extensionpoint.AbstractEPProcessor;
+
+/**
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ *
+ */
+public class JFireReportingExpressionProviderRegistry extends
+		AbstractEPProcessor {
+
+	public static class ItemCarrier {		
+		private String itemId;
+		private String parentId;
+		private String displayText;
+		private String insertText;
+		private String tooltipText;
+		
+		public ItemCarrier(String itemId) {
+			this.itemId = itemId;
+		}
+		
+		public String getItemId() {
+			return itemId;
+		}
+		
+		public String getDisplayText() {
+			return displayText;
+		}
+		
+		public void setDisplayText(String displayText) {
+			this.displayText = displayText;
+		}
+		
+		public String getInsertText() {
+			return insertText;
+		}
+		
+		public void setInsertText(String insertText) {
+			this.insertText = insertText;
+		}
+		
+		public String getParentId() {
+			return parentId;
+		}
+		
+		public void setParentId(String parentId) {
+			this.parentId = parentId;
+		}
+		
+		public String getTooltipText() {
+			return tooltipText;
+		}
+		
+		public void setTooltipText(String tooltipText) {
+			this.tooltipText = tooltipText;
+		}
+	}
+	
+	public static final String EXTENSION_POINT_ID = "org.nightlabs.jfire.reporting.admin.ui.expressionProviderEntry";
+	
+	private static final String ELEMENT_NAME_CATEGORY = "expressionProviderCategory";
+	private static final String ELEMENT_NAME_ENTRY = "expressionProviderEntry";
+
+	private Map<String, ItemCarrier> id2Carrier = new HashMap<String, ItemCarrier>();
+	private Map<String, List<ItemCarrier>> parentId2Carrier = new HashMap<String, List<ItemCarrier>>();
+	
+	
+	/**
+	 * 
+	 */
+	public JFireReportingExpressionProviderRegistry() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.base.ui.extensionpoint.AbstractEPProcessor#getExtensionPointID()
+	 */
+	@Override
+	public String getExtensionPointID() {
+		return EXTENSION_POINT_ID;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.base.ui.extensionpoint.AbstractEPProcessor#processElement(org.eclipse.core.runtime.IExtension, org.eclipse.core.runtime.IConfigurationElement)
+	 */
+	@Override
+	public void processElement(IExtension extension,
+			IConfigurationElement element) throws Exception {
+		if (element.getName().equals(ELEMENT_NAME_CATEGORY)) {
+			ItemCarrier categoryCarrier = new ItemCarrier(element.getAttribute("id"));
+			categoryCarrier.setDisplayText(element.getAttribute("name"));
+			categoryCarrier.setParentId(element.getAttribute("parentCategoryId"));
+			categoryCarrier.setTooltipText(element.getAttribute("tooltip"));
+			id2Carrier.put(categoryCarrier.getItemId(), categoryCarrier);
+		} else if (element.getName().equals(ELEMENT_NAME_ENTRY)) {
+			ItemCarrier entryCarrier = new ItemCarrier(element.getAttribute("id"));
+			entryCarrier.setDisplayText(element.getAttribute("name"));
+			entryCarrier.setInsertText(element.getAttribute("insertText"));
+			entryCarrier.setParentId(element.getAttribute("categoryId"));
+			entryCarrier.setTooltipText(element.getAttribute("tooltip"));
+			id2Carrier.put(entryCarrier.getItemId(), entryCarrier);
+		}
+	}
+	
+	@Override
+	public synchronized void process() {
+		super.process();
+		for (Map.Entry<String, ItemCarrier> entry : id2Carrier.entrySet()) {
+			String parentId = entry.getValue().getParentId();
+			List<ItemCarrier> subItems = parentId2Carrier.get(parentId);
+			if (subItems == null) {
+				subItems = new ArrayList<ItemCarrier>();
+				parentId2Carrier.put(parentId, subItems);
+			}
+			subItems.add(entry.getValue());
+		}
+		for (List<ItemCarrier> subItems : parentId2Carrier.values()) {
+			Collections.sort(subItems, new Comparator<ItemCarrier>() {
+				@Override
+				public int compare(ItemCarrier carrier1, ItemCarrier carrier2) {
+					return carrier1.getDisplayText().compareTo(carrier2.getDisplayText());
+				}
+			});
+		}
+	}
+	
+	
+	public List<ItemCarrier> getSubItemsForParentId(String parentId) {
+		return Collections.unmodifiableList(parentId2Carrier.get(parentId));
+	}
+	
+	private static JFireReportingExpressionProviderRegistry sharedInstance;
+	
+	public static JFireReportingExpressionProviderRegistry sharedInstance() {
+		if (sharedInstance == null) {
+			synchronized (JFireReportingExpressionProviderRegistry.class) {
+				if (sharedInstance == null) {
+					sharedInstance = new JFireReportingExpressionProviderRegistry();
+					sharedInstance.process();
+				}
+			}
+		}
+		return sharedInstance;
+	}
+
+}
