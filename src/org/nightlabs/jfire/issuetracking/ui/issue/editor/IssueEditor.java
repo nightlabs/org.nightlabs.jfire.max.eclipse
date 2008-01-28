6aa0c301c9ef6095e61675bc0a71d86b81ae614a
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-28 11:20:26 +0000

    fixed issue:0000423
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@9786 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssueEditor.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssueEditor.java
@@ -1,9 +1,12 @@
 package org.nightlabs.jfire.issuetracking.ui.issue.editor;
 
 import javax.jdo.FetchPlan;
+import javax.jdo.JDOHelper;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
@@ -11,14 +14,24 @@ import org.eclipse.ui.PartInitException;
 import org.nightlabs.base.ui.entity.editor.EntityEditor;
 import org.nightlabs.base.ui.job.Job;
 import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jdo.ObjectID;
+import org.nightlabs.jfire.base.ui.editlock.EditLockCallback;
+import org.nightlabs.jfire.base.ui.editlock.EditLockCarrier;
+import org.nightlabs.jfire.base.ui.editlock.EditLockHandle;
+import org.nightlabs.jfire.base.ui.editlock.EditLockMan;
+import org.nightlabs.jfire.base.ui.editlock.InactivityAction;
+import org.nightlabs.jfire.issue.EditLockTypeIssue;
 import org.nightlabs.jfire.issue.Issue;
 import org.nightlabs.jfire.issue.IssueSubject;
 import org.nightlabs.jfire.issue.dao.IssueDAO;
+import org.nightlabs.progress.NullProgressMonitor;
 import org.nightlabs.progress.ProgressMonitor;
 
 
 public class IssueEditor extends EntityEditor{
 	public static final String EDITOR_ID = IssueEditor.class.getName();
+	
+	private EditLockHandle editLockHandle;
 	
 	private IssueEditorInput issueEditorInput;
 	@Override
@@ -44,10 +57,26 @@ public class IssueEditor extends EntityEditor{
 						setTitleToolTip(issue.getSubject().getText());
 					}
 				});
+				
+				editLockHandle = EditLockMan.sharedInstance().acquireEditLock(EditLockTypeIssue.EDIT_LOCK_TYPE_ID, (ObjectID)JDOHelper.getObjectId(issue), "TODO", // TODO description //$NON-NLS-1$
+//						null,
+						new EditLockCallback() {
+					@Override
+					public InactivityAction getEditLockAction(EditLockCarrier editLockCarrier) {
+						return InactivityAction.REFRESH_LOCK;
+					}
+				}, getSite().getShell(), new NullProgressMonitor());
+				
 				return Status.OK_STATUS;
 			}
 		};
 		job.setPriority(org.eclipse.core.runtime.jobs.Job.SHORT);
 		job.schedule();
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		editLockHandle.release();
 	}
 }
