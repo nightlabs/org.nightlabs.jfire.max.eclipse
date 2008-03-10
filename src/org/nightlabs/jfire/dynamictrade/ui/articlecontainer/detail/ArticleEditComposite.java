13d45fe33ef8376ea2db9d6f3a2220efccb032dd
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-03-10 15:32:34 +0000

    removed static listeners from RCPUtil and put them into AbstractArticleEdit
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.dynamictrade.ui@10067 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/ui/articlecontainer/detail/ArticleEditComposite.java
+++ src/org/nightlabs/jfire/dynamictrade/ui/articlecontainer/detail/ArticleEditComposite.java
@@ -42,7 +42,6 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.nightlabs.base.ui.composite.XComposite;
-import org.nightlabs.base.ui.util.RCPUtil;
 import org.nightlabs.jfire.trade.Article;
 
 /**
@@ -71,7 +70,7 @@ public class ArticleEditComposite extends XComposite
 		articleTable.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event)
 			{
-				if (!RCPUtil.isKeyDown(SWT.CTRL)) {
+				if (!articleEdit.isCtrlKeyDown()) {
 					ignoreSetSelectedArticles = true;
 					try {
 						articleEdit.getSegmentEdit().setSelectedArticles(EMPTY_SET_ARTICLE);
