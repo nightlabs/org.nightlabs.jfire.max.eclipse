f58ca3cf6f8fbaff1c8ed765d5892b63a06005f8
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-03-14 15:09:02 +0000

    Removed Warnings (added generics where possible)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.dynamictrade.ui@10107 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/ui/quicklist/DynamicProductTypeQuickListFilter.java
+++ src/org/nightlabs/jfire/dynamictrade/ui/quicklist/DynamicProductTypeQuickListFilter.java
@@ -45,8 +45,8 @@ extends AbstractProductTypeQuickListFilter
 	}
 
 	@Override
-	public Set<Class> getClasses() {
-		Set<Class> classes = new HashSet<Class>();
+	public Set<Class<? extends Object>> getClasses() {
+		Set<Class<? extends Object>> classes = new HashSet<Class<? extends Object>>();
 		classes.add(DynamicProductType.class);
 		return classes;
 	}
