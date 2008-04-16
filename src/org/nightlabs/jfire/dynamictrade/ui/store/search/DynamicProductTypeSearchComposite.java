676178d65058da52164ef1b49549be1e43d0805f
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-04-16 14:45:55 +0000

    - removed unnecessary generics from Query-framework - added a lot of javadoc - externalised all string in the UI components
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.dynamictrade.ui@10300 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/ui/store/search/DynamicProductTypeSearchComposite.java
+++ src/org/nightlabs/jfire/dynamictrade/ui/store/search/DynamicProductTypeSearchComposite.java
@@ -1,7 +1,6 @@
 package org.nightlabs.jfire.dynamictrade.ui.store.search;
 
 import org.eclipse.swt.widgets.Composite;
-import org.nightlabs.jfire.dynamictrade.store.DynamicProductType;
 import org.nightlabs.jfire.dynamictrade.store.search.DynamicProductTypeQuery;
 import org.nightlabs.jfire.store.search.AbstractProductTypeQuery;
 import org.nightlabs.jfire.trade.ui.store.search.AbstractProductTypeSearchComposite;
@@ -22,7 +21,8 @@ extends AbstractProductTypeSearchComposite
 	}
 
 	@Override
-	protected AbstractProductTypeQuery<DynamicProductType> createNewQuery() {
+	protected AbstractProductTypeQuery createNewQuery()
+	{
 		return new DynamicProductTypeQuery();
 	}
 
