fb0567e8c315f4fb0a8753943e9081705b068647
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-04-17 06:33:51 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.voucher.ui@10315 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/voucher/ui/quicklist/VoucherTypeQuickListFilter.java
+++ src/org/nightlabs/jfire/voucher/ui/quicklist/VoucherTypeQuickListFilter.java
@@ -9,16 +9,26 @@ import javax.jdo.FetchPlan;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.nightlabs.base.ui.notification.SelectionManager;
 import org.nightlabs.jdo.NLJDOHelper;
 import org.nightlabs.jdo.search.SearchFilter;
 import org.nightlabs.jfire.base.ui.login.Login;
 import org.nightlabs.jfire.store.ProductType;
 import org.nightlabs.jfire.store.StoreManager;
 import org.nightlabs.jfire.store.StoreManagerUtil;
+import org.nightlabs.jfire.trade.ArticleContainer;
+import org.nightlabs.jfire.trade.LegalEntity;
+import org.nightlabs.jfire.trade.dao.ArticleContainerDAO;
+import org.nightlabs.jfire.trade.id.ArticleContainerID;
+import org.nightlabs.jfire.trade.ui.TradePlugin;
 import org.nightlabs.jfire.trade.ui.producttype.quicklist.AbstractProductTypeQuickListFilter;
 import org.nightlabs.jfire.voucher.store.VoucherType;
 import org.nightlabs.jfire.voucher.store.VoucherTypeSearchFilter;
 import org.nightlabs.jfire.voucher.ui.resource.Messages;
+import org.nightlabs.notification.NotificationAdapterWorkerThreadAsync;
+import org.nightlabs.notification.NotificationEvent;
+import org.nightlabs.notification.NotificationListener;
+import org.nightlabs.progress.NullProgressMonitor;
 import org.nightlabs.progress.ProgressMonitor;
 
 public class VoucherTypeQuickListFilter
@@ -27,6 +37,56 @@ extends AbstractProductTypeQuickListFilter
 	public static String[] DEFAULT_FETCH_GROUP = new String[] {
 		FetchPlan.DEFAULT,
 		ProductType.FETCH_GROUP_NAME};
+
+
+	public static String[] FETCH_GROUPS_VENDOR = new String[] {
+		FetchPlan.DEFAULT,
+		ArticleContainer.FETCH_GROUP_VENDOR_ID,
+	};
+
+
+	public VoucherTypeQuickListFilter() {
+		SelectionManager.sharedInstance().addNotificationListener(
+				TradePlugin.ZONE_SALE,
+				ArticleContainer.class, notificationListenerVendorSelected);
+	}
+
+	private NotificationListener notificationListenerVendorSelected = new NotificationAdapterWorkerThreadAsync() {
+		public void notify(NotificationEvent event) {
+
+			ArticleContainer ac = null;
+
+			if (!event.getSubjects().isEmpty())			
+			{			
+				ac = ArticleContainerDAO.sharedInstance().getArticleContainer((ArticleContainerID)event.getFirstSubject(),FETCH_GROUPS_VENDOR, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT,new NullProgressMonitor());
+
+			}
+
+			if(ac == null)	
+				return;
+
+			final VoucherTypeSearchFilter searchFilter = new VoucherTypeSearchFilter(SearchFilter.CONJUNCTION_DEFAULT);
+			searchFilter.setVendorID(ac.getVendorID());
+
+			try {
+				StoreManager storeManager = StoreManagerUtil.getHome(
+						Login.getLogin().getInitialContextProperties()).create();
+				final Collection<ProductType> voucherTypes = storeManager.searchProductTypes(
+						searchFilter, DEFAULT_FETCH_GROUP, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+				Display.getDefault().syncExec(new Runnable() {
+					public void run() {
+						voucherTypeTable.setInput(voucherTypes);
+					}
+				});
+			} catch (Exception x) {
+				throw new RuntimeException(x);
+			}
+
+
+
+		}
+	};
+
 
 	private VoucherTypeTable voucherTypeTable;
 
