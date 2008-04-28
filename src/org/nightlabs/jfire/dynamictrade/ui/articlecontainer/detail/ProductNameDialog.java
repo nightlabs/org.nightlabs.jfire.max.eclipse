002f1767462f4f6f6df339c94ca91103cdd10900
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-04-28 22:33:55 +0000

    Resolved https://www.jfire.org/modules/bugs/view.php?id=533
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.dynamictrade.ui@10410 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/ui/articlecontainer/detail/ProductNameDialog.java
+++ src/org/nightlabs/jfire/dynamictrade/ui/articlecontainer/detail/ProductNameDialog.java
@@ -2,6 +2,7 @@ package org.nightlabs.jfire.dynamictrade.ui.articlecontainer.detail;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
@@ -16,7 +17,7 @@ public class ProductNameDialog
 extends CenteredDialog
 {
 	private I18nText productName;
-	private II18nTextEditor productNameEditor;
+	private I18nTextEditorMultiLine productNameEditor;
 	private boolean editable;
 	
 	public ProductNameDialog(Shell parentShell, I18nText productName, boolean editable)
@@ -41,6 +42,7 @@ extends CenteredDialog
 		this.productNameEditor = new I18nTextEditorMultiLine(area);
 		this.productNameEditor.setEditable(editable);
 		this.productNameEditor.setI18nText(productName, EditMode.BUFFERED);
+		this.productNameEditor.setLayoutData(new GridData(GridData.FILL_BOTH));
 		return area;
 	}
 
