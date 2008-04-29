8a079896f650b87435b8af6b8a3a32d960e134e0
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-04-29 04:09:43 +0000

    SelectIssueAndIssueLinkTypePage.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@10411 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issuelink/attach/AttachIssueToObjectWizard.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issuelink/attach/AttachIssueToObjectWizard.java
@@ -1,18 +1,23 @@
 package org.nightlabs.jfire.issuetracking.ui.issuelink.attach;
 
 import org.nightlabs.base.ui.wizard.DynamicPathWizard;
-import org.nightlabs.jfire.issue.Issue;
-import org.nightlabs.jfire.issuetracking.ui.issue.IssueLinkTable;
+import org.nightlabs.jdo.ObjectID;
 
 public class AttachIssueToObjectWizard 
 extends DynamicPathWizard
 {
-	public AttachIssueToObjectWizard(IssueLinkTable issueLinkTable, Issue issue) {
+	private ObjectID objectID;
+	private SelectIssueAndIssueLinkTypePage selectIssueAndIssueLinkTypePage;
+	
+	public AttachIssueToObjectWizard(ObjectID objectID) {
+		this.objectID = objectID;
 		setWindowTitle("Attach Issue(s) to Objects");
 	}
 
 	@Override
 	public void addPages() {
+		selectIssueAndIssueLinkTypePage = new SelectIssueAndIssueLinkTypePage();
+		addPage(selectIssueAndIssueLinkTypePage);
 	}
 
 	@Override
---
+++ src/org/nightlabs/jfire/issuetracking/ui/issuelink/attach/SelectIssueAndIssueLinkTypePage.java
@@ -0,0 +1,177 @@
+package org.nightlabs.jfire.issuetracking.ui.issuelink.attach;
+
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.nightlabs.base.ui.composite.ListComposite;
+import org.nightlabs.base.ui.composite.XComposite;
+import org.nightlabs.base.ui.language.I18nTextEditor;
+import org.nightlabs.base.ui.table.AbstractTableComposite;
+import org.nightlabs.base.ui.wizard.DynamicPathWizardPage;
+import org.nightlabs.i18n.I18nTextBuffer;
+import org.nightlabs.jfire.issue.IssueLinkType;
+import org.nightlabs.jfire.issuetracking.ui.overview.IssueEntryListFactory;
+import org.nightlabs.jfire.issuetracking.ui.overview.IssueEntryListViewer;
+
+public class SelectIssueAndIssueLinkTypePage 
+extends DynamicPathWizardPage
+{
+	private Button createNewIssueLinkTypeRadio;
+	private Button selectExistingIssueLinkTypeRadio;
+	
+	private I18nTextEditor newIssueLinkTypeNameEditor;
+	private I18nTextBuffer newIssueLinkTypeName = new I18nTextBuffer();
+	
+	private ListComposite<IssueLinkType> issueLinkTypeList;
+	private IssueLinkType selectedIssueLinkType;
+	
+	public static enum Action {
+		createNewIssueLinkType,
+		selectExistingIssueLinkType
+	}
+
+	private Action action;
+
+	public Action getAction() {
+		return action;
+	}
+	public void setAction(Action action) {
+		this.action = action;
+		createNewIssueLinkTypeRadio.setSelection(false);
+		selectExistingIssueLinkTypeRadio.setSelection(false);
+
+		switch (action) {
+			case createNewIssueLinkType:
+				createNewIssueLinkTypeRadio.setSelection(true);
+				break;
+			case selectExistingIssueLinkType:
+				selectExistingIssueLinkTypeRadio.setSelection(true);
+				break;
+			default:
+				throw new IllegalStateException("Unknown action: " + action);
+		}
+
+		getContainer().updateButtons();
+	}
+	
+	public SelectIssueAndIssueLinkTypePage() {
+		super(SelectIssueAndIssueLinkTypePage.class.getName());
+		setDescription("");
+	}
+
+	@Override
+	public Control createPageContents(Composite parent) {
+		XComposite mainComposite = new XComposite(parent, SWT.NONE);
+		mainComposite.getGridLayout().numColumns = 1;
+
+		createNewIssueLinkTypeRadio = new Button(mainComposite, SWT.RADIO);		
+		createNewIssueLinkTypeRadio.setText("Create a new relation");
+		createNewIssueLinkTypeRadio.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		createNewIssueLinkTypeRadio.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				setAction(Action.createNewIssueLinkType);
+			}
+		});
+
+		newIssueLinkTypeNameEditor = new I18nTextEditor(mainComposite);
+		newIssueLinkTypeNameEditor.setI18nText(newIssueLinkTypeName);
+		newIssueLinkTypeNameEditor.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		newIssueLinkTypeNameEditor.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				setAction(Action.createNewIssueLinkType);
+			}
+		});
+
+		selectExistingIssueLinkTypeRadio = new Button(mainComposite, SWT.RADIO);
+		selectExistingIssueLinkTypeRadio.setText("Select a relation from the list");
+		selectExistingIssueLinkTypeRadio.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		selectExistingIssueLinkTypeRadio.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				setAction(Action.selectExistingIssueLinkType);
+			}
+		});
+
+		Group manageRelationGroup = new Group(mainComposite, SWT.NONE);
+		manageRelationGroup.setLayout(new GridLayout(1, false));
+		manageRelationGroup.setText("Predefined Relations");
+		manageRelationGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		XComposite manageComposite = new XComposite(manageRelationGroup, SWT.NONE);
+		manageComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		manageComposite.getGridLayout().numColumns = 1;
+		
+		issueLinkTypeList = new ListComposite<IssueLinkType>(manageComposite, SWT.SINGLE);
+		issueLinkTypeList.setLabelProvider(new LabelProvider() {
+			@Override
+			public String getText(Object element) {
+				return ((IssueLinkType)element).getName().getText();
+			}
+		});
+		GridData gridData = new GridData(GridData.FILL_BOTH);
+		issueLinkTypeList.setLayoutData(gridData);
+		issueLinkTypeList.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				selectedIssueLinkType = issueLinkTypeList.getSelectedElement();
+				setAction(Action.selectExistingIssueLinkType);
+			}
+		});
+
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				setAction(Action.selectExistingIssueLinkType);
+			}
+		});
+		
+		IssueEntryListViewer iViewer = new IssueEntryListViewer(new IssueEntryListFactory().createEntry()) {
+			@Override
+			protected void addResultTableListeners(AbstractTableComposite tableComposite) {
+				tableComposite.addDoubleClickListener(new IDoubleClickListener() {
+					@Override
+					public void doubleClick(DoubleClickEvent evt) {
+					}
+				});
+				
+				tableComposite.addSelectionChangedListener(new ISelectionChangedListener() {
+					public void selectionChanged(SelectionChangedEvent e) {
+					}
+				});
+			}
+		};	
+		
+		iViewer.createComposite(mainComposite);
+		iViewer.getComposite().setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		return mainComposite;
+	}
+
+	@Override
+	public boolean isPageComplete() {
+//		if (createNewIssueLinkTypeRadio == null) // check if UI is already created
+//			return false;
+//
+//		switch (action) {
+//			case createNewIssueLinkType:
+//				return !newIssueLinkTypeName.isEmpty();
+//			case selectExistingIssueLinkType:
+//				return selectedIssueLinkType != null;
+//			default:
+//				throw new IllegalStateException("Unknown action: " + action);
+		return true;
+		}
+}
