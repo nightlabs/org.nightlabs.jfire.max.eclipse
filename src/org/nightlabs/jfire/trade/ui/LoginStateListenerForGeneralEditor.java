05f0baec82a3b2bba683ef259741429899c6a616
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-05-07 11:31:36 +0000

    added a new listener for the extension point logistate to close all open editors upon log out
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@10490 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/trade/ui/LoginStateListenerForGeneralEditor.java
@@ -0,0 +1,61 @@
+package org.nightlabs.jfire.trade.ui;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.nightlabs.jfire.base.ui.login.Login;
+import org.nightlabs.jfire.base.ui.login.LoginStateListener;
+import org.nightlabs.jfire.trade.ui.articlecontainer.detail.GeneralEditor;
+
+/**
+ * @author Fitas Amine - fitas at nightlabs dot de
+ */
+
+public class LoginStateListenerForGeneralEditor
+implements LoginStateListener
+{
+
+	@Override
+	public void loginStateChanged(int loginState, IAction action)
+	{		
+		// TODO: is also called at application shutdown (e.g. classloading configuration has changed)
+		// and leads to the fact that the AbstractApplication$ExitThread kills the app after 60s
+		// and therefore the application do NOT restarts.
+		if (loginState == Login.LOGINSTATE_OFFLINE)
+			closeAllEditors();
+		
+	}
+
+
+	private void closeAllEditors()
+	{
+//		Display.getDefault().syncExec(new Runnable()
+//		{
+//			public void run()
+//			{
+
+				IWorkbench wb = PlatformUI.getWorkbench();
+				IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+				IWorkbenchPage page = win.getActivePage();
+				IEditorReference[] references = page.getEditorReferences();
+
+				for (IEditorReference reference : references) {
+					IEditorPart editor = reference.getEditor(false);
+					if (editor instanceof GeneralEditor) {
+
+						if(editor != null)
+							page.closeEditor(editor, true);
+					}
+
+				}
+//
+//			}
+//		});
+	}
+
+
+}
