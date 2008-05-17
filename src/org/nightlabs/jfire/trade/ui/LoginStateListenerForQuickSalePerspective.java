f1ba1d0b02bd47d132bc4ff8e6e1cb00b09f62a1
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-05-17 13:23:19 +0000

    solved the bug that this waited forever and preventing the shutdown until our ExitThread enforces shutdown
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@10566 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/LoginStateListenerForQuickSalePerspective.java
+++ src/org/nightlabs/jfire/trade/ui/LoginStateListenerForQuickSalePerspective.java
@@ -1,23 +1,18 @@
 package org.nightlabs.jfire.trade.ui;
 
 import org.apache.log4j.Logger;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.swt.widgets.Display;
 import org.nightlabs.base.ui.login.LoginState;
 import org.nightlabs.base.ui.util.RCPUtil;
-import org.nightlabs.jfire.base.ui.login.Login;
 import org.nightlabs.jfire.base.ui.login.LoginStateChangeEvent;
 import org.nightlabs.jfire.base.ui.login.LoginStateListener;
 
 public class LoginStateListenerForQuickSalePerspective
 implements LoginStateListener
-{
+{	
 	@Override
 	public void afterLoginStateChange(LoginStateChangeEvent event)
 	{		
-		// TODO: is also called at application shutdown (e.g. classloading configuration has changed)
-		// and leads to the fact that the AbstractApplication$ExitThread kills the app after 60s
-		// and therefore the application do NOT restarts.
 		if (event.getNewLoginState() == LoginState.LOGGED_IN)
 			checkOrderOpenAsynchronously();
 	}
@@ -28,17 +23,28 @@ implements LoginStateListener
 		{
 			public void run()
 			{
-				Logger logger = Logger.getLogger(LoginStateListenerForQuickSalePerspective.class);
+				final Logger logger = Logger.getLogger(LoginStateListenerForQuickSalePerspective.class);
 
 				String activePerspectiveID = RCPUtil.getActivePerspectiveID();
 				if (activePerspectiveID == null) {
-					logger.info("activePerspectiveID is null. Will re-enqueue this method into the event dispatcher and exit."); //$NON-NLS-1$
-					try {
-						Thread.sleep(500);
-					} catch (InterruptedException e) {
-						// ignore
-					}
-					checkOrderOpenAsynchronously();
+					// This Thread is necessary in order to prevent blocking the application from shutdown.
+					// It might happen, that the application never has an active perspective, because it is shut down before
+					// completely starting (e.g. when the classloader-config changed and the login occurs very early
+					// and decides to restart the application.
+					Thread thread = new Thread() {
+						@Override
+						public void run() {
+							logger.info("activePerspectiveID is null. Will re-enqueue this method into the event dispatcher and exit."); //$NON-NLS-1$
+							try {
+								Thread.sleep(500);
+							} catch (InterruptedException e) {
+								// ignore
+							}
+							checkOrderOpenAsynchronously();
+						}
+					};
+					thread.setDaemon(true);
+					thread.start();
 					return;
 				}
 
@@ -48,10 +54,8 @@ implements LoginStateListener
 		});
 	}
 
-
 	@Override
 	public void beforeLoginStateChange(LoginStateChangeEvent event) {
-		// TODO Auto-generated method stub
-		
+		// nothing to do here
 	}
 }
