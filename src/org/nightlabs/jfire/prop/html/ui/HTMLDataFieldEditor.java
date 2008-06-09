364038f4196aff01179dc9f04a015f1401771eaa
marc <marc@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-09 14:27:47 +0000

    minimal ui
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.prop.html.ui@10761 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/prop/html/ui/HTMLDataFieldEditor.java
+++ src/org/nightlabs/jfire/prop/html/ui/HTMLDataFieldEditor.java
@@ -1,9 +1,17 @@
 package org.nightlabs.jfire.prop.html.ui;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.eclipse.ui.fckeditor.FCKEditorInput;
+import org.nightlabs.eclipse.ui.fckeditor.IFCKEditorInput;
+import org.nightlabs.eclipse.ui.fckeditor.test.TestUtil;
 import org.nightlabs.jfire.base.ui.prop.edit.AbstractDataFieldEditor;
 import org.nightlabs.jfire.prop.IStruct;
 import org.nightlabs.jfire.prop.html.HTMLDataField;
@@ -25,9 +33,36 @@ public class HTMLDataFieldEditor extends AbstractDataFieldEditor<HTMLDataField>
 	@Override
 	public Control createControl(Composite parent)
 	{
-		Label l = new Label(parent, SWT.NONE);
-		l.setText("My data field editor");
+		Label l = new Label(parent, SWT.WRAP);
+		if(!getDataField().isEmpty())
+			l.setText(getDataField().getHtml());
+		else
+			l.setText("No content.");
 		this.control = l;
+
+		Label l2 = new Label(parent, SWT.WRAP);
+		int count = getDataField().getFiles() == null ? 0 : getDataField().getFiles().size();
+		l2.setText(String.format("%d files", count));
+		
+		final Button b = new Button(parent, SWT.PUSH);
+		b.setText("Edit...");
+		b.addSelectionListener(new SelectionAdapter() {
+			/* (non-Javadoc)
+			 * @see org.eclipse.swt.events.SelectionAdapter#widgetSelected(org.eclipse.swt.events.SelectionEvent)
+			 */
+			@Override
+			public void widgetSelected(SelectionEvent event)
+			{
+				try {
+					IFCKEditorInput editorInput = new FCKEditorInput(getDataField(), getDataField().getStructFieldID());
+					RCPUtil.getActiveWorkbenchPage().openEditor(editorInput, "org.nightlabs.eclipse.ui.fckeditor.FCKEditor");
+				} catch (Throwable e) {
+					MessageDialog.openError(b.getShell(), "Error", "Error: "+e.toString());
+					e.printStackTrace();
+				}
+			}
+		});
+		
 		return l;
 	}
 
