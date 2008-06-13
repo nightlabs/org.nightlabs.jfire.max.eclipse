c5dae51cc7e102dbef0d98109219bd62b13808cb
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-13 09:55:41 +0000

    adopted ExceptionHandler to the new ExceptionHandlerParam
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@10849 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/exceptionhandler/NotAvailableExceptionHandler.java
+++ src/org/nightlabs/jfire/trade/ui/exceptionhandler/NotAvailableExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.nightlabs.jfire.trade.ui.exceptionhandler;
 
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.nightlabs.base.ui.exceptionhandler.ExceptionHandlerParam;
 import org.nightlabs.base.ui.exceptionhandler.IExceptionHandler;
 import org.nightlabs.base.ui.util.RCPUtil;
 import org.nightlabs.jfire.trade.ui.resource.Messages;
@@ -8,9 +9,8 @@ import org.nightlabs.jfire.trade.ui.resource.Messages;
 public class NotAvailableExceptionHandler
 		implements IExceptionHandler
 {
-
 	@Override
-	public boolean handleException(Thread thread, Throwable thrownException, Throwable triggerException)
+	public boolean handleException(ExceptionHandlerParam handlerParam)
 	{
 		MessageDialog.openError(
 				RCPUtil.getActiveShell(),
@@ -18,5 +18,4 @@ public class NotAvailableExceptionHandler
 				Messages.getString("org.nightlabs.jfire.trade.ui.exceptionhandler.NotAvailableExceptionHandler.dialog.message")); //$NON-NLS-1$
 		return true;
 	}
-
 }
