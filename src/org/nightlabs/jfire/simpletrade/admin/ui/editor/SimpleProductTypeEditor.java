82550725b184623764d51026ea99c25a913bd6dc
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-24 13:09:56 +0000

    Added GenericProductTypeSearchAdminActionHandler who works with IEditorMatchingStrategies. Added therefore also AbstractProductTypeAdminEditorMatchingStrategy and the implementations for all ProductTypes
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.simpletrade.admin.ui@11040 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/simpletrade/admin/ui/editor/SimpleProductTypeEditor.java
+++ src/org/nightlabs/jfire/simpletrade/admin/ui/editor/SimpleProductTypeEditor.java
@@ -1,5 +1,7 @@
 package org.nightlabs.jfire.simpletrade.admin.ui.editor;
 
+import org.nightlabs.jfire.simpletrade.store.SimpleProductType;
+import org.nightlabs.jfire.store.ProductType;
 import org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor;
 
 
@@ -10,7 +12,13 @@ public class SimpleProductTypeEditor
 extends AbstractProductTypeAdminEditor
 {
 	public static final String ID_EDITOR = SimpleProductTypeEditor.class.getName();
-	
-	public SimpleProductTypeEditor() { }
 
+//	/* (non-Javadoc)
+//	 * @see org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor#getProductTypeClass()
+//	 */
+//	@Override
+//	public Class<? extends ProductType> getProductTypeClass() {
+//		return SimpleProductType.class;
+//	}
+	
 }
