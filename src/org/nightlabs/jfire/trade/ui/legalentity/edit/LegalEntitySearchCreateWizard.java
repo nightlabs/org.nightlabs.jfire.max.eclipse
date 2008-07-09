0b0fd176175447981622bc3828a96bd7ccb72e93
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-09 16:56:11 +0000

    added allowEditLegalEntity constructor parameter to PersonSearchWizardPage and LegalEntitySearchCreateWizard.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@11357 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/legalentity/edit/LegalEntitySearchCreateWizard.java
+++ src/org/nightlabs/jfire/trade/ui/legalentity/edit/LegalEntitySearchCreateWizard.java
@@ -23,15 +23,26 @@ public class LegalEntitySearchCreateWizard extends DynamicPathWizard {
 
 	private PersonSearchWizardPage personSearchWizardPage;
 	private LegalEntity legalEntity;
-	/**
-	 * 
-	 */
-	public LegalEntitySearchCreateWizard(String quickSearchText, boolean allowNewLegalEntityCreation) {
-		personSearchWizardPage = new PersonSearchWizardPage(quickSearchText);
+	
+	public LegalEntitySearchCreateWizard(String quickSearchText, 
+			boolean allowNewLegalEntityCreation, boolean allowEditLegalEnity) 
+	{
+		personSearchWizardPage = new PersonSearchWizardPage(quickSearchText, allowNewLegalEntityCreation, allowEditLegalEnity);
 		addPage(personSearchWizardPage);
 		setWindowTitle(Messages.getString("org.nightlabs.jfire.trade.ui.legalentity.edit.LegalEntitySearchCreateWizard.windowsTitle")); //$NON-NLS-1$
 	}
 
+	public LegalEntitySearchCreateWizard(String quickSearchText) 
+	{
+		this(quickSearchText, true, false);
+	}
+
+	public LegalEntitySearchCreateWizard(String quickSearchText, boolean allowNewLegalEntityCreation) 
+	{
+		this(quickSearchText, allowNewLegalEntityCreation, false);
+	}
+	
+	
 	/** {@inheritDoc}
 	 */
 	@Override
@@ -66,7 +77,21 @@ public class LegalEntitySearchCreateWizard extends DynamicPathWizard {
 	 * @return The found or newly created {@link LegalEntity}.
 	 */
 	public static LegalEntity open(String quickSearchText, boolean allowNewLegalEntityCreation) {
-		LegalEntitySearchCreateWizard wiz = new LegalEntitySearchCreateWizard(quickSearchText, allowNewLegalEntityCreation);
+		return open(quickSearchText, allowNewLegalEntityCreation, false);
+	}
+
+	/**
+	 * Opens the wizard and returns the {@link LegalEntity}
+	 * the user found or created.
+	 * 
+	 * @param quickSearchText The text to search for instantly.
+	 * @param allowNewLegalEntityCreation Whether the user should be able to create a new Person/LegalEntity in the wizard.
+	 * @param allowEditLegalEntity Whether the user should be able to edit a selected person in the wizard
+	 * @return The found or newly created {@link LegalEntity}.
+	 */	
+	public static LegalEntity open(String quickSearchText, boolean allowNewLegalEntityCreation, boolean allowEditLegalEntity) 
+	{
+		LegalEntitySearchCreateWizard wiz = new LegalEntitySearchCreateWizard(quickSearchText, allowNewLegalEntityCreation, allowEditLegalEntity);
 		DynamicPathWizardDialog dlg = new DynamicPathWizardDialog(wiz);
 		if (dlg.open() == Window.OK) {
 			return wiz.getLegalEntity();
