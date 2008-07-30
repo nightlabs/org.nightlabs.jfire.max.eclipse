ba22a593cda8c4961415cb9e3deacd21a7df7baa
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-30 10:53:39 +0000

    IssueWorkTimeRange & its UIs
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@11567 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssueWorkTimeSection.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssueWorkTimeSection.java
@@ -1,18 +1,23 @@
 package org.nightlabs.jfire.issuetracking.ui.issue.editor;
 
+import java.awt.GridLayout;
 import java.text.DateFormat;
+import java.util.Collection;
 import java.util.Date;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.nightlabs.jfire.base.ui.security.UserSearchDialog;
 import org.nightlabs.jfire.issue.Issue;
+import org.nightlabs.jfire.issue.IssueWorkTimeRange;
+import org.nightlabs.util.CollectionUtil;
 
 /**
  * @author Chairat Kongarayawetchakun <!-- chairat [AT] nightlabs [DOT] de -->
@@ -22,13 +27,15 @@ public class IssueWorkTimeSection
 extends AbstractIssueEditorGeneralSection 
 {
 	private Issue issue;
-	
-	private Label startTimeLabel;
+
+	private Label statusLabel;
 	private Button startStopButton;
-	
-	
+
+	private Label startTimeLabel;
+	private Label endTimeLabel;
+
 	private static DateFormat dateTimeFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
-	
+
 	/**
 	 * @param section
 	 * @param managedForm
@@ -38,22 +45,23 @@ extends AbstractIssueEditorGeneralSection
 		getClient().getGridLayout().numColumns = 3;
 		getClient().getGridLayout().makeColumnsEqualWidth = false;
 		getSection().setText("Work Time");
-		
+
 		new Label(getClient(), SWT.NONE).setText("Status: ");
-				
-		startTimeLabel = new Label(getClient(), SWT.NONE);
-		startTimeLabel.setText(" - ");
-		
+
+		statusLabel = new Label(getClient(), SWT.NONE);
+		statusLabel.setText(" - ");
+
 		startStopButton = new Button(getClient(), SWT.NONE);
 		startStopButton.setText("");
 		startStopButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent se) {
-				if (issue.isStarted()) {
+				if (issue.isStarted()) { //End
 					issue.endWorking(new Date());
+					markDirty();
 				}
-				
-				else {
+
+				else {	//Start
 					if (issue.getAssignee() == null) {
 						UserSearchDialog userSearchDialog = new UserSearchDialog(getSection().getShell(), null);
 						int returnCode = userSearchDialog.open();
@@ -61,24 +69,43 @@ extends AbstractIssueEditorGeneralSection
 							issue.setAssignee(userSearchDialog.getSelectedUser());
 						}
 					}
+					
 					issue.startWorking(new Date());
+					markDirty();
 				}
-				markDirty();
 			}
 		});
-		
-		new Label(getClient(), SWT.NONE).setText("Time: ");
-		Label finishTimeLabel = new Label(getClient(), SWT.NONE);
-		finishTimeLabel.setText("");
+
+		new Label(getClient(), SWT.NONE).setText("Start Time: ");
+		startTimeLabel = new Label(getClient(), SWT.NONE);
+		startTimeLabel.setText("");
+		GridData gd = new GridData();
+		gd.horizontalSpan = 2;
+		startTimeLabel.setLayoutData(gd);
+
+		new Label(getClient(), SWT.NONE).setText("End Time: ");
+		endTimeLabel = new Label(getClient(), SWT.NONE);
+		endTimeLabel.setText("");
+		gd = new GridData();
+		gd.horizontalSpan = 2;
+		endTimeLabel.setLayoutData(gd);
 	}
 
 	@Override
 	protected void doSetIssue(Issue newIssue) {
 		this.issue = newIssue;
-		
+
 		if (issue.isStarted()) 
 			startStopButton.setText("Stop");
 		else 
 			startStopButton.setText("Start");
+
+		IssueWorkTimeRange workTime = issue.getLastestIssueWorkTimeRange();
+		if (workTime != null) {
+			statusLabel.setText(issue.isStarted() ? "Working" : "Finished");
+			startTimeLabel.setText(workTime.getFrom() == null? "" : dateTimeFormat.format(workTime.getFrom()));
+			endTimeLabel.setText(workTime.getTo() == null? "" : dateTimeFormat.format(workTime.getTo()));
+			getClient().pack(true);
+		}
 	}
 }
