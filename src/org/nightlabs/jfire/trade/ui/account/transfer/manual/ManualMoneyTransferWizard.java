c9980f071ab416c07b315c3c166c58219f81d24a
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-01 10:22:47 +0000

    https://www.jfire.org/modules/bugs/view.php?id=661
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@11603 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/account/transfer/manual/ManualMoneyTransferWizard.java
+++ src/org/nightlabs/jfire/trade/ui/account/transfer/manual/ManualMoneyTransferWizard.java
@@ -1,5 +1,12 @@
 package org.nightlabs.jfire.trade.ui.account.transfer.manual;
 
+import java.lang.reflect.InvocationTargetException;
+import java.rmi.RemoteException;
+
+import javax.jdo.FetchPlan;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.nightlabs.base.ui.wizard.DynamicPathWizard;
 import org.nightlabs.i18n.I18nText;
 import org.nightlabs.jdo.NLJDOHelper;
@@ -14,6 +21,7 @@ import org.nightlabs.jfire.base.ui.login.Login;
 import org.nightlabs.jfire.trade.ui.resource.Messages;
 import org.nightlabs.jfire.transfer.Transfer;
 import org.nightlabs.jfire.transfer.id.AnchorID;
+import org.nightlabs.progress.NullProgressMonitor;
 
 /**
  * @author Chairat Kongarayawetchakun - chairatk[at]nightlabs[dot]de
@@ -24,7 +32,7 @@ public class ManualMoneyTransferWizard extends DynamicPathWizard{
 	private AccountChooserWizardPage toAccountChooserPage;
 	private ManualMoneyTransferWizardPage moneyTransferPage;
 	private AnchorID selectedAccountAnchorID;
-	
+
 	public ManualMoneyTransferWizard(AnchorID selectedAccountAnchorID){
 		setWindowTitle(Messages.getString("org.nightlabs.jfire.trade.ui.account.transfer.manual.ManualMoneyTransferWizard.windowTitle")); //$NON-NLS-1$
 		this.selectedAccountAnchorID = selectedAccountAnchorID;
@@ -41,60 +49,70 @@ public class ManualMoneyTransferWizard extends DynamicPathWizard{
 			fromAccountChooserPage.setSelectedAccount(selectedAccountAnchorID);
 			addPage(fromAccountChooserPage);
 		}//if
-		
+
 		toAccountChooserPage = new AccountChooserWizardPage();
 		toAccountChooserPage.setTitle(Messages.getString("org.nightlabs.jfire.trade.ui.account.transfer.manual.ManualMoneyTransferWizard.toAccountChooserPageTitle")); //$NON-NLS-1$
 		addPage(toAccountChooserPage);
-		
+
 		moneyTransferPage = new ManualMoneyTransferWizardPage();
 		addPage(moneyTransferPage);
 	}
 
+	private ManualMoneyTransfer moneyTransfer = null;
+
 	@Override
 	public boolean performFinish() {
-		ManualMoneyTransfer moneyTransfer = null;
 		try {
-			AccountingManagerHome accountingManagerHome = AccountingManagerUtil.getHome(Login.getLogin().getInitialContextProperties());
-			AccountingManager accountingManager = accountingManagerHome.create();
-			
 			if(fromAccountChooserPage != null){
 				selectedAccountAnchorID = fromAccountChooserPage.getSelectedAccount();
 			}//if
-			
-			AnchorID toAccountID = moneyTransferPage.getToAccount();
-			
-			I18nText reason = moneyTransferPage.getReason();
-			
-			Account fromAccount = AccountDAO.sharedInstance().getAccount(moneyTransferPage.getFromAccount(), new String[]{Account.FETCH_GROUP_THIS_ACCOUNT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, null);
-			moneyTransfer = accountingManager.createManualMoneyTransfer(moneyTransferPage.getFromAccount(),
-					toAccountID,
-					CurrencyID.create(fromAccount.getCurrency().getCurrencyID()),
-					moneyTransferPage.getAmount(),
-					reason,
-					true,
-					new String[]{Transfer.FETCH_GROUP_THIS_TRANSFER},
-					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+
+
+			getContainer().run(false, false, new IRunnableWithProgress() {
+				public void run(IProgressMonitor _monitor) throws InvocationTargetException, InterruptedException {
+					try {
+						AccountingManagerHome accountingManagerHome = AccountingManagerUtil.getHome(Login.getLogin().getInitialContextProperties());
+						AccountingManager accountingManager = accountingManagerHome.create();
+
+						AnchorID toAccountID = moneyTransferPage.getToAccount();
+						I18nText reason = moneyTransferPage.getReason();
+
+						Account fromAccount = AccountDAO.sharedInstance().getAccount(moneyTransferPage.getFromAccount(), new String[]{Account.FETCH_GROUP_CURRENCY}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new NullProgressMonitor());
+						moneyTransfer = accountingManager.createManualMoneyTransfer(moneyTransferPage.getFromAccount(),
+								toAccountID,
+								CurrencyID.create(fromAccount.getCurrency().getCurrencyID()),
+								moneyTransferPage.getAmount(),
+								reason,
+								true,
+								new String[]{FetchPlan.DEFAULT},
+								NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				}
+			});
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 		return moneyTransfer == null? false:true;
 	}
-	
+
 	@Override
 	public boolean canFinish() {
 		if(selectedAccountAnchorID == null || toAccountChooserPage.getSelectedAccount() == null || moneyTransferPage.getAmount() <= 0)
 			return false;
 		return true;
 	}
-	
+
 	public AccountChooserWizardPage getFromAccountChooserPage() {
 		return fromAccountChooserPage;
 	}
-	
+
 	public AccountChooserWizardPage getToAccountChooserPage() {
 		return toAccountChooserPage;
 	}
-	
+
 	public AnchorID getSelectedAccountAnchorID() {
 		return selectedAccountAnchorID;
 	}
