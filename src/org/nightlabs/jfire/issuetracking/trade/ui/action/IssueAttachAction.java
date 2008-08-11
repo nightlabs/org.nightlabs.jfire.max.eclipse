04a36b6100cb9455cc2325bd0fe47bc78720d6c6
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-11 11:46:12 +0000

    -fixed bug #0000526: UI layout problems when trying to attach an order  -> added async Runnable that retrieved the correct parent size and resets the sashweights.  -> the WizardPage had a layout problem (the icon in the topright wasn't correctly displayed -> the header of the TrayIconDialog could be collapsed) --> added a layout(true,true) after creating the content.
    -The Login dialog forced the user to write in the field corresponding to the one resulting in an error
     -> loosened the constraint so user can type one field after the other.
    
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.trade.ui@11715 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/trade/ui/action/IssueAttachAction.java
+++ src/org/nightlabs/jfire/issuetracking/trade/ui/action/IssueAttachAction.java
@@ -2,6 +2,7 @@ package org.nightlabs.jfire.issuetracking.trade.ui.action;
 
 import javax.jdo.JDOHelper;
 
+import org.eclipse.swt.graphics.Point;
 import org.nightlabs.base.ui.wizard.DynamicPathWizardDialog;
 import org.nightlabs.jdo.ObjectID;
 import org.nightlabs.jfire.issuetracking.ui.issuelink.attach.AttachIssueToObjectWizard;
@@ -27,9 +28,16 @@ public class IssueAttachAction extends ArticleContainerAction
 	{
 		ArticleContainer articleContainer = this.getArticleContainer();
 		ObjectID objectID = (ObjectID)JDOHelper.getObjectId(articleContainer);
-		
+
 		AttachIssueToObjectWizard attachIssueToObjectWizard = new AttachIssueToObjectWizard(articleContainer);
-		DynamicPathWizardDialog dialog = new DynamicPathWizardDialog(attachIssueToObjectWizard);
+		DynamicPathWizardDialog dialog = new DynamicPathWizardDialog(attachIssueToObjectWizard)
+		{
+			@Override
+			protected Point getInitialSize()
+			{
+				return new Point(convertHorizontalDLUsToPixels(600), convertVerticalDLUsToPixels(450));
+			}
+		};
 		dialog.open();
 	}
 }
