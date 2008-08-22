0e7b2df3cf498eb68e8230804f6c4f1f80518085
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-22 09:25:15 +0000

    modified Project
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.admin.ui@11908 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/admin/ui/project/ActiveProjectTreeController.java
+++ src/org/nightlabs/jfire/issuetracking/admin/ui/project/ActiveProjectTreeController.java
@@ -1,82 +1,82 @@
-//package org.nightlabs.jfire.issuetracking.admin.ui.project;
-//
-//import java.util.Collection;
-//import java.util.List;
-//import java.util.Set;
-//
-//import javax.jdo.FetchPlan;
-//
-//import org.eclipse.core.runtime.IProgressMonitor;
-//import org.nightlabs.annotation.Implement;
-//import org.nightlabs.base.ui.progress.ProgressMonitorWrapper;
-//import org.nightlabs.jdo.NLJDOHelper;
-//import org.nightlabs.jfire.base.ui.jdo.tree.ActiveJDOObjectTreeController;
-//import org.nightlabs.jfire.issue.project.Project;
-//import org.nightlabs.jfire.issue.project.ProjectDAO;
-//import org.nightlabs.jfire.issue.project.ProjectItem;
-//import org.nightlabs.jfire.issue.project.ProjectParentResolver;
-//import org.nightlabs.jfire.issue.project.id.ProjectID;
-//import org.nightlabs.jfire.jdo.notification.TreeNodeParentResolver;
-//
-///**
-// * @author Chairat Kongarayawetchakun - chairat[at]nightlabs[dot]de
-// *
-// */
-//public class ActiveProjectTreeController 
-//extends ActiveJDOObjectTreeController<ProjectID, Project, ProjectTreeNode>
-//{
-//	public static final String[] FETCH_GROUPS_PROJECT = {
-//		FetchPlan.DEFAULT, Project.FETCH_GROUP_NAME, Project.FETCH_GROUP_PROJECT_ITEMS
-//	};
-//
-//	@Implement
-//	@Override
-//	protected ProjectTreeNode createNode()
-//	{
-//		return new ProjectTreeNode();
-//	}
-//
-//	@Implement
-//	@Override
-//	protected Collection<Project> retrieveChildren(ProjectID parentID, Project parent, IProgressMonitor monitor)
-//	{
-//		if (parentID != null) {
-//			Project project = ProjectDAO.sharedInstance().getProject(
-//					parentID, FETCH_GROUPS_PROJECT, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new ProgressMonitorWrapper(monitor));
-//			Collection<Project> res = null;//project.getProjectItems();
-//			return res;
-//		}
-//		
-//		return null;//ProjectDAO.sharedInstance().getProjects(FETCH_GROUPS_PROJECT, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new ProgressMonitorWrapper(monitor));
-//	}
-//
-//	@Implement
-//	@Override
-//	protected Collection<Project> retrieveJDOObjects(Set<ProjectID> objectIDs, IProgressMonitor monitor)
-//	{
-//		Collection<Project> res = ProjectDAO.sharedInstance().getProjects(
-//				objectIDs, FETCH_GROUPS_PROJECT, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new ProgressMonitorWrapper(monitor));
-//		return res;
-//	}
-//
-//	@Implement
-//	@Override
-//	protected void sortJDOObjects(List<Project> objects)
-//	{
-//		// no need to sort now - later maybe
-//	}
-//
-//	@Implement
-//	@Override
-//	protected TreeNodeParentResolver createTreeNodeParentResolver()
-//	{
-//		return new ProjectParentResolver();
-//	}
-//
-//	@Implement
-//	@Override
-//	protected Class<Project> getJDOObjectClass()
-//	{
-//		return Project.class;
-//	}
-//}
+package org.nightlabs.jfire.issuetracking.admin.ui.project;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import javax.jdo.FetchPlan;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.nightlabs.annotation.Implement;
+import org.nightlabs.base.ui.progress.ProgressMonitorWrapper;
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.base.ui.jdo.tree.ActiveJDOObjectTreeController;
+import org.nightlabs.jfire.base.ui.login.Login;
+import org.nightlabs.jfire.issue.project.Project;
+import org.nightlabs.jfire.issue.project.ProjectDAO;
+import org.nightlabs.jfire.issue.project.ProjectParentResolver;
+import org.nightlabs.jfire.issue.project.id.ProjectID;
+import org.nightlabs.jfire.jdo.notification.TreeNodeParentResolver;
+
+/**
+ * @author Chairat Kongarayawetchakun - chairat[at]nightlabs[dot]de
+ *
+ */
+public class ActiveProjectTreeController 
+extends ActiveJDOObjectTreeController<ProjectID, Project, ProjectTreeNode>
+{
+	public static final String[] FETCH_GROUPS_PROJECT = {
+		FetchPlan.DEFAULT, Project.FETCH_GROUP_NAME, Project.FETCH_GROUP_SUBPROJECTS, Project.FETCH_GROUP_PARENT_PROJECT
+	};
+
+	@Implement
+	@Override
+	protected ProjectTreeNode createNode()
+	{
+		return new ProjectTreeNode();
+	}
+
+	@Implement
+	@Override
+	protected Collection<Project> retrieveChildren(ProjectID parentID, Project parent, IProgressMonitor monitor)
+	{
+		if (parentID != null) {
+			Project project = ProjectDAO.sharedInstance().getProject(
+					parentID, FETCH_GROUPS_PROJECT, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new ProgressMonitorWrapper(monitor));
+			Collection<Project> res = project.getSubProjects();
+			return res;
+		}
+		
+		return ProjectDAO.sharedInstance().getRootProjects(Login.sharedInstance().getOrganisationID());
+	}
+
+	@Implement
+	@Override
+	protected Collection<Project> retrieveJDOObjects(Set<ProjectID> objectIDs, IProgressMonitor monitor)
+	{
+		Collection<Project> res = ProjectDAO.sharedInstance().getProjects(
+				objectIDs, FETCH_GROUPS_PROJECT, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new ProgressMonitorWrapper(monitor));
+		return res;
+	}
+
+	@Implement
+	@Override
+	protected void sortJDOObjects(List<Project> objects)
+	{
+		// no need to sort now - later maybe
+	}
+
+	@Implement
+	@Override
+	protected TreeNodeParentResolver createTreeNodeParentResolver()
+	{
+		return new ProjectParentResolver();
+	}
+
+	@Implement
+	@Override
+	protected Class<Project> getJDOObjectClass()
+	{
+		return Project.class;
+	}
+}
--- src/org/nightlabs/jfire/issuetracking/admin/ui/project/ProjectTreeComposite.java
+++ src/org/nightlabs/jfire/issuetracking/admin/ui/project/ProjectTreeComposite.java
@@ -1,228 +1,228 @@
-//package org.nightlabs.jfire.issuetracking.admin.ui.project;
-//
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Locale;
-//
-//import javax.jdo.FetchPlan;
-//
-//import org.eclipse.jface.action.Action;
-//import org.eclipse.jface.action.IAction;
-//import org.eclipse.jface.action.IContributionItem;
-//import org.eclipse.jface.action.IMenuListener;
-//import org.eclipse.jface.action.IMenuManager;
-//import org.eclipse.jface.action.MenuManager;
-//import org.eclipse.jface.action.Separator;
-//import org.eclipse.jface.dialogs.InputDialog;
-//import org.eclipse.jface.viewers.TreeViewer;
-//import org.eclipse.jface.window.Window;
-//import org.eclipse.swt.events.DisposeEvent;
-//import org.eclipse.swt.events.DisposeListener;
-//import org.eclipse.swt.graphics.Image;
-//import org.eclipse.swt.widgets.Composite;
-//import org.eclipse.swt.widgets.Control;
-//import org.eclipse.swt.widgets.Menu;
-//import org.eclipse.swt.widgets.Tree;
-//import org.eclipse.ui.IWorkbenchActionConstants;
-//import org.eclipse.ui.part.DrillDownAdapter;
-//import org.nightlabs.annotation.Implement;
-//import org.nightlabs.base.ui.resource.SharedImages;
-//import org.nightlabs.base.ui.tree.AbstractTreeComposite;
-//import org.nightlabs.base.ui.util.RCPUtil;
-//import org.nightlabs.jdo.NLJDOHelper;
-//import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeContentProvider;
-//import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeLabelProvider;
-//import org.nightlabs.jfire.base.ui.jdo.tree.JDOTreeNodesChangedEvent;
-//import org.nightlabs.jfire.base.ui.jdo.tree.JDOTreeNodesChangedEventHandler;
-//import org.nightlabs.jfire.base.ui.login.Login;
-//import org.nightlabs.jfire.idgenerator.IDGenerator;
-//import org.nightlabs.jfire.issue.project.Project;
-//import org.nightlabs.jfire.issue.project.ProjectDAO;
-//import org.nightlabs.jfire.issue.project.id.ProjectID;
-//import org.nightlabs.jfire.issuetracking.admin.ui.IssueTrackingAdminPlugin;
-//import org.nightlabs.progress.NullProgressMonitor;
-//
-///**
-// * @author Chairat Kongarayawetchakun - chairat[at]nightlabs[dot]de
-// *
-// */
-//public class ProjectTreeComposite 
-//extends AbstractTreeComposite<Project> 
-//{
-//	protected static class ProjectTreeContentProvider
-//	extends JDOObjectTreeContentProvider<ProjectID, Project, ProjectTreeNode>
-//	{
-//		@Override
-//		public boolean hasJDOObjectChildren(Project project) {
-//			return project.getProjectItems().size() > 0;
-//		}
-//	}
-//
-//	private ActiveProjectTreeController activeProjectTreeController;
-//
-//	public ProjectTreeComposite(Composite parent, int treeStyle)
-//	{
-//		super(parent, treeStyle, true, true, false);
-//		activeProjectTreeController = new ActiveProjectTreeController()
-//		{
-//			@Override
-//			protected void onJDOObjectsChanged(JDOTreeNodesChangedEvent<ProjectID, ProjectTreeNode> changedEvent)
-//			{
-//				JDOTreeNodesChangedEventHandler.handle(getTreeViewer(), changedEvent);
-//			}
-//		};
-//
-//		setInput(activeProjectTreeController);
-//		addDisposeListener(new DisposeListener() {
-//			public void widgetDisposed(DisposeEvent e)
-//			{
-//				activeProjectTreeController.close();
-//				activeProjectTreeController = null;
-//			}
-//		});
-//
-//		
-//		addContextMenuContribution(new CreateProjectAction());
-//		
-//		drillDownAdapter = new DrillDownAdapter(getTreeViewer());
-//		hookContextMenu();
-//	}
-//	
-//	public ProjectTreeComposite(Composite parent)
-//	{
-//		this(parent, DEFAULT_STYLE_SINGLE);
-//	}
-//	
-//	private void hookContextMenu() {
-//		MenuManager menuMgr = new MenuManager("#PopupMenu"); //$NON-NLS-1$
-//		menuMgr.setRemoveAllWhenShown(true);
-//		menuMgr.addMenuListener(new IMenuListener() {
-//			public void menuAboutToShow(IMenuManager manager) {
-//				ProjectTreeComposite.this.fillContextMenu(manager);
-//			}
-//		});
-//		Menu menu = menuMgr.createContextMenu(getTreeViewer().getControl());
-//		getTreeViewer().getControl().setMenu(menu);
-//	}
-//
-//	/**
-//	 * Contains instances of both, {@link IContributionItem} and {@link IAction}
-//	 */
-//	private List<Object> contextMenuContributions;
-//
-//	public void addContextMenuContribution(IContributionItem contributionItem)
-//	{
-//		if (contextMenuContributions == null)
-//			contextMenuContributions = new LinkedList<Object>();
-//
-//		contextMenuContributions.add(contributionItem);
-//	}
-//
-//	public void addContextMenuContribution(IAction action)
-//	{
-//		if (contextMenuContributions == null)
-//			contextMenuContributions = new LinkedList<Object>();
-//
-//		contextMenuContributions.add(action);
-//	}
-//
-//	private void fillContextMenu(IMenuManager manager) {
-//		if (contextMenuContributions != null) {
-//			for (Object contextMenuContribution : contextMenuContributions) {
-//				if (contextMenuContribution instanceof IContributionItem)
-//					manager.add((IContributionItem)contextMenuContribution);
-//				else if (contextMenuContribution instanceof IAction)
-//					manager.add((IAction)contextMenuContribution);
-//				else
-//					throw new IllegalStateException("How the hell got an instance of " + (contextMenuContribution == null ? "null" : contextMenuContribution.getClass()) + " in the contextMenuContributions list?!"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-//			}
-//		}
-//
-//		drillDownAdapter.addNavigationActions(manager);
-//
-//		// Other plug-ins can contribute their actions here
-//		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-//	}
-//
-//	protected DrillDownAdapter drillDownAdapter;
-//
-//	protected static class ProjectTreeLabelProvider extends JDOObjectTreeLabelProvider<ProjectID, Project, ProjectTreeNode>
-//	{
-//		@Override
-//		protected String getJDOObjectText(Project jdoObject, int columnIndex) {
-//			return jdoObject.getName().getText();
-//		}
-//
-//		@Override
-//		protected Image getJDOObjectImage(Project project, int columnIndex) {
-//			if (columnIndex == 0)
-//				return SharedImages.getSharedImage(IssueTrackingAdminPlugin.getDefault(),
-//						ProjectTreeComposite.class, "project");
-//
-//			return super.getJDOObjectImage(project, columnIndex);
-//		}
-//	}
-//
-//	@Implement
-//	@Override
-//	public void createTreeColumns(Tree tree)
-//	{
-//	}
-//
-//	@Implement
-//	@Override
-//	public void setTreeProvider(TreeViewer treeViewer)
-//	{
-//		treeViewer.setContentProvider(new ProjectTreeContentProvider());
-//		treeViewer.setLabelProvider(new ProjectTreeLabelProvider());
-//	}
-//
-//	@Override
-//	protected Project getSelectionObject(Object obj)
-//	{
-//		if (obj instanceof ProjectTreeNode)
-//			return ((ProjectTreeNode)obj).getJdoObject();
-//
-//		return null;
-//	}
-//	
-//	public class CreateProjectAction extends Action {
-//		private InputDialog dialog;
-//		public CreateProjectAction() {
-//			setId(CreateProjectAction.class.getName());
-//			setImageDescriptor(SharedImages.getSharedImageDescriptor(
-//					IssueTrackingAdminPlugin.getDefault(), 
-//					ProjectSection.class, 
-//			"Create"));
-//			setToolTipText("Create Project");
-//			setText("Create");
-//		}
-//
-//		@Override
-//		public void run() {
-//			dialog = new InputDialog(RCPUtil.getActiveShell(), "Create Project", "Enter project's name", "Name", null) {
-//				@Override
-//				protected void okPressed() {
-//					try {
-//						Project project = new Project(Login.getLogin().getOrganisationID(), IDGenerator.nextID(Project.class));
-////						project.setProject(getFirstSelectedElement());
-//						project.getName().setText(Locale.ENGLISH.getLanguage(), getValue());
-//						ProjectDAO.sharedInstance().storeProject(project, false, new String[]{FetchPlan.DEFAULT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new NullProgressMonitor());
-//						dialog.close();
-//					} catch (Exception e) {
-//						throw new RuntimeException(e);
-//					}
-//				};
-//				
-//				@Override
-//				protected Control createDialogArea(Composite parent) {
-//					Control dialogArea = super.createDialogArea(parent);
-//					return dialogArea;
-//				}
-//			};
-//			
-//			if (dialog.open() != Window.OK)
-//				return;
-//		}		
-//	}
-//}
+package org.nightlabs.jfire.issuetracking.admin.ui.project;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.jdo.FetchPlan;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.part.DrillDownAdapter;
+import org.nightlabs.annotation.Implement;
+import org.nightlabs.base.ui.resource.SharedImages;
+import org.nightlabs.base.ui.tree.AbstractTreeComposite;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeContentProvider;
+import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeLabelProvider;
+import org.nightlabs.jfire.base.ui.jdo.tree.JDOTreeNodesChangedEvent;
+import org.nightlabs.jfire.base.ui.jdo.tree.JDOTreeNodesChangedEventHandler;
+import org.nightlabs.jfire.base.ui.login.Login;
+import org.nightlabs.jfire.idgenerator.IDGenerator;
+import org.nightlabs.jfire.issue.project.Project;
+import org.nightlabs.jfire.issue.project.ProjectDAO;
+import org.nightlabs.jfire.issue.project.id.ProjectID;
+import org.nightlabs.jfire.issuetracking.admin.ui.IssueTrackingAdminPlugin;
+import org.nightlabs.progress.NullProgressMonitor;
+
+/**
+ * @author Chairat Kongarayawetchakun - chairat[at]nightlabs[dot]de
+ *
+ */
+public class ProjectTreeComposite 
+extends AbstractTreeComposite<Project> 
+{
+	protected static class ProjectTreeContentProvider
+	extends JDOObjectTreeContentProvider<ProjectID, Project, ProjectTreeNode>
+	{
+		@Override
+		public boolean hasJDOObjectChildren(Project project) {
+			return project.getSubProjects().size() > 0;
+		}
+	}
+
+	private ActiveProjectTreeController activeProjectTreeController;
+
+	public ProjectTreeComposite(Composite parent, int treeStyle)
+	{
+		super(parent, treeStyle, true, true, false);
+		activeProjectTreeController = new ActiveProjectTreeController()
+		{
+			@Override
+			protected void onJDOObjectsChanged(JDOTreeNodesChangedEvent<ProjectID, ProjectTreeNode> changedEvent)
+			{
+				JDOTreeNodesChangedEventHandler.handle(getTreeViewer(), changedEvent);
+			}
+		};
+
+		setInput(activeProjectTreeController);
+		addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e)
+			{
+				activeProjectTreeController.close();
+				activeProjectTreeController = null;
+			}
+		});
+
+		
+		addContextMenuContribution(new CreateProjectAction());
+		
+		drillDownAdapter = new DrillDownAdapter(getTreeViewer());
+		hookContextMenu();
+	}
+	
+	public ProjectTreeComposite(Composite parent)
+	{
+		this(parent, DEFAULT_STYLE_SINGLE);
+	}
+	
+	private void hookContextMenu() {
+		MenuManager menuMgr = new MenuManager("#PopupMenu"); //$NON-NLS-1$
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				ProjectTreeComposite.this.fillContextMenu(manager);
+			}
+		});
+		Menu menu = menuMgr.createContextMenu(getTreeViewer().getControl());
+		getTreeViewer().getControl().setMenu(menu);
+	}
+
+	/**
+	 * Contains instances of both, {@link IContributionItem} and {@link IAction}
+	 */
+	private List<Object> contextMenuContributions;
+
+	public void addContextMenuContribution(IContributionItem contributionItem)
+	{
+		if (contextMenuContributions == null)
+			contextMenuContributions = new LinkedList<Object>();
+
+		contextMenuContributions.add(contributionItem);
+	}
+
+	public void addContextMenuContribution(IAction action)
+	{
+		if (contextMenuContributions == null)
+			contextMenuContributions = new LinkedList<Object>();
+
+		contextMenuContributions.add(action);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		if (contextMenuContributions != null) {
+			for (Object contextMenuContribution : contextMenuContributions) {
+				if (contextMenuContribution instanceof IContributionItem)
+					manager.add((IContributionItem)contextMenuContribution);
+				else if (contextMenuContribution instanceof IAction)
+					manager.add((IAction)contextMenuContribution);
+				else
+					throw new IllegalStateException("How the hell got an instance of " + (contextMenuContribution == null ? "null" : contextMenuContribution.getClass()) + " in the contextMenuContributions list?!"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			}
+		}
+
+		drillDownAdapter.addNavigationActions(manager);
+
+		// Other plug-ins can contribute their actions here
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+
+	protected DrillDownAdapter drillDownAdapter;
+
+	protected static class ProjectTreeLabelProvider extends JDOObjectTreeLabelProvider<ProjectID, Project, ProjectTreeNode>
+	{
+		@Override
+		protected String getJDOObjectText(Project jdoObject, int columnIndex) {
+			return jdoObject.getName().getText();
+		}
+
+		@Override
+		protected Image getJDOObjectImage(Project project, int columnIndex) {
+			if (columnIndex == 0)
+				return SharedImages.getSharedImage(IssueTrackingAdminPlugin.getDefault(),
+						ProjectTreeComposite.class, "project");
+
+			return super.getJDOObjectImage(project, columnIndex);
+		}
+	}
+
+	@Implement
+	@Override
+	public void createTreeColumns(Tree tree)
+	{
+	}
+
+	@Implement
+	@Override
+	public void setTreeProvider(TreeViewer treeViewer)
+	{
+		treeViewer.setContentProvider(new ProjectTreeContentProvider());
+		treeViewer.setLabelProvider(new ProjectTreeLabelProvider());
+	}
+
+	@Override
+	protected Project getSelectionObject(Object obj)
+	{
+		if (obj instanceof ProjectTreeNode)
+			return ((ProjectTreeNode)obj).getJdoObject();
+
+		return null;
+	}
+	
+	public class CreateProjectAction extends Action {
+		private InputDialog dialog;
+		public CreateProjectAction() {
+			setId(CreateProjectAction.class.getName());
+			setImageDescriptor(SharedImages.getSharedImageDescriptor(
+					IssueTrackingAdminPlugin.getDefault(), 
+					ProjectSection.class, 
+			"Create"));
+			setToolTipText("Create Project");
+			setText("Create");
+		}
+
+		@Override
+		public void run() {
+			dialog = new InputDialog(RCPUtil.getActiveShell(), "Create Project", "Enter project's name", "Name", null) {
+				@Override
+				protected void okPressed() {
+					try {
+						Project project = new Project(Login.getLogin().getOrganisationID(), IDGenerator.nextID(Project.class));
+//						project.setProject(getFirstSelectedElement());
+						project.getName().setText(Locale.ENGLISH.getLanguage(), getValue());
+						ProjectDAO.sharedInstance().storeProject(project, false, new String[]{FetchPlan.DEFAULT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new NullProgressMonitor());
+						dialog.close();
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				};
+				
+				@Override
+				protected Control createDialogArea(Composite parent) {
+					Control dialogArea = super.createDialogArea(parent);
+					return dialogArea;
+				}
+			};
+			
+			if (dialog.open() != Window.OK)
+				return;
+		}		
+	}
+}
