6294bc055b593872db85aee27d7beff83e422dc8
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-09 09:59:36 +0000

    Project combo
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@12029 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/create/ProjectComboComposite.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/create/ProjectComboComposite.java
@@ -1,13 +1,8 @@
 package org.nightlabs.jfire.issuetracking.ui.issue.create;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.jdo.FetchPlan;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -24,15 +19,15 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.nightlabs.base.ui.composite.XComposite;
-import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.base.ui.custom.XCombo;
 import org.nightlabs.jfire.base.ui.login.Login;
 import org.nightlabs.jfire.issue.project.Project;
 import org.nightlabs.jfire.issue.project.ProjectDAO;
-import org.nightlabs.progress.NullProgressMonitor;
+import org.nightlabs.jfire.issue.project.id.ProjectID;
+import org.nightlabs.util.CollectionUtil;
 import org.nightlabs.util.NLLocale;
 
 public class ProjectComboComposite 
@@ -53,61 +48,35 @@ implements ISelectionProvider
 		}
 	}
 	
-	private Combo projectCombo;
+	private XCombo projectCombo;
+	private Project selectedProject;
+	private List<Project> projectList;
 	
 	public ProjectComboComposite(Composite parent, int style,String filterOrganisationID, boolean filterOrganisationIDInverse)
 	{
 		super(parent, style, LayoutMode.TIGHT_WRAPPER);
 		
-		projectCombo = new Combo(this, SWT.BORDER);
-		projectCombo.setLayoutData(new GridData(GridData.FILL_BOTH));
+		projectCombo = new XCombo(this, SWT.BORDER);
+		projectCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		projectCombo.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e)
 			{
+				int idx = projectCombo.getSelectionIndex();
+				if (idx < 0 || idx > projectList.size() - 1)
+					selectedProject = null;
+				else
+					selectedProject = projectList.get(projectList.size() - 1 - idx);
+
 				fireSelectionChangedEvent();
 			}
 		});
 	}
 	
-	protected void fireSelectionChangedEvent()
-	{
-		selectedProject = null;
-		selection = null;
-
-		Object[] listeners = selectionChangedListeners.getListeners();
-		if (listeners.length == 0)
-			return;
-
-		SelectionChangedEvent event = new SelectionChangedEvent(this, getSelection());
-		for (int i = 0; i < listeners.length; i++) {
-			ISelectionChangedListener listener = (ISelectionChangedListener) listeners[i];
-			listener.selectionChanged(event);
-		}
-	}
-	
-	private ListenerList selectionChangedListeners = new ListenerList();
-	
-	@Override
-	public void addSelectionChangedListener(ISelectionChangedListener listener) {
-		selectionChangedListeners.add(listener);
-	}
-
-	private IStructuredSelection selection = null;
-
-	@Override
-	public ISelection getSelection() {
-		if (selection == null)
-			selection = new StructuredSelection(getSelectedProject());
-		return selection;
-	}
-
-	private Set<Project> projectSet = new HashSet<Project>();
+	private Collection<Project> projectSet = new HashSet<Project>();
 	public void loadProjects()
 	{
-		projects.clear();
 		projectCombo.removeAll();
-		projectCombo.add("Loading Projects............");
 
 		new Job("Loading Projects............") {
 			@Override
@@ -128,13 +97,18 @@ implements ISelectionProvider
 								return;
 
 							projectCombo.removeAll();
+							Project[] projectArray = CollectionUtil.collection2TypedArray(projectSet, Project.class);
+							projectList = CollectionUtil.array2ArrayList(projectArray);
 							
-							for (Project project : projectSet) {
-								projectCombo.add(project.getName().getText(NLLocale.getDefault().getLanguage()));
+							for (int n = projectList.size() - 1; n >= 0; n--) {
+								StringBuffer sb = new StringBuffer("");
+								
+								for (int i = 0; i < projectList.get(n).getLevel(); i++) 
+									sb.append("»»");
+								projectCombo.add(null, (sb.toString().equals("")?"": sb.append(" ")) +  projectList.get(n).getName().getText(NLLocale.getDefault().getLanguage()));
 							}
 							
 							ProjectComboComposite.this.getParent().layout(true);
-							fireSelectionChangedEvent();
 						}
 					});
 				} catch (Exception x) {
@@ -146,12 +120,17 @@ implements ISelectionProvider
 		}.schedule();
 	}
 	
+	private int level = 0;
 	private void generateSub(Project project) {
 		boolean hasMoreChilds = false;
 		
 		Collection<Project> sp = ProjectDAO.sharedInstance().getProjectsByParentProjectID(project.getOrganisationID(), project.getProjectID());
 		if (sp != null && sp.size() > 0) {
 			hasMoreChilds = true;
+			level++;
+			for (Project p : sp) {
+				p.setLevel(level);
+			}
 			projectSet.addAll(sp);
 		}
 		
@@ -162,22 +141,83 @@ implements ISelectionProvider
 		}
 	}
 	
-	private List<Project> projects = new ArrayList<Project>(0);
-	private Project selectedProject = null;
-	
-	public Project getSelectedProject()
+	private ListenerList selectionChangedListeners = new ListenerList();
+	private void fireSelectionChangedEvent()
 	{
-		return null;
+		if (selectionChangedListeners.isEmpty())
+			return;
+
+		SelectionChangedEvent event = new SelectionChangedEvent(this, getSelection());
+
+		for (Object l : selectionChangedListeners.getListeners()) {
+			ISelectionChangedListener listener = (ISelectionChangedListener) l;
+			listener.selectionChanged(event);
+		}
 	}
-	
+
 	@Override
-	public void removeSelectionChangedListener(ISelectionChangedListener listener) {
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+		selectionChangedListeners.add(listener);
+	}
+
+	@Override
+	public ISelection getSelection() {
+		if (selectedProject == null)
+			return new StructuredSelection(new Object[0]);
+
+		return new StructuredSelection(selectedProject);
+	}
+
+	@Override
+	public void removeSelectionChangedListener(ISelectionChangedListener listener)
+	{
 		selectionChangedListeners.remove(listener);
 	}
 
 	@Override
-	public void setSelection(ISelection arg0) {
-		throw new UnsupportedOperationException("NYI");
+	public void setSelection(ISelection selection) {
+		if (!(selection instanceof IStructuredSelection))
+			throw new IllegalArgumentException("selection is not an instance of " + IStructuredSelection.class.getName() + " but " + (selection == null ? null : selection.getClass().getName())); //$NON-NLS-1$ //$NON-NLS-2$
+
+		IStructuredSelection sel = (IStructuredSelection) selection;
+		Object selObj = sel.getFirstElement();
+
+		if (selObj == null)
+			setSelectedProjectID(-1);
+		if (selObj instanceof Project)
+			setSelectedProject((Project) selObj);
+		else if (selObj instanceof ProjectID)
+			setSelectedProject((ProjectID) selObj);
+		else
+			throw new IllegalArgumentException("selection.getFirstElement() is neither null, nor an instanceof " + Project.class.getName()+ " or " + ProjectID.class.getName()+ "! It is an instance of " + (selObj == null ? null : selObj.getClass().getName())); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	}
+	
+	public void setSelectedProject(Project project) {
+		setSelectedProjectID(project == null ? null : project.getProjectID());
 	}
 
+	public void setSelectedProject(ProjectID projectID) {
+		setSelectedProjectID(projectID == null ? null : projectID.projectID);
+	}
+	
+	private void setSelectedProjectID(long projectID) {
+		int idx = -1;
+		int i = 0;
+		for (Project project : projectList) {
+			if (project.getProjectID() == projectID) {
+				idx = i;
+				break;
+			}
+			++i;
+		}
+
+		if (idx < 0) {
+			projectCombo.deselectAll();
+			selectedProject = null;
+		}
+		else {
+			projectCombo.select(idx);
+			selectedProject = projectList.get(idx);
+		}
+	}
 }
