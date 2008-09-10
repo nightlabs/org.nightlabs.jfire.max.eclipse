736e725a67158a0a3d10940bcc274dd7fa45355c
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-10 14:11:50 +0000

    showing the children nodes of the Offers of the recurringOrder in the headertree
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@12040 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/trade/ui/articlecontainer/header/recurring/RecurringOfferTreeNode.java
@@ -0,0 +1,84 @@
+package org.nightlabs.jfire.trade.ui.articlecontainer.header.recurring;
+
+import java.util.List;
+
+import org.nightlabs.annotation.Implement;
+import org.nightlabs.jfire.trade.recurring.RecurringOffer;
+import org.nightlabs.jfire.trade.ui.articlecontainer.header.HeaderTreeNode;
+import org.nightlabs.progress.ProgressMonitor;
+import org.nightlabs.jdo.ObjectIDUtil;
+import org.eclipse.swt.graphics.Image;
+
+public class RecurringOfferTreeNode extends HeaderTreeNode{
+
+
+
+	private RecurringOffer recurringOffer;
+
+	public RecurringOfferTreeNode(HeaderTreeNode parent, byte position, RecurringOffer offer)
+	{
+		super(parent, position);
+		this.recurringOffer = offer;
+		init();
+	}
+
+
+	@Override
+	protected List<HeaderTreeNode> createChildNodes(List<Object> childData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public Image getColumnImage(int columnIndex)
+	{
+		switch (columnIndex) {
+			case 0:
+				return getHeaderTreeComposite().getImageOfferTreeNode();
+			default:
+				return null;
+		}
+	}
+
+	@Override
+	@Implement
+	public String getColumnText(int columnIndex)
+	{
+		switch (columnIndex) {
+			case 0: return recurringOffer.getOfferIDPrefix() + '/' + ObjectIDUtil.longObjectIDFieldToString(recurringOffer.getOfferID());
+			default:
+				return ""; //$NON-NLS-1$
+		}
+	}
+
+	@Override
+	protected List<Object> loadChildData(ProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public RecurringOffer getRecurringOffer() {
+		return recurringOffer;
+	}
+
+
+	/**
+	 * This method returns always <tt>false</tt>.
+	 *
+	 * @see org.nightlabs.jfire.trade.ui.articlecontainer.header.HeaderTreeNode#hasChildren()
+	 */
+	@Override
+	public boolean hasChildren()
+	{
+		return false;
+	}
+
+
+
+
+
+
+}
