0e5892505f5397831c2b7f249444275943adf000
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-15 05:41:41 +0000

    revived active project tree.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@12078 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/ProjectTreeComposite.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/ProjectTreeComposite.java
@@ -1,32 +1,44 @@
 package org.nightlabs.jfire.issuetracking.ui.issue;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.jdo.FetchPlan;
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.part.DrillDownAdapter;
 import org.nightlabs.annotation.Implement;
+import org.nightlabs.base.ui.progress.ProgressMonitorWrapper;
 import org.nightlabs.base.ui.resource.SharedImages;
 import org.nightlabs.base.ui.tree.AbstractTreeComposite;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.jdo.NLJDOHelper;
 import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeContentProvider;
 import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeLabelProvider;
 import org.nightlabs.jfire.base.ui.jdo.tree.JDOTreeNodesChangedEvent;
 import org.nightlabs.jfire.base.ui.jdo.tree.JDOTreeNodesChangedEventHandler;
+import org.nightlabs.jfire.base.ui.login.Login;
+import org.nightlabs.jfire.idgenerator.IDGenerator;
 import org.nightlabs.jfire.issue.project.Project;
 import org.nightlabs.jfire.issue.project.ProjectDAO;
 import org.nightlabs.jfire.issue.project.id.ProjectID;
@@ -40,7 +52,9 @@ import org.nightlabs.progress.NullProgressMonitor;
 public class ProjectTreeComposite 
 extends AbstractTreeComposite<Project> 
 {
-	private static String[] FETCH_GROUPS = new String[]{ FetchPlan.DEFAULT, Project.FETCH_GROUP_SUBPROJECTS, Project.FETCH_GROUP_NAME};
+	private static String[] FETCH_GROUPS = new String[]{
+		FetchPlan.DEFAULT, Project.FETCH_GROUP_NAME, Project.FETCH_GROUP_SUBPROJECTS, Project.FETCH_GROUP_PARENT_PROJECT
+	};
 	
 	protected static class ProjectTreeContentProvider
 	extends JDOObjectTreeContentProvider<ProjectID, Project, ProjectTreeNode>
@@ -75,6 +89,9 @@ extends AbstractTreeComposite<Project>
 			}
 		});
 
+		addContextMenuContribution(new CreateProjectAction());
+		addContextMenuContribution(new RenameProjectAction());
+		
 		drillDownAdapter = new DrillDownAdapter(getTreeViewer());
 		hookContextMenu();
 	}
@@ -176,4 +193,90 @@ extends AbstractTreeComposite<Project>
 
 		return null;
 	}
+	
+	public class CreateProjectAction extends Action {
+	private InputDialog dialog;
+	public CreateProjectAction() {
+		setId(CreateProjectAction.class.getName());
+		setImageDescriptor(SharedImages.getSharedImageDescriptor(
+				IssueTrackingPlugin.getDefault(), 
+				ProjectTreeComposite.class, 
+		"Create"));
+		setToolTipText("Create Sub Project");
+		setText("Create Sub Project");
+	}
+
+	@Override
+	public void run() {
+		dialog = new InputDialog(RCPUtil.getActiveShell(), "Create Sub Project", "Enter project's name", "Name", null) {
+			@Override
+			protected void okPressed() {
+				try {
+					TreeSelection selection = (TreeSelection)getTreeViewer().getSelection();
+					Project projectToStore = ((ProjectTreeNode)(selection.getFirstElement())).getJdoObject();
+					Project project = new Project(Login.getLogin().getOrganisationID(), IDGenerator.nextID(Project.class));
+					project.getName().setText(Locale.ENGLISH.getLanguage(), getValue());
+					
+					projectToStore = ProjectDAO.sharedInstance().getProject(
+							projectToStore.getObjectId(), FETCH_GROUPS, 1, new NullProgressMonitor());
+					Collection<Project> res = project.getSubProjects();
+					projectToStore.addSubProject(project);
+					ProjectDAO.sharedInstance().storeProject(projectToStore, false, new String[]{FetchPlan.DEFAULT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new NullProgressMonitor());
+					dialog.close();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			};
+			
+			@Override
+			protected Control createDialogArea(Composite parent) {
+				Control dialogArea = super.createDialogArea(parent);
+				return dialogArea;
+			}
+		};
+		
+		if (dialog.open() != Window.OK)
+			return;
+	}		
+}
+
+public class RenameProjectAction extends Action {
+	private InputDialog dialog;
+	public RenameProjectAction() {
+		setId(CreateProjectAction.class.getName());
+		setImageDescriptor(SharedImages.getSharedImageDescriptor(
+				IssueTrackingPlugin.getDefault(), 
+				ProjectTreeComposite.class, 
+		"Rename"));
+		setToolTipText("Rename Project");
+		setText("Rename Project");
+	}
+
+	@Override
+	public void run() {
+		TreeSelection selection = (TreeSelection)getTreeViewer().getSelection();
+		final Project projectToStore = ((ProjectTreeNode)(selection.getFirstElement())).getJdoObject();
+		dialog = new InputDialog(RCPUtil.getActiveShell(), "Rename Project", "Enter project's name", projectToStore.getName().getText(), null) {
+			@Override
+			protected void okPressed() {
+				try {
+					projectToStore.getName().setText(Locale.ENGLISH.getLanguage(), getValue());
+					ProjectDAO.sharedInstance().storeProject(projectToStore, false, new String[]{FetchPlan.DEFAULT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new NullProgressMonitor());
+					dialog.close();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			};
+			
+			@Override
+			protected Control createDialogArea(Composite parent) {
+				Control dialogArea = super.createDialogArea(parent);
+				return dialogArea;
+			}
+		};
+		
+		if (dialog.open() != Window.OK)
+			return;
+	}		
+}
 }
