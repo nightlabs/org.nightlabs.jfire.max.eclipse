65ed9726c4e190109af81f2b1032c93e26e5ab99
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-22 03:17:59 +0000

    moved icon.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.admin.ui@12157 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/admin/ui/overview/ProjectCategory.java
+++ src/org/nightlabs/jfire/issuetracking/admin/ui/overview/ProjectCategory.java
@@ -1,11 +1,17 @@
 package org.nightlabs.jfire.issuetracking.admin.ui.overview;
 
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.nightlabs.base.ui.composite.XComposite;
 import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
+import org.nightlabs.base.ui.util.RCPUtil;
 import org.nightlabs.jfire.base.ui.overview.CategoryFactory;
 import org.nightlabs.jfire.base.ui.overview.CustomCompositeCategory;
+import org.nightlabs.jfire.issue.project.Project;
+import org.nightlabs.jfire.issuetracking.admin.ui.project.ProjectEditor;
+import org.nightlabs.jfire.issuetracking.admin.ui.project.ProjectEditorInput;
 import org.nightlabs.jfire.issuetracking.ui.project.ProjectTreeComposite;
 
 /**
@@ -25,18 +31,18 @@ extends CustomCompositeCategory {
 		XComposite wrapper = new XComposite(composite, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
 		final ProjectTreeComposite pc = new ProjectTreeComposite(wrapper);
 		
-//		pc.addDoubleClickListener(new IDoubleClickListener() {
-//			public void doubleClick(DoubleClickEvent e) {
-//				Project project = pc.getFirstSelectedElement();
-//				
-//				try {
-//					RCPUtil.openEditor(new ProjectEditorInput(project.getObjectId()),
-//							ProjectEditor.EDITOR_ID);
-//				} catch (Exception ex) {
-//					throw new RuntimeException(ex);
-//				}
-//			}
-//		});
+		pc.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent e) {
+				Project project = pc.getFirstSelectedElement();
+				
+				try {
+					RCPUtil.openEditor(new ProjectEditorInput(project.getObjectId()),
+							ProjectEditor.EDITOR_ID);
+				} catch (Exception ex) {
+					throw new RuntimeException(ex);
+				}
+			}
+		});
 		return wrapper;
 	}
 }
