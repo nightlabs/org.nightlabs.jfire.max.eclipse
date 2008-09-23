4baa66feb5f945a41d97130f6c3faea23a2c9e21
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-23 11:46:01 +0000

    fetch groups in article container
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@12172 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/articlecontainer/detail/RecurringContainerEditComposite.java
+++ src/org/nightlabs/jfire/trade/ui/articlecontainer/detail/RecurringContainerEditComposite.java
@@ -1,23 +1,56 @@
 package org.nightlabs.jfire.trade.ui.articlecontainer.detail;
 
+import javax.jdo.FetchPlan;
+
 import org.eclipse.swt.widgets.Composite;
 import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.jbpm.graph.def.StatableLocal;
 import org.nightlabs.jfire.trade.ArticleContainer;
+import org.nightlabs.jfire.trade.FetchGroupsTrade;
+import org.nightlabs.jfire.trade.OfferLocal;
+import org.nightlabs.jfire.trade.Order;
+import org.nightlabs.jfire.trade.Segment;
+import org.nightlabs.jfire.trade.SegmentType;
 import org.nightlabs.jfire.trade.id.ArticleContainerID;
 import org.nightlabs.jfire.trade.id.OfferID;
 import org.nightlabs.jfire.trade.id.OrderID;
+import org.nightlabs.jfire.trade.recurring.RecurringOffer;
 import org.nightlabs.jfire.trade.recurring.dao.RecurringOfferDAO;
 import org.nightlabs.jfire.trade.recurring.dao.RecurringOrderDAO;
 import org.nightlabs.progress.ProgressMonitor;
 
 public class RecurringContainerEditComposite extends ArticleContainerEditComposite {
 
+	
 	public RecurringContainerEditComposite(Composite parent,
 			ArticleContainerID containerID) {
 		super(parent, containerID);
 	}
 
+	public static final String[] FETCH_GROUPS_RECURRING_ARTICLE_CONTAINER_WITHOUT_ARTICLES = {
+		FetchPlan.DEFAULT,
+		FetchGroupsTrade.FETCH_GROUP_ARTICLE_CONTAINER_IN_EDITOR,
+		Segment.FETCH_GROUP_THIS_SEGMENT,
+		SegmentType.FETCH_GROUP_THIS_SEGMENT_TYPE,
+		StatableLocal.FETCH_GROUP_STATE
+	};
 
+	public static final String[] FETCH_GROUPS_RECURRING_ORDER_WITH_ARTICLES = {
+		FetchGroupsTrade.FETCH_GROUP_ARTICLE_CONTAINER_IN_EDITOR,
+		Order.FETCH_GROUP_THIS_ORDER, Segment.FETCH_GROUP_THIS_SEGMENT,
+		SegmentType.FETCH_GROUP_THIS_SEGMENT_TYPE,
+		FetchGroupsTrade.FETCH_GROUP_ARTICLE_IN_ORDER_EDITOR, FetchPlan.DEFAULT };
+
+	public static final String[] FETCH_GROUPS_RECURRING_OFFER_WITH_ARTICLES = {
+		FetchGroupsTrade.FETCH_GROUP_ARTICLE_CONTAINER_IN_EDITOR,
+		RecurringOffer.FETCH_GROUP_ARTICLES,
+		RecurringOffer.FETCH_GROUP_RECURRING_OFFER_CONFIGURATION,
+		OfferLocal.FETCH_GROUP_THIS_OFFER_LOCAL,
+		StatableLocal.FETCH_GROUP_STATE, Order.FETCH_GROUP_CUSTOMER_GROUP,
+		Segment.FETCH_GROUP_THIS_SEGMENT,
+		SegmentType.FETCH_GROUP_THIS_SEGMENT_TYPE,
+		FetchGroupsTrade.FETCH_GROUP_ARTICLE_IN_OFFER_EDITOR, FetchPlan.DEFAULT };
+	
 		
 	@Override	
 	protected ArticleContainer retrieveArticleContainer(ArticleContainerID articleContainerID, boolean withArticles, ProgressMonitor monitor)
@@ -25,7 +58,7 @@ public class RecurringContainerEditComposite extends ArticleContainerEditComposi
 		if (articleContainerID instanceof OrderID)
 			return RecurringOrderDAO.sharedInstance().getRecurringOrder(
 					(OrderID) articleContainerID,
-					withArticles ? FETCH_GROUPS_ORDER_WITH_ARTICLES : FETCH_GROUPS_ARTICLE_CONTAINER_WITHOUT_ARTICLES,
+					withArticles ? FETCH_GROUPS_RECURRING_ORDER_WITH_ARTICLES : FETCH_GROUPS_RECURRING_ARTICLE_CONTAINER_WITHOUT_ARTICLES,
 					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT,
 					monitor
 			);
@@ -33,7 +66,7 @@ public class RecurringContainerEditComposite extends ArticleContainerEditComposi
 		if (articleContainerID instanceof OfferID)
 			return RecurringOfferDAO.sharedInstance().getRecurringOffer(
 					(OfferID) articleContainerID,
-					withArticles ? FETCH_GROUPS_OFFER_WITH_ARTICLES : FETCH_GROUPS_ARTICLE_CONTAINER_WITHOUT_ARTICLES,
+					withArticles ? FETCH_GROUPS_RECURRING_OFFER_WITH_ARTICLES : FETCH_GROUPS_RECURRING_ARTICLE_CONTAINER_WITHOUT_ARTICLES,
 					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT,
 					monitor
 			);
