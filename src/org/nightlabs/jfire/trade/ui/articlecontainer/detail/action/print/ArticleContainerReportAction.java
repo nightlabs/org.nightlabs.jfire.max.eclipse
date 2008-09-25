8dda190f779f7f36d7df70b62100a97b579945f3
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-25 22:19:49 +0000

    started ui for editing ReportTextPartConfiguration
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@12191 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/articlecontainer/detail/action/print/ArticleContainerReportAction.java
+++ src/org/nightlabs/jfire/trade/ui/articlecontainer/detail/action/print/ArticleContainerReportAction.java
@@ -3,13 +3,9 @@
  */
 package org.nightlabs.jfire.trade.ui.articlecontainer.detail.action.print;
 
-import org.nightlabs.jfire.accounting.id.InvoiceID;
-import org.nightlabs.jfire.reporting.trade.ReportingTradeConstants;
-import org.nightlabs.jfire.store.id.DeliveryNoteID;
 import org.nightlabs.jfire.trade.id.ArticleContainerID;
-import org.nightlabs.jfire.trade.id.OfferID;
-import org.nightlabs.jfire.trade.id.OrderID;
 import org.nightlabs.jfire.trade.ui.articlecontainer.detail.action.ArticleContainerAction;
+import org.nightlabs.jfire.trade.ui.report.ReportTradeUtil;
 
 /**
  * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
@@ -28,15 +24,6 @@ public abstract class ArticleContainerReportAction extends ArticleContainerActio
 	 */
 	protected String getReportRegistryItemType() {
 		ArticleContainerID articleContainerID = getArticleContainerID();
-		
-		if (articleContainerID instanceof InvoiceID)
-			return ReportingTradeConstants.REPORT_REGISTRY_ITEM_TYPE_INVOICE;
-		else if (articleContainerID instanceof OrderID)
-			return ReportingTradeConstants.REPORT_REGISTRY_ITEM_TYPE_ORDER;
-		else if (articleContainerID instanceof OfferID)
-			return ReportingTradeConstants.REPORT_REGISTRY_ITEM_TYPE_OFFER;
-		else if (articleContainerID instanceof DeliveryNoteID)
-			return ReportingTradeConstants.REPORT_REGISTRY_ITEM_TYPE_DELIVERY_NOTE;
-		return null;
+		return ReportTradeUtil.getReportRegistryItemType(articleContainerID);
 	}
 }
