ddc9504e50a0a3c0be180ac78e2f60c9917dbde9
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-30 08:23:53 +0000

    added listener.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@12215 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssueTypeAndStateSection.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/editor/IssueTypeAndStateSection.java
@@ -7,7 +7,11 @@ import javax.jdo.FetchPlan;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -30,7 +34,7 @@ extends AbstractIssueEditorGeneralSection
 {
 	private Label projectLabel;
 	private ProjectComboComposite pc;
-	
+
 	private Label issueTypeLabel;
 	private Label statusLabel;
 
@@ -53,7 +57,7 @@ extends AbstractIssueEditorGeneralSection
 		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
 		gd.horizontalSpan = 3;
 		issueTypeLabel.setLayoutData(gd);
-		
+
 		projectLabel = new Label(getClient(), SWT.WRAP);
 		gd = new GridData();
 		gd.horizontalSpan = 2;
@@ -63,7 +67,14 @@ extends AbstractIssueEditorGeneralSection
 		gd = new GridData();
 		gd.horizontalSpan = 1;
 		pc.setLayoutData(gd);
-		
+		pc.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent e) {
+				getController().getIssue().setProject(pc.getSelectedProject());
+				markDirty();
+			}
+		});
+
 		statusLabel = new Label(getClient(), SWT.WRAP);
 		statusLabel.setText("Status: ");
 		gd = new GridData();
@@ -73,7 +84,7 @@ extends AbstractIssueEditorGeneralSection
 		gd = new GridData();
 		gd.horizontalSpan = 2;
 		currentStateComposite.setLayoutData(gd);
-		
+
 		nextTransitionComposite = new NextTransitionComposite(getClient(), SWT.NONE);
 		nextTransitionComposite.addSignalListener(new SignalListener() {
 			public void signal(SignalEvent event) {
@@ -99,105 +110,105 @@ extends AbstractIssueEditorGeneralSection
 				}
 
 //				if (assignInPossibleTransition(getIssue(), new NullProgressMonitor())) {
-//					if (getIssue().getAssignee() == null) {
-//						if (!isDirty()) {
-//							UserSearchDialog userSearchDialog = new UserSearchDialog(getSection().getShell(), null);
-//							int returnCode = userSearchDialog.open();
-//							if (returnCode == Dialog.OK) {
-//								User assigneeUser = userSearchDialog.getSelectedUser();
-//								if (assigneeUser != null) {
-//									getIssue().setAssignee(assigneeUser);
-//								}
-//							}//if							
-//						}
-//					}
+//				if (getIssue().getAssignee() == null) {
+//				if (!isDirty()) {
+//				UserSearchDialog userSearchDialog = new UserSearchDialog(getSection().getShell(), null);
+//				int returnCode = userSearchDialog.open();
+//				if (returnCode == Dialog.OK) {
+//				User assigneeUser = userSearchDialog.getSelectedUser();
+//				if (assigneeUser != null) {
+//				getIssue().setAssignee(assigneeUser);
+//				}
+//				}//if							
+//				}
+//				}
 //				}
 
-// is this commit necessary? I commented it out.
+//				is this commit necessary? I commented it out.
 //				commit(true);
 
-// instead of signalling it here, we save the whole editor
+//				instead of signalling it here, we save the whole editor
 //				signalIssue(event);
 				getController().setJbpmTransitionName(event.getTransition().getJbpmTransitionName());
 				markDirty();
 				getController().getEntityEditor().doSave(new NullProgressMonitor()); // spawns a job anyway - does nothing expensive on the UI thread.
 			}
 		});
-		
+
 		gd = new GridData();
 		gd.horizontalSpan = 3;
 		nextTransitionComposite.setLayoutData(gd);
 	}
 
 //	/**
-//	 * The fetch groups of issue data.
-//	 */
+//	* The fetch groups of issue data.
+//	*/
 //	public static final String[] FETCH_GROUPS = new String[] {
-//		FetchPlan.DEFAULT, 
-//		Issue.FETCH_GROUP_ISSUE_LOCAL,
-//		IssueType.FETCH_GROUP_NAME,
-//		IssueLocal.FETCH_GROUP_STATE,
-//		IssueLocal.FETCH_GROUP_STATES,
-//		State.FETCH_GROUP_STATE_DEFINITION,
-//		StateDefinition.FETCH_GROUP_NAME};
+//	FetchPlan.DEFAULT, 
+//	Issue.FETCH_GROUP_ISSUE_LOCAL,
+//	IssueType.FETCH_GROUP_NAME,
+//	IssueLocal.FETCH_GROUP_STATE,
+//	IssueLocal.FETCH_GROUP_STATES,
+//	State.FETCH_GROUP_STATE_DEFINITION,
+//	StateDefinition.FETCH_GROUP_NAME};
 
 //	protected void signalIssue(final SignalEvent event) {
-//		Job job = new Job("Performing transition") {
-//			@Override
-//			protected IStatus run(ProgressMonitor monitor)
-//			{
-//				monitor.beginTask("Performing transition of issue", 100);
-//				try {
-//					IssueID issueID = (IssueID)JDOHelper.getObjectId(getIssue());
-//					IssueManager im = IssueManagerUtil.getHome(Login.getLogin().getInitialContextProperties()).create();
-////					Issue issue = im.signalIssue((IssueID)JDOHelper.getObjectId(getIssue()), event.getTransition().getJbpmTransitionName(), 
-////							true, FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-//					im.signalIssue(issueID, event.getTransition().getJbpmTransitionName(), false, null, 1);
-//					// make sure the object cannot be loaded from the cache anymore (we would load an out-dated version, since the notification from the server is surely not yet here)
-//					Cache.sharedInstance().removeByObjectID(issueID, false);
-//					monitor.worked(30);
-//					getController().load(new SubProgressMonitor(monitor, 70));
-//
-////					currentStateComposite.setStatable(issue);
-////					nextTransitionComposite.setStatable(issue);
-//				} catch (Exception x) {
-//					throw new RuntimeException(x);
-//				} finally {
-//					monitor.done();
-//				}
-//				return Status.OK_STATUS;
-//			}
-//		};
-//		job.setPriority(Job.SHORT);
-//		job.setUser(true);
-//		job.schedule();		
+//	Job job = new Job("Performing transition") {
+//	@Override
+//	protected IStatus run(ProgressMonitor monitor)
+//	{
+//	monitor.beginTask("Performing transition of issue", 100);
+//	try {
+//	IssueID issueID = (IssueID)JDOHelper.getObjectId(getIssue());
+//	IssueManager im = IssueManagerUtil.getHome(Login.getLogin().getInitialContextProperties()).create();
+////	Issue issue = im.signalIssue((IssueID)JDOHelper.getObjectId(getIssue()), event.getTransition().getJbpmTransitionName(), 
+////	true, FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+//	im.signalIssue(issueID, event.getTransition().getJbpmTransitionName(), false, null, 1);
+//	// make sure the object cannot be loaded from the cache anymore (we would load an out-dated version, since the notification from the server is surely not yet here)
+//	Cache.sharedInstance().removeByObjectID(issueID, false);
+//	monitor.worked(30);
+//	getController().load(new SubProgressMonitor(monitor, 70));
+
+////	currentStateComposite.setStatable(issue);
+////	nextTransitionComposite.setStatable(issue);
+//	} catch (Exception x) {
+//	throw new RuntimeException(x);
+//	} finally {
+//	monitor.done();
+//	}
+//	return Status.OK_STATUS;
+//	}
+//	};
+//	job.setPriority(Job.SHORT);
+//	job.setUser(true);
+//	job.schedule();		
 //	}
 
 //	protected void signalAssign() {
-//		Job job = new Job("Performing transition") {
-//			@Override
-//			@Implement
-//			protected IStatus run(IProgressMonitor monitor)
-//			{
-//				try {
-//					IssueManager im = IssueManagerUtil.getHome(Login.getLogin().getInitialContextProperties()).create();
-//					Issue issue = im.storeIssue(getController().getIssue(), true, FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-//					issue = im.signalIssue((IssueID)JDOHelper.getObjectId(issue), JbpmConstants.TRANSITION_NAME_ASSIGN, 
-//							true, FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-//
-//					currentStateComposite.setStatable(issue);
-//					nextTransitionComposite.setStatable(issue);
-//
-//					getController().setIssue(issue);
-//				} catch (Exception x) {
-//					throw new RuntimeException(x);
-//				}
-//				return Status.OK_STATUS;
-//			}
-//		};
-//		job.setPriority(Job.SHORT);
-//		job.setUser(true);
-//		job.schedule();
+//	Job job = new Job("Performing transition") {
+//	@Override
+//	@Implement
+//	protected IStatus run(IProgressMonitor monitor)
+//	{
+//	try {
+//	IssueManager im = IssueManagerUtil.getHome(Login.getLogin().getInitialContextProperties()).create();
+//	Issue issue = im.storeIssue(getController().getIssue(), true, FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+//	issue = im.signalIssue((IssueID)JDOHelper.getObjectId(issue), JbpmConstants.TRANSITION_NAME_ASSIGN, 
+//	true, FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+
+//	currentStateComposite.setStatable(issue);
+//	nextTransitionComposite.setStatable(issue);
+
+//	getController().setIssue(issue);
+//	} catch (Exception x) {
+//	throw new RuntimeException(x);
+//	}
+//	return Status.OK_STATUS;
+//	}
+//	};
+//	job.setPriority(Job.SHORT);
+//	job.setUser(true);
+//	job.schedule();
 //	}
 
 	protected void doSetIssue(Issue issue) {
@@ -206,14 +217,14 @@ extends AbstractIssueEditorGeneralSection
 						"Issue type: %s", 
 						issue.getIssueType().getName().getText())
 		);
-		
+
 		projectLabel.setText(
 				String.format(
-						"Project:")
+				"Project:")
 		);
-		
+
 		pc.setSelectedProject(issue.getProject());
-		
+
 		currentStateComposite.setStatable(issue);
 		nextTransitionComposite.setStatable(issue);		
 	}
