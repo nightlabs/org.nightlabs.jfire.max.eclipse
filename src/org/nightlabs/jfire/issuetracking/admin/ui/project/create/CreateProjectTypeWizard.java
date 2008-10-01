66ea50eda6b136d02eba8c2b3c362d8a1cec529a
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-10-01 07:48:45 +0000

    created wizard.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.admin.ui@12224 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/issuetracking/admin/ui/project/create/CreateProjectTypeWizard.java
@@ -0,0 +1,68 @@
+package org.nightlabs.jfire.issuetracking.admin.ui.project.create;
+
+import javax.jdo.FetchPlan;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swt.widgets.Display;
+import org.nightlabs.annotation.Implement;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.base.ui.wizard.DynamicPathWizard;
+import org.nightlabs.jfire.issue.project.id.ProjectID;
+import org.nightlabs.jfire.issuetracking.admin.ui.project.ProjectEditor;
+import org.nightlabs.jfire.issuetracking.admin.ui.project.ProjectEditorInput;
+
+public class CreateProjectTypeWizard
+extends DynamicPathWizard
+{
+	private static String[] FETCH_GROUPS_PROJECT_TYPE = {
+		FetchPlan.DEFAULT
+	};
+
+	private ProjectID parentProjectID;
+	private CreateProjectTypeNameWizardPage namePage;
+
+	public CreateProjectTypeWizard(ProjectID parentProjectID) {
+		this.parentProjectID = parentProjectID;
+	}
+
+	@Override
+	public void addPages() {
+		namePage = new CreateProjectTypeNameWizardPage(parentProjectID);
+		addPage(namePage);
+	}
+
+	@Override
+	@Implement
+	public boolean performFinish()
+	{
+
+		Job job = new Job("Storing Project") {
+			@Override
+			@Implement
+			protected IStatus run(IProgressMonitor monitor)
+			{
+
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run()
+					{
+						try {
+							RCPUtil.openEditor(
+							new ProjectEditorInput(parentProjectID),
+							ProjectEditor.EDITOR_ID);
+						} catch (Exception e) {
+							throw new RuntimeException(e);
+						}
+					}
+				});
+				return Status.OK_STATUS;
+			}
+		};
+		job.setUser(true);
+		job.setPriority(Job.SHORT);
+		job.schedule();
+		return true;
+	}
+}
