28c806d58c94380b72fc58a0e6b0b757a35de8c4
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-10-02 03:11:26 +0000

    moved classes.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@12244 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/issuetracking/ui/project/create/CreateProjectAction.java
@@ -0,0 +1,63 @@
+package org.nightlabs.jfire.issuetracking.ui.project.create;
+
+import java.util.Locale;
+
+import javax.jdo.FetchPlan;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.nightlabs.base.ui.resource.SharedImages;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.base.ui.login.Login;
+import org.nightlabs.jfire.idgenerator.IDGenerator;
+import org.nightlabs.jfire.issue.project.Project;
+import org.nightlabs.jfire.issue.project.ProjectDAO;
+import org.nightlabs.jfire.issuetracking.ui.IssueTrackingPlugin;
+import org.nightlabs.jfire.issuetracking.ui.project.ProjectAdminTreeComposite;
+import org.nightlabs.progress.NullProgressMonitor;
+
+public class CreateProjectAction 
+extends Action 
+{
+	private InputDialog dialog;
+	public CreateProjectAction() {
+		setId(CreateProjectAction.class.getName());
+		setImageDescriptor(SharedImages.getSharedImageDescriptor(
+				IssueTrackingPlugin.getDefault(), 
+				ProjectAdminTreeComposite.class, 
+		"Create"));
+		setToolTipText("Create Project");
+		setText("Create Project");
+	}
+
+	@Override
+	public void run() {
+		dialog = new InputDialog(RCPUtil.getActiveShell(), "Create Project", "Enter project's name", "Name", null) {
+			@Override
+			protected void okPressed() {
+				try {
+					Project project = new Project(Login.getLogin().getOrganisationID(), IDGenerator.nextID(Project.class));
+					project.getName().setText(Locale.ENGLISH.getLanguage(), getValue());
+
+					ProjectDAO.sharedInstance().storeProject(project, false, new String[]{FetchPlan.DEFAULT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, new NullProgressMonitor());
+					dialog.close();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			};
+
+			@Override
+			protected Control createDialogArea(Composite parent) {
+				Control dialogArea = super.createDialogArea(parent);
+				return dialogArea;
+			}
+		};
+
+		if (dialog.open() != Window.OK)
+			return;
+	}		
+}
