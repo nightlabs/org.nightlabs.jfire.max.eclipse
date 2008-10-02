5392d6268af482b4f119cefef9a99be0b0b72790
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-10-02 12:08:43 +0000

    added time pattern composite
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@12259 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/articlecontainer/detail/recurring/RecurringTimingConfigSection.java
+++ src/org/nightlabs/jfire/trade/ui/articlecontainer/detail/recurring/RecurringTimingConfigSection.java
@@ -1,51 +1,55 @@
 package org.nightlabs.jfire.trade.ui.articlecontainer.detail.recurring;
 
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.ui.forms.editor.FormPage;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.nightlabs.base.ui.timepattern.TimePatternSetComposite;
+import org.nightlabs.base.ui.timepattern.builder.TimePatternSetBuilderWizard;
 
 import org.nightlabs.jfire.trade.recurring.RecurringOfferConfiguration;
 
+
+
+/**
+ * @author Fitas Amine <!-- fitas [AT] nightlabs [DOT] de -->
+ *
+ */
 public class RecurringTimingConfigSection extends AbstractRecurringConfigGeneralSection{
 
 
 	private RecurringOfferConfigurationPageController controller;
-
-	private Button buildTimePattern;
-
-
-
+	private Button taskTimePattern;
+	private TimePatternSetComposite timePatternSetComposite;
+	
 
 	public RecurringTimingConfigSection(FormPage page, Composite parent, final RecurringOfferConfigurationPageController controller) {
 
 		super(page, parent, controller);
-		getSection().setText("Timer");
+		getSection().setText("Recurring Timer");
+		getClient().getGridLayout().numColumns = 3;
+		getClient().getGridLayout().makeColumnsEqualWidth = false;
 
 
-//		buildTimePattern = new Button(above, SWT.PUSH);
-//		buildTimePattern.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END
-//		| GridData.GRAB_HORIZONTAL));
-//		buildTimePattern.setText(AccountingPlugin.getResourceString("edit.task.accountingTaskDetailSection.buildTimePattern"));		
-//		buildTimePattern.addSelectionListener(new SelectionListener() {
-//		public void widgetDefaultSelected(SelectionEvent e) {
-//		}
-//		public void widgetSelected(SelectionEvent e) {
-//		if
-//		(TimePatternSetBuilderWizard.open(timePatternSetComposite.getTimePatternSet()))
-//		{
-//		timePatternSetComposite.refresh(true);
-//		AccountingTaskDetailSection.this.markDirty();
-//		}
-//		}
-//		});
-//		timePatternSetComposite = new TimePatternSetComposite(timePatternWrapper,
+		taskTimePattern = new Button(getClient(), SWT.PUSH);
+		taskTimePattern.setText("Task Timer...");		
+		taskTimePattern.addSelectionListener(new SelectionListener() {
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+			public void widgetSelected(SelectionEvent e) {
+				if
+				(TimePatternSetBuilderWizard.open(timePatternSetComposite.getTimePatternSet()))
+				{
+					timePatternSetComposite.refresh(true);
+					RecurringTimingConfigSection.this.markDirty();
+				}
+			}
+		});
 
-
-
-
-
-
-
+		timePatternSetComposite = new TimePatternSetComposite(getClient(),
+				SWT.NONE);
 
 	}
 
