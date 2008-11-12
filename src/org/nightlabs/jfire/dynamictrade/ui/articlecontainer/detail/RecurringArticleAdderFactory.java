3ab297bb3a1bdaebc800a307ae0421fac4d11651
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-11-12 08:29:08 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.dynamictrade.ui@12778 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/ui/articlecontainer/detail/RecurringArticleAdderFactory.java
+++ src/org/nightlabs/jfire/dynamictrade/ui/articlecontainer/detail/RecurringArticleAdderFactory.java
@@ -5,7 +5,7 @@ import org.nightlabs.jfire.trade.ui.articlecontainer.detail.SegmentEdit;
 
 public class RecurringArticleAdderFactory 
 extends AbstractArticleAdderFactory{
-	
+	@Override
 	public RecurringArticleAdder createArticleAdder(SegmentEdit segmentEdit)
 	{
 		RecurringArticleAdder articleAdder = new RecurringArticleAdder();
