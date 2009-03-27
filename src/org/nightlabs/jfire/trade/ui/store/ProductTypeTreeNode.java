9f1e96024c86140ab5a8fe34455cc8c8eadaf90e
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-27 15:47:19 +0000

    replaced ProductTypePackageTree with generic ProductTypeTree in AddMoneyFlowMappingWizard
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@13712 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/trade/ui/store/ProductTypeTreeNode.java
@@ -0,0 +1,15 @@
+package org.nightlabs.jfire.trade.ui.store;
+
+import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeNode;
+import org.nightlabs.jfire.store.ProductType;
+import org.nightlabs.jfire.store.id.ProductTypeID;
+
+/**
+ * @author Daniel Mazurek - Daniel.Mazurek [dot] nightlabs [dot] de
+ *
+ */
+public class ProductTypeTreeNode
+extends JDOObjectTreeNode<ProductTypeID, ProductType, ActiveProductTypeTreeController>
+{
+
+}
