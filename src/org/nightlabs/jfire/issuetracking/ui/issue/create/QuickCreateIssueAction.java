18a3cd64dd4d247aa5f765cbcfa08a7040621e62
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-31 15:35:04 +0000

    added an action to issue perspective.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@13737 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/create/QuickCreateIssueAction.java
@@ -0,0 +1,34 @@
+package org.nightlabs.jfire.issuetracking.ui.issue.create;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class QuickCreateIssueAction implements IWorkbenchWindowActionDelegate {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IWorkbenchWindow arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(IAction arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void selectionChanged(IAction arg0, ISelection arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
