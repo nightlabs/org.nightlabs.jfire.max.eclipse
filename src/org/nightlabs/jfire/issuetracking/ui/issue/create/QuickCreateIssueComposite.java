3df890efd06c19864f5f0fe3418239eb54fe9590
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-31 17:03:52 +0000

    added all widgets for fast creating issue dialog.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@13742 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/create/QuickCreateIssueComposite.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/create/QuickCreateIssueComposite.java
@@ -3,10 +3,18 @@ package org.nightlabs.jfire.issuetracking.ui.issue.create;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.nightlabs.base.ui.composite.DateTimeControl;
 import org.nightlabs.base.ui.composite.XComposite;
+import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
+import org.nightlabs.base.ui.language.I18nTextEditor;
+import org.nightlabs.base.ui.language.I18nTextEditorMultiLine;
 import org.nightlabs.jfire.issue.Issue;
 import org.nightlabs.jfire.issuetracking.ui.department.DepartmentComboComposite;
 import org.nightlabs.jfire.issuetracking.ui.project.ProjectComboComposite;
+import org.nightlabs.jfire.trade.ui.resource.Messages;
+import org.nightlabs.l10n.DateFormatter;
 /**
  * A composite that contains UIs for adding {@link Issue}.
  * 
@@ -28,19 +36,55 @@ extends XComposite
 	}
 
 	private void createComposite() {
-		getGridLayout().numColumns = 2;
+		getGridLayout().numColumns = 1;
 		getGridLayout().makeColumnsEqualWidth = false;
 		getGridData().grabExcessHorizontalSpace = true;
 		
 		XComposite mainComposite = new XComposite(this, SWT.NONE,
 				LayoutMode.TIGHT_WRAPPER);
+		mainComposite.getGridLayout().numColumns = 4;
 		
 		ProjectComboComposite projectComboComposite = new ProjectComboComposite(mainComposite, SWT.None);
-		GridData gridData = new GridData(GridData.FILL_BOTH);
+		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
 		projectComboComposite.setLayoutData(gridData);
 		
 		DepartmentComboComposite departmentComboComposite = new DepartmentComboComposite(mainComposite, SWT.None);
-		gridData = new GridData(GridData.FILL_BOTH);
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
 		departmentComboComposite.setLayoutData(gridData);
+		
+		XComposite timeComposite = new XComposite(mainComposite, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		timeComposite.setLayoutData(gridData);
+		
+		new Label(timeComposite, SWT.NONE).setText("Start Time");
+		DateTimeControl startTimeControl  = new DateTimeControl(timeComposite, SWT.NONE, DateFormatter.FLAGS_DATE_SHORT_TIME_HM);
+		startTimeControl.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		XComposite durationComposite = new XComposite(mainComposite, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		durationComposite.setLayoutData(gridData);
+		
+		new Label(durationComposite, SWT.NONE).setText("Duration");
+		Text durationText = new Text(durationComposite, SWT.BORDER);
+		durationText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		
+		//////////////////////////////////////////////////////////////
+		XComposite subjectDescriptionComposite = new XComposite(mainComposite, SWT.NONE);
+		gridData = new GridData(GridData.FILL_BOTH);
+		gridData.horizontalSpan = 4;
+		subjectDescriptionComposite.setLayoutData(gridData);
+		
+		Label subjectLabel = new Label(subjectDescriptionComposite, SWT.WRAP);
+		subjectLabel.setLayoutData(new GridData());
+		subjectLabel.setText("Subject");
+		
+		I18nTextEditor subjectText = new I18nTextEditor(subjectDescriptionComposite);
+		
+		Label descriptionLabel = new Label(subjectDescriptionComposite, SWT.WRAP);
+		descriptionLabel.setLayoutData(new GridData());
+		descriptionLabel.setText("Description");
+		
+		I18nTextEditorMultiLine descriptionText = new I18nTextEditorMultiLine(subjectDescriptionComposite, subjectText.getLanguageChooser());
 	}
 }
