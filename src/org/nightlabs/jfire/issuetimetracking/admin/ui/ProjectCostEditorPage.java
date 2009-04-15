225a01454bff4e858e8abd9532f5c919e417b9f5
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-04-15 15:35:27 +0000

    -added some uis -modified classpath
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetimetracking.admin.ui@13920 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetimetracking/admin/ui/ProjectCostEditorPage.java
+++ src/org/nightlabs/jfire/issuetimetracking/admin/ui/ProjectCostEditorPage.java
@@ -1,20 +1,35 @@
 package org.nightlabs.jfire.issuetimetracking.admin.ui;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.IFormPage;
+import org.nightlabs.base.ui.composite.ListComposite;
 import org.nightlabs.base.ui.composite.XComposite;
+import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
 import org.nightlabs.base.ui.entity.editor.EntityEditor;
 import org.nightlabs.base.ui.entity.editor.EntityEditorPageControllerModifyEvent;
 import org.nightlabs.base.ui.entity.editor.EntityEditorPageWithProgress;
 import org.nightlabs.base.ui.entity.editor.IEntityEditorPageController;
 import org.nightlabs.base.ui.entity.editor.IEntityEditorPageFactory;
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.base.ui.login.Login;
 import org.nightlabs.jfire.issuetracking.ui.project.ProjectEditorPageController;
+import org.nightlabs.jfire.security.User;
+import org.nightlabs.jfire.security.dao.UserDAO;
+import org.nightlabs.jfire.trade.ui.currency.CurrencyCombo;
+import org.nightlabs.progress.NullProgressMonitor;
 
 public class ProjectCostEditorPage 
 extends EntityEditorPageWithProgress 
@@ -63,11 +78,88 @@ extends EntityEditorPageWithProgress
 				  SWT.V_SCROLL);
 		sc.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
-		final XComposite c = new XComposite(sc, SWT.NONE);
-		GridLayout layout = (GridLayout)c.getLayout();
+		final XComposite mainComposite = new XComposite(sc, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
+		GridLayout layout = (GridLayout)mainComposite.getLayout();
 		layout.makeColumnsEqualWidth = true;
 
-		sc.setContent(c);
+		XComposite costComposite = new XComposite(mainComposite, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
+		costComposite.getGridLayout().numColumns = 2;
+		
+		new Label(costComposite, SWT.NONE).setText("Currency");
+		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		CurrencyCombo currencyCombo = new CurrencyCombo(costComposite, SWT.NONE);
+		currencyCombo.setLayoutData(gridData);
+		
+		new Label(costComposite, SWT.NONE).setText("Monthly Cost");
+		Text costText = new Text(costComposite, SWT.SINGLE);
+		costText.setLayoutData(gridData);
+		
+		new Label(costComposite, SWT.NONE).setText("Monthly Revenue");
+		Text revenueText = new Text(costComposite, SWT.SINGLE);
+		revenueText.setLayoutData(gridData);
+
+		XComposite userComposite = new XComposite(mainComposite, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
+		userComposite.getGridLayout().numColumns = 2;
+		
+		final ListComposite<User> userList = new ListComposite<User>(userComposite, SWT.NONE);
+		userList.setLabelProvider(new LabelProvider() {
+			@Override
+			public String getText(Object element) {
+				if (element instanceof User) {
+					User user = (User) element;
+					return user.getName();
+				}
+				return "";
+			}
+		});
+		
+		Job job = new Job("Loading Users................") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					final java.util.List<User> users = UserDAO.sharedInstance().getUsers(
+							Login.getLogin().getOrganisationID(),
+							(String[]) null,
+							new String[] {
+								User.FETCH_GROUP_NAME
+							},
+							NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT,
+							new NullProgressMonitor()
+					);
+					
+					Display.getDefault().asyncExec(new Runnable()
+					{
+						public void run()
+						{
+							userList.setInput(users);
+						}
+					});
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+				
+				return Status.OK_STATUS;
+			}
+		};
+		job.setPriority(Job.SHORT);
+		job.schedule();
+		
+		XComposite detailComposite = new XComposite(userComposite, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
+		detailComposite.getGridLayout().numColumns = 2;
+		
+		new Label(detailComposite, SWT.NONE).setText("Currency");
+		CurrencyCombo currencyCombo2 = new CurrencyCombo(detailComposite, SWT.NONE);
+		currencyCombo2.setLayoutData(gridData);
+		
+		new Label(detailComposite, SWT.NONE).setText("Monthly Cost");
+		Text costText2 = new Text(detailComposite, SWT.SINGLE);
+		costText2.setLayoutData(gridData);
+		
+		new Label(detailComposite, SWT.NONE).setText("Monthly Revenue");
+		Text revenueText2 = new Text(detailComposite, SWT.SINGLE);
+		revenueText2.setLayoutData(gridData);
+
+		sc.setContent(mainComposite);
 		sc.setExpandHorizontal(true);
 		sc.setExpandVertical(true);
 
