81301aa414b65fecc571979cd772d33c4d35bae1
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-04-16 17:58:46 +0000

    externalized strings + german translation
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.voucher.admin.ui@13942 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/voucher/admin/ui/editor/overview/VoucherLayoutOverviewPageStatusProvider.java
+++ src/org/nightlabs/jfire/voucher/admin/ui/editor/overview/VoucherLayoutOverviewPageStatusProvider.java
@@ -6,6 +6,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.nightlabs.jfire.store.ProductType;
 import org.nightlabs.jfire.trade.admin.ui.editor.overview.AbstractProductTypeOverviewPageStatusProvider;
+import org.nightlabs.jfire.voucher.admin.ui.resource.Messages;
 import org.nightlabs.jfire.voucher.scripting.VoucherLayout;
 import org.nightlabs.jfire.voucher.store.VoucherType;
 
@@ -28,8 +29,8 @@ extends AbstractProductTypeOverviewPageStatusProvider
 			VoucherType voucherType = (VoucherType) productType;
 			VoucherLayout voucherLayout = voucherType.getVoucherLayout();
 			StringBuilder sb = new StringBuilder();
-			sb.append("VoucherLayout: ");
-			String name = "NONE";
+			sb.append(Messages.getString("org.nightlabs.jfire.voucher.admin.ui.editor.overview.VoucherLayoutOverviewPageStatusProvider.label.voucherLayout")); //$NON-NLS-1$
+			String name = Messages.getString("org.nightlabs.jfire.voucher.admin.ui.editor.overview.VoucherLayoutOverviewPageStatusProvider.label.none"); //$NON-NLS-1$
 			int severity = IStatus.ERROR;
 			if (voucherLayout != null) {
 				name = voucherLayout.getFileName();
