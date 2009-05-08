bad05c627431079922a2c052d652b7980a102c02
khaireel <khaireel@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-08 16:14:00 +0000

    AbstractProductTypeAdminEditor now extends ActiveEntityEditor. Woo hoo!
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.admin.ui@14515 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/admin/ui/editor/AbstractProductTypeAdminEditor.java
+++ src/org/nightlabs/jfire/trade/admin/ui/editor/AbstractProductTypeAdminEditor.java
@@ -3,26 +3,22 @@ package org.nightlabs.jfire.trade.admin.ui.editor;
 import javax.jdo.FetchPlan;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IPropertyListener;
 import org.eclipse.ui.PartInitException;
-import org.nightlabs.base.ui.entity.editor.EntityEditor;
-import org.nightlabs.base.ui.job.Job;
 import org.nightlabs.jfire.base.ui.editlock.EditLockCallback;
 import org.nightlabs.jfire.base.ui.editlock.EditLockCarrier;
 import org.nightlabs.jfire.base.ui.editlock.EditLockHandle;
 import org.nightlabs.jfire.base.ui.editlock.EditLockMan;
 import org.nightlabs.jfire.base.ui.editlock.InactivityAction;
+import org.nightlabs.jfire.base.ui.entity.editor.ActiveEntityEditor;
 import org.nightlabs.jfire.base.ui.login.part.ICloseOnLogoutEditorPart;
 import org.nightlabs.jfire.editlock.EditLock;
 import org.nightlabs.jfire.store.ProductType;
 import org.nightlabs.jfire.store.dao.ProductTypeDAO;
+import org.nightlabs.jfire.store.id.ProductTypeID;
 import org.nightlabs.jfire.trade.JFireTradeEAR;
 import org.nightlabs.jfire.trade.admin.ui.resource.Messages;
 import org.nightlabs.progress.ProgressMonitor;
@@ -37,16 +33,16 @@ import org.nightlabs.progress.ProgressMonitor;
  *
  * @author Marius Heinzmann [marius[at]NightLabs[dot]de]
  * @author Tobias Langner <!-- tobias[dot]langner[at]nightlabs[dot]de -->
+ * @author Khaireel Mohamed - khaireel at nightlabs dot de
  */
-public abstract class AbstractProductTypeAdminEditor
-extends EntityEditor
+public abstract class AbstractProductTypeAdminEditor extends ActiveEntityEditor //EntityEditor
 implements ICloseOnLogoutEditorPart
 {
-	private String managedBy = null;
+	private final String managedBy = null;
 
 	private EditLockHandle editLockHandle;
 
-	private EditLockCallback editLockCallback = new EditLockCallback() {
+	private final EditLockCallback editLockCallback = new EditLockCallback() {
 		@Override
 		public InactivityAction getEditLockAction(EditLockCarrier editLockCarrier) {
 			if (!isDirty())
@@ -67,7 +63,7 @@ implements ICloseOnLogoutEditorPart
 		}
 	};
 
-	private IPropertyListener dirtyStateListener = new IPropertyListener() {
+	private final IPropertyListener dirtyStateListener = new IPropertyListener() {
 		public void propertyChanged(Object source, int propID) {
 			if (PROP_DIRTY == propID) {
 				editLockHandle.refresh();
@@ -82,37 +78,50 @@ implements ICloseOnLogoutEditorPart
 				JFireTradeEAR.EDIT_LOCK_TYPE_ID_PRODUCT_TYPE, ((ProductTypeEditorInput)input).getJDOObjectID(),
 				Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.editLock.description"), editLockCallback //$NON-NLS-1$
 		);
-
-		addPropertyListener(dirtyStateListener);
-		Job loadPropertiesJob = new Job(Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.loadTitleJob.name")) { //$NON-NLS-1$
-			@Override
-			protected IStatus run(ProgressMonitor monitor) throws Exception {
-				final ProductType productType = ProductTypeDAO.sharedInstance().getProductType(
-						((ProductTypeEditorInput)getEditorInput()).getJDOObjectID(),
-						new String[] { FetchPlan.DEFAULT, ProductType.FETCH_GROUP_NAME, ProductType.FETCH_GROUP_PRODUCT_TYPE_LOCAL },
-						1, monitor);
-				Display.getDefault().asyncExec(new Runnable() {
-					public void run() {
-						String partTitle = productType.getName().getText();
-						managedBy = productType.getProductTypeLocal().getManagedBy();
-						if (managedBy != null) {
-							MessageDialog.openInformation(site.getShell(),
-									Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.dialog.title"), //$NON-NLS-1$
-									String.format(
-											Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.dialog.message"), //$NON-NLS-1$
-											partTitle, managedBy)
-									);
-							partTitle += String.format(Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.managedBy"), managedBy); //$NON-NLS-1$
-						}
-						setPartName(partTitle);
-						setTitle(partTitle);
-					}
-				});
-				return Status.OK_STATUS;
-			}
-		};
-		loadPropertiesJob.schedule();
+//
+//		addPropertyListener(dirtyStateListener);
+//		Job loadPropertiesJob = new Job(Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.loadTitleJob.name")) { //$NON-NLS-1$
+//			@Override
+//			protected IStatus run(ProgressMonitor monitor) throws Exception {
+//				final ProductType productType = ProductTypeDAO.sharedInstance().getProductType(
+//						((ProductTypeEditorInput)getEditorInput()).getJDOObjectID(),
+//						new String[] { FetchPlan.DEFAULT, ProductType.FETCH_GROUP_NAME, ProductType.FETCH_GROUP_PRODUCT_TYPE_LOCAL },
+//						1, monitor);
+//				Display.getDefault().asyncExec(new Runnable() {
+//					public void run() {
+//						String partTitle = productType.getName().getText();
+//						managedBy = productType.getProductTypeLocal().getManagedBy();
+//						if (managedBy != null) {
+//							MessageDialog.openInformation(site.getShell(),
+//									Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.dialog.title"), //$NON-NLS-1$
+//									String.format(
+//											Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.dialog.message"), //$NON-NLS-1$
+//											partTitle, managedBy)
+//									);
+//							partTitle += String.format(Messages.getString("org.nightlabs.jfire.trade.admin.ui.editor.AbstractProductTypeAdminEditor.managedBy"), managedBy); //$NON-NLS-1$
+//						}
+//						setPartName(partTitle);
+//						setTitle(partTitle);
+//					}
+//				});
+//				return Status.OK_STATUS;
+//			}
+//		};
+//		loadPropertiesJob.schedule();
 	}
+
+	@Override
+	protected String getEditorTitleFromEntity(Object entity) {
+		return entity instanceof ProductType ? ((ProductType)entity).getName().getText() : null;
+	}
+
+	@Override
+	protected Object retrieveEntityForEditorTitle(ProgressMonitor monitor) {
+		ProductTypeID productTypeID = ((ProductTypeEditorInput)getEditorInput()).getJDOObjectID();
+		assert productTypeID != null;
+		return ProductTypeDAO.sharedInstance().getProductType(productTypeID, new String[] { FetchPlan.DEFAULT, ProductType.FETCH_GROUP_NAME, ProductType.FETCH_GROUP_PRODUCT_TYPE_LOCAL }, 1, monitor);
+	}
+
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
