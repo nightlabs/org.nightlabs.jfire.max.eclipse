db91253227dfa3f564eaa8136b9536662f3ac3fa
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-11 18:57:07 +0000

    introduced projects org.nightlabs.jfire.trade + org.nightlabs.jfire.trade.ui
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.admin.ui@14531 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/admin/ui/layout/ILayoutPreviewRenderer.java
+++
@@ -1,8 +0,0 @@
-package org.nightlabs.jfire.trade.admin.ui.layout;
-
-import org.eclipse.swt.graphics.Image;
-import org.nightlabs.jfire.trade.ILayout;
-
-public interface ILayoutPreviewRenderer<L extends ILayout> {
-	public Image renderPreview(L layout, int maxWidth, int maxHeight);
-}
