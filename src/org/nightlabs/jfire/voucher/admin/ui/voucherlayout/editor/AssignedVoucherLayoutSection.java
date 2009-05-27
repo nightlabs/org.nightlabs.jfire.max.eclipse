b158585a717a3b5b56bbbfe50a498ed9a2e41306
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-27 16:58:23 +0000

    removed dependency org.nightlabs.reporting.ui from org.nightlabs.jfire.trade.ui
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.voucher.admin.ui@14679 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/voucher/admin/ui/voucherlayout/editor/AssignedVoucherLayoutSection.java
+++ src/org/nightlabs/jfire/voucher/admin/ui/voucherlayout/editor/AssignedVoucherLayoutSection.java
@@ -1,5 +1,7 @@
 package org.nightlabs.jfire.voucher.admin.ui.voucherlayout.editor;
 
+import java.text.DateFormat;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
@@ -14,8 +16,6 @@ import org.nightlabs.jfire.voucher.admin.ui.editor.VoucherTypeDetailPageControll
 import org.nightlabs.jfire.voucher.admin.ui.resource.Messages;
 import org.nightlabs.jfire.voucher.scripting.VoucherLayout;
 
-import com.ibm.icu.text.DateFormat;
-
 public class AssignedVoucherLayoutSection extends MessageSectionPart {
 
 	private Text voucherLayoutNameText;
@@ -24,28 +24,28 @@ public class AssignedVoucherLayoutSection extends MessageSectionPart {
 
 	public AssignedVoucherLayoutSection(VoucherLayoutPage page, Composite parent) {
 		super(page, parent, ExpandableComposite.TITLE_BAR, Messages.getString("org.nightlabs.jfire.voucher.admin.ui.voucherlayout.editor.AssignedVoucherLayoutSection.title")); //$NON-NLS-1$
-		
+
 		XComposite comp = new XComposite(getContainer(), SWT.NONE, LayoutMode.TIGHT_WRAPPER, LayoutDataMode.GRID_DATA_HORIZONTAL, 2);
 		new Label(comp, SWT.WRAP).setText(Messages.getString("org.nightlabs.jfire.voucher.admin.ui.voucherlayout.editor.AssignedVoucherLayoutSection.assignedVoucherLayoutLabel.text")); //$NON-NLS-1$
 		voucherLayoutNameText = new Text(comp, SWT.READ_ONLY | comp.getBorderStyle());
 		voucherLayoutNameText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
+
 		this.voucherLayoutPage = page;
 	}
-	
+
 	public void setVoucherLayout(VoucherLayout voucherLayout) {
 		if (voucherLayout == null)
 			voucherLayoutNameText.setText(Messages.getString("org.nightlabs.jfire.voucher.admin.ui.voucherlayout.editor.AssignedVoucherLayoutSection.noVoucherLayoutAssignedPlaceholder")); //$NON-NLS-1$
 		else
 			voucherLayoutNameText.setText(voucherLayout.getFileName() + " (" + DateFormat.getDateTimeInstance().format(voucherLayout.getFileTimestamp()) + ")"); //$NON-NLS-1$ //$NON-NLS-2$
-		
+
 		this.assignedVoucherLayout = voucherLayout;
 	}
-	
+
 	@Override
 	public void commit(boolean onSave) {
 		super.commit(onSave);
-		
+
 		((VoucherTypeDetailPageController) voucherLayoutPage.getPageController()).setVoucherLayout(assignedVoucherLayout);
 	}
 }
