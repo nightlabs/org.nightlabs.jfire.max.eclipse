db263ddf3bbb3f2db6d67658efdb14b1333a6582
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-28 13:17:41 +0000

    added the UI Lazy TreeList that uses the LazyTreeController
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@14689 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/trade/ui/store/ProductTypeLazyTree.java
@@ -0,0 +1,170 @@
+package org.nightlabs.jfire.trade.ui.store;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.part.DrillDownAdapter;
+import org.nightlabs.base.ui.resource.SharedImages;
+import org.nightlabs.base.ui.tree.AbstractTreeComposite;
+import org.nightlabs.jfire.base.ui.jdo.tree.lazy.JDOLazyTreeNodesChangedEvent;
+import org.nightlabs.jfire.base.ui.jdo.tree.lazy.JDOLazyTreeNodesChangedEventHandler;
+import org.nightlabs.jfire.base.ui.jdo.tree.lazy.JDOObjectLazyTreeContentProvider;
+import org.nightlabs.jfire.base.ui.jdo.tree.lazy.JDOObjectLazyTreeLabelProvider;
+import org.nightlabs.jfire.store.ProductType;
+import org.nightlabs.jfire.store.id.ProductTypeID;
+import org.nightlabs.jfire.trade.ui.TradePlugin;
+
+/**
+ * @author Fitas Amine - fitas [dot] nightlabs [dot] de
+ *
+ */
+public class ProductTypeLazyTree 
+extends AbstractTreeComposite<ProductType>
+{
+	protected static class ProductTypeTreeContentProvider
+	extends JDOObjectLazyTreeContentProvider<ProductTypeID, ProductType, ProductTypeLazyTreeNode>
+	{
+
+	}
+
+	private ActiveProductTypeLazyTreeController activeProductTypeTreeController;
+
+	public ProductTypeLazyTree(Composite parent, int treeStyle)
+	{
+		super(parent, treeStyle|SWT.VIRTUAL, true, true, false);
+		activeProductTypeTreeController = new ActiveProductTypeLazyTreeController()
+		{
+			@Override
+			protected void onJDOObjectsChanged(JDOLazyTreeNodesChangedEvent<ProductTypeID, ProductTypeLazyTreeNode> changedEvent)
+			{
+				JDOLazyTreeNodesChangedEventHandler.handle(getTreeViewer(), changedEvent);
+			}
+		};
+
+		setInput(activeProductTypeTreeController);
+		addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e)
+			{
+				activeProductTypeTreeController.close();
+				activeProductTypeTreeController = null;
+			}
+		});
+
+		drillDownAdapter = new DrillDownAdapter(getTreeViewer());
+		hookContextMenu();
+	}
+
+	public ProductTypeLazyTree(Composite parent)
+	{
+		this(parent, DEFAULT_STYLE_SINGLE);
+	}
+
+	private void hookContextMenu() {
+		MenuManager menuMgr = new MenuManager("#PopupMenu"); //$NON-NLS-1$
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				ProductTypeLazyTree.this.fillContextMenu(manager);
+			}
+		});
+		Menu menu = menuMgr.createContextMenu(getTreeViewer().getControl());
+		getTreeViewer().getControl().setMenu(menu);
+//		if (getSite() != null)
+//			getSite().registerContextMenu(menuMgr, getTreeViewer());
+	}
+
+	/**
+	 * Contains instances of both, {@link IContributionItem} and {@link IAction}
+	 */
+	private List<Object> contextMenuContributions;
+
+	public void addContextMenuContribution(IContributionItem contributionItem)
+	{
+		if (contextMenuContributions == null)
+			contextMenuContributions = new LinkedList<Object>();
+
+		contextMenuContributions.add(contributionItem);
+	}
+
+	public void addContextMenuContribution(IAction action)
+	{
+		if (contextMenuContributions == null)
+			contextMenuContributions = new LinkedList<Object>();
+
+		contextMenuContributions.add(action);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		if (contextMenuContributions != null) {
+			for (Object contextMenuContribution : contextMenuContributions) {
+				if (contextMenuContribution instanceof IContributionItem)
+					manager.add((IContributionItem)contextMenuContribution);
+				else if (contextMenuContribution instanceof IAction)
+					manager.add((IAction)contextMenuContribution);
+				else
+					throw new IllegalStateException("How the hell got an instance of " + (contextMenuContribution == null ? "null" : contextMenuContribution.getClass()) + " in the contextMenuContributions list?!"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			}
+		}
+
+		drillDownAdapter.addNavigationActions(manager);
+
+		// Other plug-ins can contribute their actions here
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+
+	protected DrillDownAdapter drillDownAdapter;
+
+	protected static class ProductTypeTreeLabelProvider extends JDOObjectLazyTreeLabelProvider<ProductTypeID, ProductType, ProductTypeLazyTreeNode>
+	{
+		@Override
+		protected String getJDOObjectText(ProductType jdoObject, int columnIndex) {
+			return jdoObject.getName().getText();
+		}
+
+		@Override
+		protected Image getJDOObjectImage(ProductType productType, int columnIndex) {
+			if (columnIndex == 0)
+				return SharedImages.getSharedImage(TradePlugin.getDefault(),
+						ProductTypeTreeLabelProvider.class, productType.getInheritanceNatureString());
+
+			return super.getJDOObjectImage(productType, columnIndex);
+		}
+	}
+
+	@Override
+	public void createTreeColumns(Tree tree)
+	{
+//		TreeColumn column = new TreeColumn(tree, SWT.LEFT);
+	}
+
+	@Override
+	public void setTreeProvider(TreeViewer treeViewer)
+	{
+		treeViewer.setContentProvider(new ProductTypeTreeContentProvider());
+		treeViewer.setLabelProvider(new ProductTypeTreeLabelProvider());
+	}
+
+	@Override
+	protected ProductType getSelectionObject(Object obj)
+	{
+		if (obj instanceof ProductTypeLazyTreeNode)
+			return ((ProductTypeLazyTreeNode)obj).getJdoObject();
+
+		return null;
+	}
+}
