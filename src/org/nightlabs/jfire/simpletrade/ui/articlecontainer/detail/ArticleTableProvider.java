ca799076b6f3d2c102b77b082e8dbf1b24ef2988
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-06-25 15:48:38 +0000

    refactored editing of article delivery date to use org.eclipse.tableprovider.ui framework
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.simpletrade.ui@14829 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/simpletrade/ui/articlecontainer/detail/ArticleTableProvider.java
@@ -0,0 +1,130 @@
+package org.nightlabs.jfire.simpletrade.ui.articlecontainer.detail;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.jdo.FetchPlan;
+import javax.jdo.JDOHelper;
+
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.accounting.Tariff;
+import org.nightlabs.jfire.simpletrade.store.SimpleProductType;
+import org.nightlabs.jfire.store.ProductType;
+import org.nightlabs.jfire.trade.Article;
+import org.nightlabs.jfire.trade.dao.ArticleDAO;
+import org.nightlabs.jfire.trade.id.ArticleID;
+import org.nightlabs.jfire.trade.ui.articlecontainer.detail.ArticleTableProviderConstants;
+import org.nightlabs.progress.ProgressMonitor;
+import org.nightlabs.tableprovider.ui.TableProvider;
+
+/**
+ * @author Daniel Mazurek - daniel [at] nightlabs [dot] de
+ *
+ */
+public class ArticleTableProvider
+implements TableProvider<ArticleID, Article>
+{
+	public static final String TYPE_PRODUCT_TYPE_NAME = "ProductTypeName";
+	public static final String TYPE_TARIFF = "Tariff";
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.tableprovider.ui.TableProvider#getObjects(java.util.Collection, org.nightlabs.progress.ProgressMonitor)
+	 */
+	@Override
+	public Map<ArticleID, Article> getObjects(Collection<ArticleID> objectIDs, String scope, ProgressMonitor monitor)
+	{
+		String[] fetchGroups = getFetchGroups(scope);
+		if (fetchGroups == null) {
+			return Collections.emptyMap();
+		}
+		Collection<Article> articles = ArticleDAO.sharedInstance().getArticles(
+				objectIDs,
+				fetchGroups,
+				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT,
+				monitor);
+		Map<ArticleID, Article> articleID2Article = new HashMap<ArticleID, Article>();
+		for (Article article : articles) {
+			if (isCompatible(article, scope)) {
+				ArticleID articleID = (ArticleID) JDOHelper.getObjectId(article);
+				articleID2Article.put(articleID, article);
+			}
+		}
+		return articleID2Article;
+	}
+
+	protected String[] getFetchGroups(String scope) {
+		if (scope.equals(ArticleTableProviderConstants.SCOPE_PRODUCT_TYPE)) {
+			return new String[] {FetchPlan.DEFAULT, Article.FETCH_GROUP_PRODUCT_TYPE,
+					ProductType.FETCH_GROUP_NAME, Article.FETCH_GROUP_TARIFF, Tariff.FETCH_GROUP_NAME};
+		}
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.tableprovider.ui.TableProvider#getText(java.util.Set, java.lang.Object)
+	 */
+	@Override
+	public String getText(Set<String> types, Article element, String scope)
+	{
+		if (scope.equals(ArticleTableProviderConstants.SCOPE_PRODUCT_TYPE)) {
+			if (types.contains(TYPE_PRODUCT_TYPE_NAME)) {
+				return element.getProductType().getName().getText();
+			}
+			else if (types.contains(TYPE_TARIFF)) {
+				Tariff tariff = element.getTariff();
+				return tariff.getName().getText();
+			}
+		}
+		return "";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.tableprovider.ui.TableProvider#getTypeName(java.lang.String)
+	 */
+	@Override
+	public String getTypeName(String type)
+	{
+		if (type.equals(TYPE_PRODUCT_TYPE_NAME)) {
+			return "ProductType Name";
+		}
+		else if (type.equals(TYPE_TARIFF)) {
+			return "Tariff";
+		}
+		return type;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.tableprovider.ui.TableProvider#getTypes()
+	 */
+	@Override
+	public String[] getTypes(String scope) {
+		if (scope.equals(ArticleTableProviderConstants.SCOPE_PRODUCT_TYPE)) {
+			return new String[] {TYPE_PRODUCT_TYPE_NAME, TYPE_TARIFF};
+		}
+		return new String[] {};
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.tableprovider.ui.TableProvider#isCompatible(java.lang.String)
+	 */
+	@Override
+	public boolean isCompatible(Article article, String scope) {
+		if (scope.equals(ArticleTableProviderConstants.SCOPE_PRODUCT_TYPE)) {
+			try {
+				return article.getProductType() instanceof SimpleProductType;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isGeneric() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
