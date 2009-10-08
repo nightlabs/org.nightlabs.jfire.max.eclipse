5f2fa4fcf511592cd0e57a92dbfcad1147cc8a88
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-10-08 10:11:39 +0000

    - improved the output string of the value provider.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.reporting.admin.parameter.ui@15596 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/admin/parameter/ui/figure/ValueProviderConfigFigure.java
+++ src/org/nightlabs/jfire/reporting/admin/parameter/ui/figure/ValueProviderConfigFigure.java
@@ -1,5 +1,8 @@
 package org.nightlabs.jfire.reporting.admin.parameter.ui.figure;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.LineBorder;
 import org.eclipse.swt.graphics.Color;
@@ -18,21 +21,21 @@ extends AbstractInputNodeFigure
 	{
 		super(editPart);
 		this.valueProvider = valueProvider;
-		
+
 		LineBorder lineBorder = new LineBorder(1);
 		lineBorder.setColor(new Color(null, 114, 114, 114));
 		setBorder(lineBorder);
 	}
-		
+
 	private ValueProvider valueProvider;
-	
+
 	@Override
 	protected void paintFigure(Graphics g)
 	{
 		super.paintFigure(g);
 		drawOutputConnector(g, getBounds());
 	}
-		
+
 	protected String getInputString(ValueProviderInputParameter inputParam)
 	{
 //		return "ID " + inputParam.getParameterID() + "\n" + "Type " + inputParam.getParameterType();
@@ -42,9 +45,26 @@ extends AbstractInputNodeFigure
 	@Override
 	protected String getOutputString()
 	{
+		// We remove all packages in order to shorten the displayed types - using simple class-names only. For example:
+		//
+		// * java.lang.String => String
+		// * org.nightlabs.jfire.security.id.UserID => UserID
+		// * java.util.Collection<org.nightlabs.jfire.security.id.UserID> => Collection<UserID>
+		// * java.util.Map<org.nightlabs.jfire.security.id.UserID, java.util.List<org.nightlabs.jfire.something.Whatever>> => Map<UserID, List<Whatever>>
+		//
 		String s = valueProvider.getOutputType();
-		int index = s.lastIndexOf("."); //$NON-NLS-1$
-		return s.substring(index+1);
+		Pattern pattern = Pattern.compile("([^.<>,\\s]*)([\\.<>,\\s]?)"); //$NON-NLS-1$
+		Matcher matcher = pattern.matcher(s);
+		StringBuilder sb = new StringBuilder();
+		while (matcher.find()) {
+			String word = matcher.group(1);
+			String symbol = matcher.group(2);
+			if (!".".equals(symbol)) {
+				sb.append(word);
+				sb.append(symbol);
+			}
+		}
+		return sb.toString();
 	}
 
 	@Override
@@ -59,12 +79,12 @@ extends AbstractInputNodeFigure
 		String s = valueProvider.getInputParameters().get(index).getParameterType();
 		return valueProvider.getInputParameters().get(index).getParameterID() + "("+ s.substring(s.lastIndexOf(".")+1) +")";		 //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
-	
+
 	@Override
 	public String getName() {
 		return valueProvider.getName().getText();
 	}
-	
+
 //	@Override
 //	public String getIputParameterID(int index) {
 //		return valueProvider.getInputParameters().get(index).getParameterID();
