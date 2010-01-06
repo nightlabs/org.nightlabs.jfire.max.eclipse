0a3773906d01634c3d326067ae973548f4e55334
khaireel <khaireel@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-01-06 15:53:44 +0000

    1. The PersonRelationTree has (finally) been unravelled following a desired open path. However, this approach requires the 'unique' PropertySetIDs in the pathToExpand, because the PersonRelationIDs retrieved and the PersonRelationIDs created on the tree are NOT the same. E.g. pathToExpand :: [propertySetID=6][personRelationID=25][personRelationID=23][personRelationID=20] ~~ Retrieved from server.
    On the client side:
    [01]
    ++ objectIDsToRoot :: [propertySetID=7]
    ++ objectIDsToRoot :: [propertySetID=6] <------------ FOUND! Expanding...
    
    [02]
    ++ objectIDsToRoot :: [propertySetID=7]
    ++ objectIDsToRoot :: [propertySetID=6][personRelationID=24]
    ++ objectIDsToRoot :: [propertySetID=6][personRelationID=26]
    ++ objectIDsToRoot :: [propertySetID=6][personRelationID=28]
    ++ objectIDsToRoot :: [propertySetID=6][personRelationID=3a]
    
    DONE.
    
    --> We fail to detect the [personRelationID=25] after [propertySetID=6] on the second level. On inspection of the data, we found that [personRelationID=25] is supposed to be [personRelationID=24], which we wanted to detect to further our expansion.
    
    
    
    2. Thus, the server-side PersonRelationManagerBean, the DAO and the Remote has been reverted to a previous version, which returns only a Deque of PropertySetID. This now works with the current framework of listeners for the lazy-tree.
    
    3. TODO More fine-tuning.
    
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.personrelation.ui@16567 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/personrelation/ui/PersonRelationTreeNode.java
+++ src/org/nightlabs/jfire/personrelation/ui/PersonRelationTreeNode.java
@@ -1,22 +1,18 @@
 package org.nightlabs.jfire.personrelation.ui;
 
-import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.nightlabs.jdo.ObjectID;
 import org.nightlabs.jfire.base.ui.jdo.tree.lazy.JDOObjectLazyTreeNode;
 import org.nightlabs.jfire.personrelation.PersonRelation;
+import org.nightlabs.jfire.personrelation.id.PersonRelationID;
 import org.nightlabs.jfire.prop.id.PropertySetID;
 
 public class PersonRelationTreeNode
 extends JDOObjectLazyTreeNode<ObjectID, Object, PersonRelationTreeController>
 {
-	// ----------------------------- FARK-MARK --------------->>
-	//private List<PropertySetID> filterChil
-	// ----------------------------- FARK-MARK --------------->>
-
-
-
 	// Attempt to stop the unending iteration. At least for use with Behr's descriptions. See notes.
 	/**
 	 * Based on the PropertySetID represented by this node, check to see if the same PropertySetID
@@ -50,10 +46,27 @@ extends JDOObjectLazyTreeNode<ObjectID, Object, PersonRelationTreeController>
 	}
 
 	/**
+	 * @return the PropertySetID represented by this node. Returns null if node does not present a Person-related object.
+	 */
+	public PropertySetID getPropertySetID() {
+		ObjectID jdoObjectID = getJdoObjectID();
+		if (jdoObjectID instanceof PropertySetID)
+			return (PropertySetID) jdoObjectID;
+
+		else {
+			Object jdoObject = getJdoObject();
+			if (jdoObject instanceof PersonRelation)
+				return ((PersonRelation)jdoObject).getToID();
+		}
+
+		return null;
+	}
+
+	/**
 	 * @return a List of PropertySetIDs ordered from this node all the way up to the root.
 	 */
 	public List<PropertySetID> getPropertySetIDsToRoot() {
-		return getPropertySetIDsToRoot(this, new ArrayList<PropertySetID>());
+		return getPropertySetIDsToRoot(this, new LinkedList<PropertySetID>());
 	}
 
 	private List<PropertySetID> getPropertySetIDsToRoot(PersonRelationTreeNode node, List<PropertySetID> propSetIDs) {
@@ -72,5 +85,26 @@ extends JDOObjectLazyTreeNode<ObjectID, Object, PersonRelationTreeController>
 		}
 
 		return getPropertySetIDsToRoot(node.getParent(), propSetIDs);
+	}
+
+	/**
+	 * Use this mainly for comparison with a given path-for-expansion.
+	 * @return the ObjectIDs, containing PersonRelationIDs and/or PropertySetIDs from this node to the root.
+	 */
+	public Deque<ObjectID> getObjectIDsToRoot() {
+		return getObjectIDsToRoot(this, new LinkedList<ObjectID>());
+	}
+
+	private Deque<ObjectID> getObjectIDsToRoot(PersonRelationTreeNode node, Deque<ObjectID> objectIDs) {
+		// Base case.
+		if (node.getParent() == null)
+			return objectIDs;
+
+		// Iterative case.
+		ObjectID jdoObjectID = node.getJdoObjectID();
+		if (jdoObjectID instanceof PropertySetID || jdoObjectID instanceof PersonRelationID)
+			objectIDs.push(jdoObjectID);
+
+		return getObjectIDsToRoot(node.getParent(), objectIDs);
 	}
 }
