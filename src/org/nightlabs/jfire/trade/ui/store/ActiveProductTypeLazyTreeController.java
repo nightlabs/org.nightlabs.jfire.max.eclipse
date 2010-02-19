a9ccf26913b0b73ff11a7f5034c38f3be335de26
khaireel <khaireel@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-02-19 09:52:00 +0000

    1. The PersonRelationTree now cleanly terminates; i.e. we will no longer see a non-stop recurrence relation where a child-node refers to its parent-node, or any of its ancestors. There is a way to do this without changing the original codes in the super (abstract) class ActiveJDOObjectLazyTreeController.
    2. From 1., the method retrieveChildObjectIDs(..) has been restored, in favour of returning a List of TreeNodes given a valid JDOObjectID through the method getTreeNodeList(..).
    
    3. Two methods in the extended controller, PersonRelationTreeController, were amended to incorporate 1. and 2., and so far they hold and dont break the integrity with other parts.
    
    4. A context menu has been added to the PersonRelationIssueTreeView, similar to the Actions from the view's toolbar. Though this can be further optimised, I think, since I dont know how to find a common ground between an IViewActionDelegate (that is already established as an extension in the plugin.xml) and a simple IAction (or IContributionItem).
    
    5. Ensured that the NotificationListener in the PersonRelationIssueTreeView executes only when necessary.
    
    6. New icons to show "companyGroup" and "subsidiary" PersonRelationTypes.
    
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.trade.ui@16904 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/ui/store/ActiveProductTypeLazyTreeController.java
+++ src/org/nightlabs/jfire/trade/ui/store/ActiveProductTypeLazyTreeController.java
@@ -1,14 +1,12 @@
 package org.nightlabs.jfire.trade.ui.store;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.jdo.FetchPlan;
 
 import org.nightlabs.jdo.NLJDOHelper;
-import org.nightlabs.jdo.ObjectID;
 import org.nightlabs.jfire.base.ui.jdo.tree.lazy.ActiveJDOObjectLazyTreeController;
 import org.nightlabs.jfire.jdo.notification.TreeNodeParentResolver;
 import org.nightlabs.jfire.store.ProductType;
@@ -64,13 +62,6 @@ public class ActiveProductTypeLazyTreeController extends ActiveJDOObjectLazyTree
 	protected Map<ProductTypeID, Long> retrieveChildCount(
 			Set<ProductTypeID> parentIDs, ProgressMonitor monitor) {
 		return ProductTypeDAO.sharedInstance().getChildProductTypeCounts(parentIDs, monitor);
-	}
-
-	@Override
-	protected Collection<ObjectID> retrieveChildObjectIDs(
-			List<ObjectID> objectIDsToRoot, ProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-		return null; //super.retrieveChildObjectIDs(objectIDsToRoot, monitor);
 	}
 
 }
