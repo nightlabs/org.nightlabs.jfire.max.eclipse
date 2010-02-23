03c8b9b591e792eccb3036e1153d8da1cf8787e8
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-02-23 15:44:16 +0000

    Continued work on PropertySetViewer framework which is configurable per search use case.
    Already implemented:
     - One PropertySetViewer implementation, the PersonResultTable, exists.
     - Every preference page that implements AbstractPropertySetSearchPreferencePage
       automatically is augmented with the configuration for the viewer.
     - The configuration for the PersonResultTable yet allows to specify the fields to
       be displayed.
       
    Not implemented yet:
     - The PropertySetViewer configured for a use case is not yet used at all. The
       respective code displaying the result should obtain the viewer factory from the
       cfmod and render it   
     - More PropertySetViewers (PersonRelationTree) with corresponding factories 
       and configuration.    
     - Extend configurability of the PersonResultTable
    
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.contact.ui@16942 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/contact/ui/preferences/ContactSearchEditLayoutPreferencePage.java
+++ src/org/nightlabs/jfire/contact/ui/preferences/ContactSearchEditLayoutPreferencePage.java
@@ -1,13 +1,13 @@
 package org.nightlabs.jfire.contact.ui.preferences;
 
-import org.nightlabs.jfire.base.ui.prop.search.config.AbstractPersonSearchEditLayoutPreferencePage;
+import org.nightlabs.jfire.base.ui.person.search.AbstractPersonSearchEditLayoutPreferencePage;
 import org.nightlabs.jfire.contact.ui.PersonSearchUseCaseConstants;
 import org.nightlabs.jfire.layout.AbstractEditLayoutConfigModule;
 
 public class ContactSearchEditLayoutPreferencePage extends AbstractPersonSearchEditLayoutPreferencePage {
 
 	@Override
-	protected String getConfigModuleID() {
+	public String getConfigModuleID() {
 		return AbstractEditLayoutConfigModule.getCfModID(AbstractEditLayoutConfigModule.CLIENT_TYPE_RCP, PersonSearchUseCaseConstants.USE_CASE_ID_CONTACT_SEARCH);
 	}
 
