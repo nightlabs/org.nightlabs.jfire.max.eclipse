6b8d9691ec5fb0525c9cfca532438eb90e7e4298
khaireel <khaireel@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-04-13 17:18:46 +0000

    1. Create a new abstract super class "ContextMenuReadyComposite", placed on top of the current AbstractTreeComposite and AbstractTableComposite, that manages both Composites' internal context-menus (if and when context-menus are needed). The menu-items are treated as priority-ordered context-menus (initialised through the method integratePriorityOrderedContextMenu()), and which has been streamlined to handle 3 types of contextMenuContributions: (i) IContributionItem, (ii) IAction, and (iii) IViewActionDelegate.
    2. An example of such an implementation in 1. can be seen in the class org.nightlabs.jfire.personrelation.ui.AbstractPersonRelationTreeView.
    
    
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.dynamictrade.admin.ui@17393 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/admin/ui/tree/DynamicProductTypeTree.java
+++ src/org/nightlabs/jfire/dynamictrade/admin/ui/tree/DynamicProductTypeTree.java
@@ -6,6 +6,7 @@ import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.part.DrillDownAdapter;
 import org.nightlabs.base.ui.resource.SharedImages;
 import org.nightlabs.base.ui.tree.AbstractTreeComposite;
 import org.nightlabs.jfire.base.ui.jdo.tree.JDOObjectTreeContentProvider;
@@ -53,7 +54,9 @@ extends AbstractTreeComposite<DynamicProductType>
 
 //		drillDownAdapter = new DrillDownAdapter(getTreeViewer());
 //		hookContextMenu();
-		createContextMenu(true);
+		
+		TreeViewer treeViewer = getTreeViewer();
+		createContextMenu(new DrillDownAdapter(treeViewer), treeViewer.getControl());
 	}
 
 	// ------------------------------------------------------------------------------------- ++ ------------------------------->>
