778205bb29ca126cb8eb830cb616aefe59e2c6d5
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-03-23 18:33:35 +0000

    solved issue https://www.jfire.org/modules/bugs/view.php?id=1924
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.asterisk.ui@18701 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/asterisk/ui/asteriskserver/CreateAsteriskServerAction.java
+++ src/org/nightlabs/jfire/asterisk/ui/asteriskserver/CreateAsteriskServerAction.java
@@ -1,33 +1,18 @@
 package org.nightlabs.jfire.asterisk.ui.asteriskserver;
 
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IViewActionDelegate;
-import org.eclipse.ui.IViewPart;
 import org.nightlabs.base.ui.wizard.DynamicPathWizardDialog;
+import org.nightlabs.jfire.base.ui.login.action.LSDWorkbenchWindowActionDelegate;
 
 /**
  * @author Chairat Kongarayawetchakun - chairat[at]nightlabs[dot]de
  */
 public class CreateAsteriskServerAction 
-implements IViewActionDelegate 
+extends LSDWorkbenchWindowActionDelegate 
 {
-	public static final String ID = CreateAsteriskServerAction.class.getName();
-	
-	private IViewPart viewPart;
-	@Override
-	public void init(IViewPart viewPart) {
-		this.viewPart = viewPart;
-	}
-
 	@Override
 	public void run(IAction action) {
-		DynamicPathWizardDialog dialog = new DynamicPathWizardDialog(viewPart.getSite().getShell(), new CreateAsteriskServerWizard());
+		DynamicPathWizardDialog dialog = new DynamicPathWizardDialog(getShell(), new CreateAsteriskServerWizard());
 		dialog.open();
-	}
-
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-
 	}
 }
