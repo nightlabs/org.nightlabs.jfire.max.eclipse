12c9a6135f64cdc27d9204c50c0a3b78aa7172fa
ddudnik <ddudnik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-09-29 14:57:13 +0000

    - added a double click listener for opening ProjectEditor from ProjectAdminView
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@19531 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/project/ProjectAdminView.java
+++ src/org/nightlabs/jfire/issuetracking/ui/project/ProjectAdminView.java
@@ -1,9 +1,13 @@
 package org.nightlabs.jfire.issuetracking.ui.project;
 
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.nightlabs.base.ui.util.RCPUtil;
 import org.nightlabs.jfire.base.login.ui.Login;
 import org.nightlabs.jfire.base.login.ui.part.LSDViewPart;
+import org.nightlabs.jfire.issue.project.Project;
 import org.nightlabs.jfire.issuetracking.ui.project.create.CreateProjectAction;
 
 public class ProjectAdminView
@@ -20,6 +24,18 @@ extends LSDViewPart
 	@Override
 	public void createPartContents(Composite parent) {
 		projectTreeComposite = new ProjectAdminTreeComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL, true);
+		projectTreeComposite.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent e) {
+				Project project = projectTreeComposite.getFirstSelectedElement();
+				
+				try {
+					RCPUtil.openEditor(new ProjectEditorInput(project.getObjectId()),
+							ProjectEditor.EDITOR_ID);
+				} catch (Exception ex) {
+					throw new RuntimeException(ex);
+				}
+			}
+		});
 
 		createActions();
 		createToolbarButtons();
