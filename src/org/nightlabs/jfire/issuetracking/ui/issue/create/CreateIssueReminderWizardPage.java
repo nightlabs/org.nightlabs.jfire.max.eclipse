61f0d8d3ca151b2ef492d1f000f3c2bf7751c457
ddudnik <ddudnik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-10-03 10:10:42 +0000

    - fixed: deadline was not set when date was selected in DateTimeControl with calendar widget (SelectionListener for DateTimeControl was missing)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.issuetracking.ui@19553 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetracking/ui/issue/create/CreateIssueReminderWizardPage.java
+++ src/org/nightlabs/jfire/issuetracking/ui/issue/create/CreateIssueReminderWizardPage.java
@@ -3,6 +3,8 @@ package org.nightlabs.jfire.issuetracking.ui.issue.create;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -18,7 +20,7 @@ import org.nightlabs.jfire.issuetracking.ui.IssueTrackingPlugin;
 import org.nightlabs.jfire.issuetracking.ui.issue.IssueUserComposite;
 import org.nightlabs.jfire.issuetracking.ui.issue.IssueUserComposite.Orientation;
 import org.nightlabs.jfire.issuetracking.ui.resource.Messages;
-import org.nightlabs.l10n.DateFormatter;
+import org.nightlabs.l10n.IDateFormatter;
 
 /**
  * @author Chairat Kongarayawetchakun - chairat[at]nightlabs[dot]de
@@ -49,11 +51,17 @@ extends WizardHopPage
 		deadlineLabel.setText(Messages.getString("org.nightlabs.jfire.issuetracking.ui.issue.create.CreateIssueReminderWizardPage.label.deadline.text")); //$NON-NLS-1$
 
 		Composite deadlineWrapper = new XComposite(mainComposite, SWT.NONE, LayoutMode.TIGHT_WRAPPER, LayoutDataMode.GRID_DATA_HORIZONTAL, 2);
-		deadlineDateTime = new DateTimeControl(deadlineWrapper, SWT.NONE, DateFormatter.FLAGS_DATE_SHORT_TIME_HMS_WEEKDAY);
+		deadlineDateTime = new DateTimeControl(deadlineWrapper, SWT.NONE, IDateFormatter.FLAGS_DATE_SHORT_TIME_HMS_WEEKDAY);
 		deadlineDateTime.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		deadlineDateTime.addModifyListener(new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
+				issue.setDeadlineTimestamp(deadlineDateTime.getDate());
+			}
+		});
+		deadlineDateTime.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
 				issue.setDeadlineTimestamp(deadlineDateTime.getDate());
 			}
 		});
